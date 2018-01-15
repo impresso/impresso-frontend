0964cba6db26100142b3b7284918d90e3f46ce0e
Thijs van Beek <thijs@midasweb.nl>
2018-01-15 14:26:45 +0100

    added abstract filter wrapper and context for a more moduler approach

---
+++ src/components/modules/FilterAbstractContext.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="filter-context">
+    <b-form-select v-model="filter.context" :options="options" class="mb-3" />
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'filter',
+  },
+  props: ['filter', 'options'],
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/components/modules/FilterString.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <b-input type="text" v-model="filter.query" />
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'filter',
+  },
+  props: ['filter'],
+};
+</script>
+
+<style lang="css">
+</style>
