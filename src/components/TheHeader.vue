a3ebd7b76fd63c099aefca61570831b14d6b438c
Thijs van Beek <thijs@midasweb.nl>
2018-02-05 16:01:19 +0100

    cleanup on homepage and header for better responsive layout

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,12 @@
 <template lang="html">
   <header id="header" class="py-2" v-bind:class="{loading: showProgress}">
-    <div class="container">
+    <b-container fluid>
       <b-row>
         <b-col>
           <router-link :to="{name: 'home'}" class="logo">impresso</router-link>
         </b-col>
         <b-col class="text-right">
-          {{$t("language")}}
+          <!-- {{$t("language")}} -->
           <b-dropdown right size="sm" variant="link" :text="languages[activeLanguageCode].name">
             <b-dropdown-item
               v-for="language in languages"
@@ -17,7 +17,7 @@
           </b-dropdown>
         </b-col>
       </b-row>
-    </div>
+    </b-container>
   </header>
 </template>
 
@@ -66,32 +66,26 @@ export default {
 };
 </script>
 
-<style scoped lang="less">
-@bg_color: #234;
-@fg_color: #432;
-@speed: 1s;
+<style lang="less">
 header {
-    background: @bg_color;
-    color: white;
+    background: #345;
+    color:white;
+    transition: background-color 100ms;
     .logo {
         color: white;
         display: block;
         padding-top: 0.25rem; //inherterd from the language dropdown
     }
 
-    &.loading {
-        background: @bg_color;
-        background: linear-gradient(45deg, @bg_color, @fg_color, @bg_color);
-        animation: progress @speed linear infinite;
+    .btn{
+      color:white;
+      &:hover{
+        color:#ccc;
+      }
     }
-}
 
-@-webkit-keyframes progress {
-    0% {
-        background-position: -100vw 0;
-    }
-    100% {
-        background-position: 0 0;
+    &.loading {
+        background: #f50;
     }
 }
 </style>
