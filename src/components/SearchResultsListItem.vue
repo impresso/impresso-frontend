1df52610980cb3545516750c3cfdab8f35ed2c79
Thijs van Beek <thijs@midasweb.nl>
2018-06-26 16:55:17 +0200

    added brief description and details of article on search restul list item

--- src/components/SearchResultsListItem.vue
+++ src/components/SearchResultsListItem.vue
@@ -4,9 +4,16 @@
       <open-seadragon-viewer v-model="article.pages[0].iiif"></open-seadragon-viewer>
     </div>
     <h2><a href="#" v-on:click.prevent="click">{{article.title}}</a></h2>
-    <p>{{$d(article.date, "long")}}</p>
-    <collection-tagger v-model="article"></collection-tagger>
-
+    <p>
+      <strong>{{article.newspaper.name}}</strong> |
+      {{$d(article.date, "long")}} | 
+      page: <span>{{article.pages.map(page => page.num)}}</span> |
+      <collection-tagger v-model="article"></collection-tagger>
+    </p>
+    <p v-if="article.excerpt.length > 0">{{article.excerpt}}</p>
+    <ul>
+      <li v-for="match in article.matches">{{match.fragment}}</li>
+    </ul>
     <div>
       <b-badge pill v-for="tag in article.tags" variant="secondary" v-bind:key="tag.uid">{{tag.name}}</b-badge>
     </div>
