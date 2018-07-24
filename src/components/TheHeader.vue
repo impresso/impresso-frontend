cfda1912eaf9bf0080f447d2ac7cf1e870d0a02d
Thijs van Beek <thijs@midasweb.nl>
2018-07-24 16:59:56 +0200

    removed styling and refactored to use (vue)bootstrap

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,38 +1,37 @@
 <template lang="html">
-  <header id="TheHeader" v-bind:class="{loading: showProgress}">
-    <div class="header-logo">
-      <router-link :to="{name: 'home'}">
-        <img src="./../assets/img/impresso-logo-h-i@2x.png"  />
-      </router-link>
-    </div>
-    <div class="header-left">
-      <router-link v-bind:to="{ name: 'home'}">{{$t("home")}}</router-link> >
-      <router-link v-bind:to="{ name: 'search'}">{{$t("search")}}</router-link>
-    </div>
-    <div class="header-center">
-      <div v-html="headerTitle" v-show="headerTitle"></div>
-    </div>
-    <div class="header-right">
-      <b-dropdown v-if="user" right no-caret>
-        <template slot="button-content">
-          <img src="http://via.placeholder.com/25&text=RA" alt="">
-          <div>
-            <strong>{{userFullName}}</strong><br>{{user.group}}
-          </div>
-        </template>
-        <b-dropdown-item v-bind:to="{ name: 'dashboard'}">{{$t("dashboard")}}</b-dropdown-item>
-        <b-dropdown-item v-bind:to="{ name: 'collection'}">{{$t("collections")}}</b-dropdown-item>
-        <b-dropdown-item v-on:click.prevent="logout">{{$t("logout")}}</b-dropdown-item>
-      </b-dropdown>
-      <b-link v-else v-bind:to="{ name: 'login'}">{{$t("login")}}</b-link >
-      <b-dropdown v-bind:text="languages[activeLanguageCode].name" right>
-        <b-dropdown-item v-for="language in languages"
-        v-bind:active="activeLanguageCode === language.code"
-        v-bind:key="language.code"
-        v-on:click="selectLanguage(language.code)">{{language.name}}</b-dropdown-item>
-      </b-dropdown>
-    </div>
-  </header>
+  <b-navbar id="TheHeader" toggleable="md" type="dark" v-bind:variant="navbarVariant">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand :to="{name: 'home'}">
+      <img src="./../assets/img/impresso-logo-h-i@2x.png"  />
+    </b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <li class="nav-item">
+          <router-link v-bind:to="{ name: 'home'}" exact-active-class="active" class="nav-link">{{$t("home")}}</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link v-bind:to="{ name: 'search'}" exact-active-class="active" class="nav-link">{{$t("search")}}</router-link>
+        </li>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown v-bind:text="languages[activeLanguageCode].name" right>
+          <b-dropdown-item v-for="language in languages"
+          v-bind:active="activeLanguageCode === language.code"
+          v-bind:key="language.code"
+          v-on:click="selectLanguage(language.code)">{{language.name}}</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown v-if="user" right>
+          <template slot="button-content">
+            <em>{{userFullName}}</em>
+          </template>
+          <b-dropdown-item v-bind:to="{ name: 'dashboard'}">{{$t("dashboard")}}</b-dropdown-item>
+          <b-dropdown-item v-bind:to="{ name: 'collection'}">{{$t("collections")}}</b-dropdown-item>
+          <b-dropdown-item v-on:click.prevent="logout">{{$t("logout")}}</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item v-else v-bind:to="{ name: 'login'}">{{$t("login")}}</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -65,8 +64,8 @@ export default {
     activeLanguageCode() {
       return this.$store.state.settings.language_code;
     },
-    showProgress() {
-      return this.$store.state.processing_status;
+    navbarVariant() {
+      return this.$store.state.processing_status ? 'warning' : 'dark';
     },
     user() {
       return this.$store.getters['user/user'];
@@ -110,58 +109,12 @@ export default {
 </script>
 
 <style lang="less">
-@import "./../assets/less/style.less";
-
 #TheHeader {
-    display: grid;
-    height: 50px;
-    z-index: 100;
-    grid-template-columns: 180px max-content auto max-content;
-    grid-template-rows: auto;
-    grid-template-areas: "header-logo header-left header-center header-right";
-
-    background: @clr-black;
-    border-top: 2px solid @impresso-yellow;
-    transition: background-color 100ms;
-
-    .header-logo {
-        grid-area: header-logo;
-        padding: 10px;
-        display: inline-block;
-        height: 50px;
-        padding: 10px 15px 10px 10px;
-        img{
-          width: 100%;
+    .navbar-brand {
+        padding: 0;
+        img {
+            height: 30px;
         }
-    }
-
-    .header-left {
-        grid-area: header-left;
-        height: 100%;
-    }
-    .header-center {
-        grid-area: header-center;
-        overflow: hidden;
-        text-align: center;
-        > div {
-            display: inline-block;
-            font-size: 1rem;
-            background: white;
-            padding: 1px 6px;
-            .text-serif();
-            .title {}
-            .subtitle {
-                font-weight: bold;
-            }
-        }
-    }
-    .header-right {
-        grid-area: header-right;
-        text-align: right;
-    }
-
-    &.loading {
-        background: @clr-yellow;
     }
 }
 </style>
@@ -169,9 +122,9 @@ export default {
 <i18n>
 {
   "en": {
-    "login": "login",
-    "logout": "logout",
-    "dashboard": "dashboard"
+    "login": "Login",
+    "logout": "Logout",
+    "dashboard": "Dashboard"
   }
 }
 </i18n>
