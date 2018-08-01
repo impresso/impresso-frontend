ffef36049f82a4506656fe0ebfc334df55ae53d3
Thijs van Beek <thijs@midasweb.nl>
2018-08-01 14:15:20 +0200

    added minWidth, maxWidth and width props to component

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,16 +1,48 @@
 <template lang="html">
-<main>
+<main v-bind:style="style">
   <slot></slot>
 </main>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    width: {
+      default: false, // 50vw, 768px, 80%
+    },
+    minWidth: {
+      default: false,
+    },
+    maxWidth: {
+      default: false,
+    },
+  },
+  computed: {
+    style() {
+      const style = {};
+
+      if (this.width) {
+        style.width = this.width;
+      }
+
+      if (this.minWidth) {
+        style['min-width'] = this.minWidth;
+      }
+
+      if (this.maxWidth) {
+        style['max-width'] = this.maxWidth;
+      }
+
+      return style;
+    },
+  },
+};
 </script>
 
 <style scoped lang="less">
 main {
-  height: 100%;
-  display: flex;
+    height: 100%;
+    display: flex;
+    margin: 0 auto;
 }
 </style>
