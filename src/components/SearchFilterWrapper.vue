08e23fb1f30d2eacd870a562e786a7b0490aca9a
Thijs van Beek <thijs@midasweb.nl>
2018-08-08 16:05:40 +0200

    added computed prop filters

--- src/components/SearchFilterWrapper.vue
+++ src/components/SearchFilterWrapper.vue
@@ -1,23 +1,23 @@
 <template lang="html">
   <div id="search-filter-wrapper">
     <div class="filters pb-2">
-      <div ref="filters" v-for="(filter, index) in search.filters" v-bind:key="index">
+      <div ref="filters" v-for="(filter, index) in filters" v-bind:key="index">
           <filter-string
             v-if="filter.type.toLowerCase() === 'string'"
-            v-model="search.filters[index]"
+            v-model="filters[index]"
             v-on:input="updateFilter"
             v-on:submit="submitFilter"
             v-on:remove="removeFilter(index)"
           />
           <filter-named-entity
             v-if="filter.type.toLowerCase() === 'entity'"
-            v-model="search.filters[index]"
+            v-model="filters[index]"
             v-on:input="updateFilter"
             v-on:remove="removeFilter(index)"
           />
           <filter-date-range
             v-if="filter.type.toLowerCase() === 'daterange'"
-            v-model="search.filters[index]"
+            v-model="filters[index]"
             v-on:input="updateFilter"
             v-on:remove="removeFilter(index)"
           />
@@ -41,6 +41,12 @@ export default {
     search: {
       get() {
         return this.$store.getters['search/getSearch'];
+      },
+    },
+    filters: {
+      get() {
+        // TODO: here we can sort the filters in order of type text/entity etc to group them
+        return this.search.filters;
       },
     },
   },
