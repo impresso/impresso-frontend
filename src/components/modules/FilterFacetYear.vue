ca524fdea3c4bed7b895f3eb4bde02c8db780219
Thijs van Beek <thijs@midasweb.nl>
2018-08-20 14:15:53 +0200

    added filter facet year component which act as a wrapper for the skyle but will also late rhold the date range selection

---
+++ src/components/modules/FilterFacetYear.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <section class="filter-facet-year">
+    <p>Put here the skyline component with v-model="data" </p>
+    <!-- <skyline v-model="data"></skyline> -->
+    <pre>{{data}}</pre>
+  </section>
+</template>
+
+<script>
+import Facet from '@/models/Facet';
+
+export default {
+  props: {
+    data: {
+      type: Facet,
+    },
+  },
+};
+</script>
+
+<style lang="less">
+</style>
