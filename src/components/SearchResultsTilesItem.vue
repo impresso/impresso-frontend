65e832c2e8750ce94a0be4611007afb0aa7e707a
Unknown <hello@youtag.lu>
2018-08-27 12:00:02 +0200

    remove dependency on local style.less

--- src/components/SearchResultsTilesItem.vue
+++ src/components/SearchResultsTilesItem.vue
@@ -27,13 +27,13 @@ export default {
 </script>
 
 <style scoped lang="less">
-@import "./../assets/less/style.less";
+// @import "./../assets/less/style.less";
 
 .thumbnail {
     width: 100%;
     height: 180px;
     cursor: move;
-    border:1px solid @clr-grey;
+    border: 1px solid #ccc;
     margin-bottom: 10px;
 }
 </style>
