60b307a2f42a02ea9f03f9ce9818223f01d10df3
Paul <hello@youtag.lu>
2018-09-04 12:10:35 +0200

    added styles to search result list

--- src/components/modules/SearchResultsListItem.vue
+++ src/components/modules/SearchResultsListItem.vue
@@ -1,16 +1,17 @@
 <template lang="html">
-  <b-media>
+  <b-media class="py-3 border-bottom">
     <div class="thumbnail" slot="aside" >
       <open-seadragon-viewer v-model="article.pages[0].iiif"></open-seadragon-viewer>
     </div>
+    <div class="font-weight-bold text-tertiary mb-1">1 / 234</div>
     <h2><a href="#" v-on:click.prevent="click" v-html="article.title"></a></h2>
-    <p>
-      <strong>{{article.newspaper.name}}</strong> |
-      {{$d(article.date, "long")}} |
-      page: <span>{{article.pages.map(page => page.num)}}</span> |
-    </p>
-    <p v-if="article.excerpt.length > 0">{{article.excerpt}}</p>
-    <ul>
+    <div class="article-meta mb-1">
+      <strong>{{article.newspaper.name}}</strong>,
+      <span class="small-caps">{{$d(article.date, "long")}}</span>
+      (p. <span>{{article.pages.map(page => page.num).join('; ')}}</span>)
+    </div>
+    <div v-if="article.excerpt.length > 0" class="article-excerpt">{{article.excerpt}}</div>
+    <ul class="article-matches">
       <li v-for="match in article.matches" v-html="match.fragment" v-show="match.fragment.trim().length > 0"></li>
     </ul>
     <div>
@@ -44,11 +45,37 @@ export default {
 
 <style scoped lang="less">
 .thumbnail {
-    width: 240px;
-    height: 180px;
+    width: 185px;
+    height: 240px;
     position: relative;
     cursor: move;
     border:1px solid #ccc;
+}
+h2 {
+  font-size: 1em;
+  font-weight: 500;
+  font-family: questa-grande;
+  a {
+    text-decoration: underline;
+    text-decoration-color:#ccc;
+    &:hover {
+      text-decoration-color: transparent;
+    }
+  }
+}
+.article-matches,
+.article-meta,
+.article-excerpt {
+  font-size: 0.9em;
+}
+ul.article-matches {
+  list-style: none;
+  padding: 0;
+  li {
+    border-left: 2px solid black;
+    margin: 1em 0;
+    padding-left: 1em;
+  }
 }
 </style>
 
