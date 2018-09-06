8af54eccf039329da0c4783715d12c68dd69c166
Paul <hello@youtag.lu>
2018-09-06 10:20:02 +0200

    added border below string/entity filters

--- src/components/SearchPage.vue
+++ src/components/SearchPage.vue
@@ -5,7 +5,7 @@
       <search-bar />
     </div>
     <div class="px-2 py-4 pt-0 border-bottom" v-if="filters.length > 0">
-      <search-filters v-on:remove="search()" v-on:submit="search()" />
+      <search-filters class="border-bottom" v-on:remove="search()" v-on:submit="search()" />
     </div>
   </i-layout-section>
   <i-layout-section>
