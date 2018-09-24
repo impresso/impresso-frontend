6275c05157095990bdd8cd5b769cc8ae5ad419be
Paul <hello@youtag.lu>
2018-09-24 14:32:01 +0200

    added layout for multiple region rows

--- src/components/modules/ArticleViewer.vue
+++ src/components/modules/ArticleViewer.vue
@@ -1,16 +1,40 @@
 <template lang="html">
   <div class="article m-2">
 
-    <div class="page">
-      <p class="text-tertiary">p. {{article.pages[0].num}}</p>
+    <div class="regions">
+
+      <div class="region">
+        <div class="page">
+          <p class="text-tertiary">p. {{article.pages[0].num}}</p>
+        </div>
+        <div class="context">
+          <img src="https://dummyimage.com/300x320/eee/ccc" width="100%" />
+        </div>
+        <div class="richtext">
+          <h5 v-html="article.title"></h5>
+          <p v-html="article.excerpt" class="text-tertiary"></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="region">
+        <div class="page">
+          <p class="text-tertiary">p. {{article.pages[0].num}}</p>
+        </div>
+        <div class="context">
+          <img src="https://dummyimage.com/300x420/eee/ccc" width="100%" />
+        </div>
+        <div class="richtext">
+          <h5 v-html="article.title"></h5>
+          <p v-html="article.excerpt" class="text-tertiary"></p>
+        </div>
+      </div>
+
+      <hr>
+
     </div>
-    <div class="context">
-      <img src="https://dummyimage.com/300x420/eee/ccc" width="100%" />
-    </div>
-    <div class="text">
-      <h5 v-html="article.title"></h5>
-      <p v-html="article.contents" class="text-tertiary"></p>
-    </div>
+
     <div class="controls text-right">
       <b-button-group>
         <b-button size="sm" variant="outline-primary">Add Tag ...</b-button>
@@ -48,9 +72,24 @@ export default {
 <style scoped lang="less">
 .article {
     display: grid;
-    grid-template-columns: 40px 1fr 2fr 220px;
+    grid-template-columns: 1fr 220px;
     grid-template-rows: auto;
-    grid-template-areas: 'page richtext context controls';
+    grid-template-areas: 'regions controls';
+    column-gap: 1rem;
+
+    .regions {
+      grid-area: regions;
+    }
+
+    .controls{
+      grid-area: controls;
+    }
+}
+.region {
+    display: grid;
+    grid-template-columns: 40px 1fr 2fr;
+    grid-template-rows: auto;
+    grid-template-areas: 'page context richtext';
     column-gap: 1rem;
 
     .page{
@@ -58,15 +97,12 @@ export default {
     }
 
     .context{
-      grid-area: richtext;
-    }
-
-    .content{
       grid-area: context;
     }
 
-    .controls{
-      grid-area: controls;
+    .richtext {
+      grid-area: richtext;
     }
+
 }
 </style>
