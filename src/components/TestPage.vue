7461a2452e64256015a738a000db93c8e58a255c
Paul <hello@youtag.lu>
2018-09-25 14:50:47 +0200

    starting with clean test component

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,35 +1,38 @@
-<template lang="html">
-  <i-layout id="TestPage">
-    <i-layout-section width="400px" class="br">
-      <search-bar v-on:submit="search" />
-    </i-layout-section>
-    <i-layout-section>
-      <h1>results</h1>
-      <pre>{{searchResults}}</pre>
-    </i-layout-section>
-  </i-layout>
+<template>
+<i-layout id="SearchPage">
+  <i-layout-section width="400px" class="border-right">
+    <div class="px-4 py-4 border-bottom">
+      <search-bar />
+    </div>
+  </i-layout-section>
+  <i-layout-section>
+  </i-layout-section>
+</i-layout>
 </template>
 
 <script>
 import SearchBar from './SearchBar';
 
 export default {
-  data: () => ({
-  }),
   computed: {
-    searchResults: {
-      get() {
-        return this.$store.getters['search/results'];
-      },
-    },
+
   },
   methods: {
+
   },
   components: {
-    SearchBar,
+
+    'search-bar': SearchBar,
+  },
+  mounted() {
+
+  },
+  beforeDestroy() {
+
   },
 };
 </script>
 
-<style scoped lang="less">
+<style lang="less">
+
 </style>
