40b09f5dc886848b23f37681dc7d91552e5602b3
Paul <hello@youtag.lu>
2018-09-26 12:23:25 +0200

    component tabbed-nav created

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -2,65 +2,59 @@
 <i-layout id="SearchPage">
   <i-layout-section width="400px" class="border-right">
 
-      <b-nav tabs class="bg-light pl-2 pt-2">
-        <b-nav-item class="small-caps" href="#tab-1" active>Active <br>Tab</b-nav-item>
-        <b-nav-item class="small-caps" href="#tab-2">Link <br>Tab</b-nav-item>
-        <b-nav-item class="small-caps" href="#tab-3">Another <br>Link Tab</b-nav-item>
-        <b-nav-item class="small-caps" disabled>Disabled <br>Tab</b-nav-item>
-      </b-nav>
-
+      <tabbed-nav v-model="tabs"></tabbed-nav>
       <!-- Tab panes -->
       <div class="tab-content py-3">
         <div class="tab-pane active" id="tab-1" role="tabpanel"
           aria-labelledby="home-tab">home</div>
-        <div class="tab-pane" id="tab2" role="tabpanel"
+        <div class="tab-pane" id="tab-2" role="tabpanel"
           aria-labelledby="profile-tab">Profile</div>
         <div class="tab-pane" id="tab-3" role="tabpanel"
           aria-labelledby="messages-tab">Messages</div>
       </div>
 
-    <div class="px-4 py-4 border-bottom">
-      <search-bar />
-    </div>
-  </i-layout-section>
-  <i-layout-section>
-    <b-navbar type="light" variant="light" class="border-bottom px-0 py-0">
-      <b-navbar-nav class="section px-3 py-3 pr-auto border-right">
-        <label class="mr-1">{{$t("label_group")}}</label>
-        <i-dropdown v-model="groupBy" v-bind:options="groupByOptions" size="sm" variant="outline-primary"></i-dropdown>
-      </b-navbar-nav>
-      <b-navbar-nav class="px-3 py-3 section border-right">
-        <label class="mr-1">{{$t("label_order")}}</label>
-        <i-dropdown v-model="orderBy" v-bind:options="orderByOptions" size="sm" variant="outline-primary"></i-dropdown>
-      </b-navbar-nav>
-      <b-navbar-nav class="px-3 py-3 section">
-        <label class="mr-1">{{$t("label_display")}}</label>
-        <b-nav-form>
-          <b-form-radio-group v-model="displayStyle" button-variant="outline-primary" size="sm" buttons>
-            <b-form-radio value="list">{{$t("display_button_list")}}</b-form-radio>
-            <b-form-radio value="tiles">{{$t("display_button_tiles")}}</b-form-radio>
-          </b-form-radio-group>
-        </b-nav-form>
-      </b-navbar-nav>
-    </b-navbar>
-
-  </i-layout-section>
-</i-layout>
+    </i-layout-section>
+    <i-layout-section>
+      <b-navbar type="light" variant="light" class="border-bottom px-0 py-0">
+        <b-navbar-nav class="section px-3 py-3 pr-auto border-right">
+          <label class="mr-1">hey</label>
+        </b-navbar-nav>
+      </b-navbar>
+    </i-layout-section>
+  </i-layout>
 </template>
 
 <script>
 import SearchBar from './SearchBar';
+import TabbedNav from './modules/TabbedNav';
 
 export default {
-  computed: {
-
-  },
+  data: () => ({
+    tabs: [
+      {
+        label: 'thijs',
+      },
+      {
+        label: 'daniele',
+        disabled: true,
+      },
+      {
+        label: 'paul',
+        active: true,
+      },
+    ],
+  }),
   methods: {
-
+    getActiveTab() {
+      return this.tabs.find(t => t.active === true);
+    },
   },
   components: {
-
+    'tabbed-nav': TabbedNav,
     'search-bar': SearchBar,
+  },
+  computed: {
+
   },
   mounted() {
 
