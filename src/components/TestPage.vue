81e460e7ac489af73d9fcb19fbd12cf949bbc054
Thijs van Beek <thijs@midasweb.nl>
2018-09-27 11:57:58 +0200

    improved tabbar component

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,11 +1,34 @@
 <template lang="html">
-
+  <div class="container">
+    <base-tabs v-model="tab" v-bind:tabs="tabs"></base-tabs>
+    <div v-for="t in tabs" v-show="t === tab">
+      {{t}}
+    </div>
+  </div>
 </template>
 
 <script>
+import BaseTabs from './base/BaseTabs';
+
 export default {
+  data: () => ({
+    tab: {},
+    tabs: [
+      {
+        label: 'thijs',
+      },
+      {
+        label: 'daniele',
+        disabled: true,
+      },
+      {
+        label: 'paul',
+        active: true,
+      },
+    ],
+  }),
+  components: {
+    BaseTabs,
+  },
 };
 </script>
-
-<style scoped lang="scss">
-</style>
