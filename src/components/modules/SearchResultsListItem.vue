5a5a242d7126741de117913a4cf23a6a77e95907
Paul <hello@youtag.lu>
2018-10-15 10:28:17 +0200

    zoom preview to first article region

--- src/components/modules/SearchResultsListItem.vue
+++ src/components/modules/SearchResultsListItem.vue
@@ -1,7 +1,10 @@
 <template lang="html">
   <b-media class="py-3 border-bottom">
     <div class="thumbnail" slot="aside" >
-      <open-seadragon-viewer v-model="article.pages[0].iiif"></open-seadragon-viewer>
+      <open-seadragon-viewer
+        v-model="article.pages[0].iiif"
+        v-bind:bbox="article.regions[0].coords">
+      </open-seadragon-viewer>
     </div>
     <h2><a href="#" v-on:click.prevent="click" v-html="article.title"></a></h2>
     <div class="article-meta mb-1">
