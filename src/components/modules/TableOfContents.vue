2d83f6a02548bb116675f81326ec508c3b2fbf60
Paul <hello@youtag.lu>
2018-10-24 12:06:30 +0200

    rebuilt table of contents; added class .active for current selection; .highlight for highlighting a range of articles

--- src/components/modules/TableOfContents.vue
+++ src/components/modules/TableOfContents.vue
@@ -1,10 +1,25 @@
 <template lang="html">
-  <div>
-    <div v-for="article in toc">
-      <h4 v-html="article.title || '<i>Title Unavailable</i>'"></h4>
-      <button class="btn btn-sm btn-secondary" type="button" name="button" v-on:click="onClick(article)">Load</button>
+  <div class="border-top">
+    <div
+      v-for="article in toc"
+      class="listArticle border-bottom p-2 bg-light"
+      v-on:click="setActiveArticle(article)"
+      v-bind:class="{
+        'active': article.uid === active,
+        'highlight': highlight.includes(article.uid) }">
+      <strong class="float-left w-75">{{ theHeader(article) }}</strong>
+      <b-button
+        class="btn btn-sm btn-secondary float-right"
+        variant="outline-primary"
+        v-on:click="loadArticle(article)">Load</b-button>
+      <br>
+      <label class="w-100">pp. {{selected}}
+        <b-badge variant="light" v-for="(page, index) in article.pages">{{page.num}}</b-badge>
+      </label>
+      <p class="teaser mt-0 mb-1">
+        {{ theTeaser(article.excerpt, 120) }}
+      </p>
       <!-- <pre>{{article}}</pre> -->
-      <hr>
     </div>
   </div>
 </template>
@@ -18,17 +33,48 @@ export default {
     toc: {
       default: [],
     },
+    active: {
+      default: '',
+    },
+    highlight: {
+      default: [],
+    },
   },
   methods: {
-    onClick(article) {
+    setActiveArticle(article) {
+      this.active = article.uid;
+    },
+    loadArticle(article) {
+      this.setActiveArticle(article);
       this.$emit('click', article.uid);
+    },
+    theHeader(article) {
+      if (article.title === '') {
+        return this.theTeaser(article.excerpt, 20);
+      }
+      return article.title;
+    },
+    theTeaser(excerpt, length) {
+      if (excerpt.length > length) { return `${excerpt.substring(0, length)} ...`; }
+      return excerpt;
     },
   },
 };
 </script>
 
 <style scoped lang="less">
-.active{
-  background: orange;
+.listArticle {
+  border-left: 0.1em solid white;
+  cursor: grab;
+}
+.highlight {
+  background: white !important;
+  border-color: lightblue;
+}
+.active {
+  background: lightblue !important;
+}
+.teaser {
+  font-size: smaller;
 }
 </style>
