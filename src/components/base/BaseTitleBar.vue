1d3c398fc2e6c111e63031466d8b1f544078192c
Paul <hello@youtag.lu>
2018-10-25 15:34:01 +0200

    improved design

--- src/components/base/BaseTitleBar.vue
+++ src/components/base/BaseTitleBar.vue
@@ -1,12 +1,14 @@
 <template lang="html">
-  <div class="titlebar pb-2 border-bottom mb-4">
-    <div class="titlebar-title">
-      <div class="title">
-          <slot></slot>
+  <div class="titlebar border-bottom mb-2">
+    <div class="titlebar-inner border-bottom">
+      <div class="titlebar-title">
+        <div class="title">
+            <slot></slot>
+        </div>
       </div>
-    </div>
-    <div class="titlebar-actions">
-      <slot name="actions"></slot>
+      <div class="titlebar-actions mb-1">
+        <slot name="actions"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -18,23 +20,22 @@ export default {
 
 <style lang="scss">
 @import "impresso-theme/src/scss/variables.sass";
-
 .titlebar {
+  border-color: $clr-secondary !important;
+}
+.titlebar-inner {
+  border-color: $clr-tertiary !important;
   display: grid;
   grid-template-areas: 'title actions';
   grid-template-columns: auto max-content;
+  margin-bottom: 1px;
   .titlebar-title {
     grid-area: 'title';
     color: $clr-secondary;
+    line-height: 2;
     font-variant: all-small-caps;
     font-weight: bold;
     font-size: 0.9em;
-    display: table;
-    height: 100%;
-    .title{
-      display: table-cell;
-      vertical-align: middle;
-    }
   }
   .titlebar-actions {
     grid-area: 'actions';
