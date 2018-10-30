f8ed0218d3a716d4467dcf717ed07c20d6ae6b89
Paul <hello@youtag.lu>
2018-10-30 14:31:47 +0100

    prepared entities page and routing

---
+++ src/components/EntityPage.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <i-layout id="SearchPage">
+    <i-layout-section width="400px" class="border-right">
+      LEFT PANE
+    </i-layout-section>
+    <i-layout-section>
+      RIGHT PANE
+    </i-layout-section>
+  </i-layout>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="css">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,49 +6,49 @@ import UserLoginPage from '../components/UserLoginPage';
 import UserDashboardPage from '../components/UserDashboardPage';
 import UserCollectionPage from '../components/UserCollectionPage';
 import TestPage from '../components/TestPage';
+import EntityPage from '../components/EntityPage';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: SearchPage,
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: SearchPage,
+  },
+  {
+    path: '/user/login',
+    name: 'login',
+    component: UserLoginPage,
+    meta: {
+      realm: 'user',
     },
-    {
-      path: '/user/login',
-      name: 'login',
-      component: UserLoginPage,
-      meta: {
-        realm: 'user',
-      },
+  },
+  {
+    path: '/user/logout',
+    name: 'logout',
+    component: UserLoginPage,
+    meta: {
+      realm: 'user',
     },
-    {
-      path: '/user/logout',
-      name: 'logout',
-      component: UserLoginPage,
-      meta: {
-        realm: 'user',
-      },
+  },
+  {
+    path: '/user/dashboard',
+    name: 'dashboard',
+    component: UserDashboardPage,
+    meta: {
+      realm: 'user',
     },
-    {
-      path: '/user/dashboard',
-      name: 'dashboard',
-      component: UserDashboardPage,
-      meta: {
-        realm: 'user',
-      },
+  },
+  {
+    path: '/user/collection/:collection_uid?',
+    name: 'collection',
+    component: UserCollectionPage,
+    props: true,
+    meta: {
+      realm: 'user',
     },
-    {
-      path: '/user/collection/:collection_uid?',
-      name: 'collection',
-      component: UserCollectionPage,
-      props: true,
-      meta: {
-        realm: 'user',
-      },
-    },
+  },
     // {
     //   path: '/archive/:archive_id',
     //   component: ArchivePage,
@@ -73,36 +73,45 @@ export default new Router({
     //   name: 'page',
     //   props: true,
     // },
-    {
-      path: '/issue/:issue_uid',
-      component: IssuePage,
-      name: 'issue',
-      props: true,
-      meta: {
-        realm: 'issueviewer',
-      },
+  {
+    path: '/issue/:issue_uid',
+    component: IssuePage,
+    name: 'issue',
+    props: true,
+    meta: {
+      realm: 'issueviewer',
     },
-    {
-      path: '/issue/:issue_uid/page/:page_uid',
-      component: IssuePage,
-      name: 'page',
-      props: true,
-      meta: {
-        realm: 'issueviewer',
-      },
+  },
+  {
+    path: '/issue/:issue_uid/page/:page_uid',
+    component: IssuePage,
+    name: 'page',
+    props: true,
+    meta: {
+      realm: 'issueviewer',
     },
-    {
-      path: '/issue/:issue_uid/page/:page_uid/article/:article_uid',
-      component: IssuePage,
-      name: 'article',
-      props: true,
-      meta: {
-        realm: 'issueviewer',
-      },
+  },
+  {
+    path: '/issue/:issue_uid/page/:page_uid/article/:article_uid',
+    component: IssuePage,
+    name: 'article',
+    props: true,
+    meta: {
+      realm: 'issueviewer',
     },
-    {
-      path: '/playground',
-      component: TestPage,
+  },
+  {
+    path: '/entity/:entity_uid',
+    component: EntityPage,
+    name: 'entity',
+    props: true,
+    meta: {
+      realm: 'entityviewer',
     },
+  },
+  {
+    path: '/playground',
+    component: TestPage,
+  },
   ],
 });
