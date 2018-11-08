f741e3a458d94e319eb074f0f1561989425d625a
Thijs van Beek <thijs@midasweb.nl>
2018-11-08 09:43:44 +0100

    added context to regex filter

--- src/models/FilterRegex.js
+++ src/models/FilterRegex.js
@@ -2,17 +2,20 @@
  * FilterRegex object
  * @param {String} query The search query
  */
+import * as contexts from './Contexts';
 
 export default function FilterRegex({
   query = '',
 } = {}) {
   this.type = 'regex';
   this.query = String(query);
+  this.context = contexts.INCLUDE;
 
   this.getName = () => this.query;
 
   this.getQuery = () => ({
     type: this.type,
     q: this.query,
+    context: this.context,
   });
 }
