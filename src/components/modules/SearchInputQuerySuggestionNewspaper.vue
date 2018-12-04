9323d0da69054e4e02de763182204bbe92694e1d
Daniele Guido <gui.daniele@gmail.com>
2018-12-04 14:47:27 +0100

    create suggestions for mentions and newspaper titles

---
+++ src/components/modules/SearchInputQuerySuggestionNewspaper.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <section v-on:click.prevent="click">
+    <span class="suggestion-icon text-accent-secondary" />
+    <label>newspaper:</label>
+    <span class="serif">{{suggestion.newspaper.name}}
+      (<span class='sans'>{{suggestion.newspaper.startYear}}</span>
+       &mdash;
+      <span class='sans'>{{suggestion.newspaper.endYear}}</span>)
+    </span>
+
+  </section>
+</template>
+
+<script>
+
+export default {
+  model: {
+    prop: 'suggestion',
+  },
+  props: ['suggestion'],
+  methods: {
+    click() {
+      this.$emit('click', this.suggestion);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+</style>
