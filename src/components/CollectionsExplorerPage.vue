65dc7d30288fcdac11142ce967f122c68bb0ec98
Paul <hello@youtag.lu>
2019-01-10 11:48:52 +0100

    added list view to collection explorer

--- src/components/CollectionsExplorerPage.vue
+++ src/components/CollectionsExplorerPage.vue
@@ -1,14 +1,36 @@
 <template lang="html">
   <i-layout-section class="p-3">
-    <h3>Collections Explorer Page</h3>
-    <pre>{{ this.collectionsMerged }}</pre>
-    <!-- <pre>{{ this.collectionAll }}</pre> -->
+    <div class="mx-3">
+      <h1>Collected items</h1>
+      <p>List all items in all collections. (Currently limited to articles)</p>
+      <!-- <pre>{{ this.collectionAll.items.filter(item => (item.labels && item.labels[0] === 'article')) }}</pre> -->
+    </div>
 
-    <div v-if="articles.length > 0" class="collection-group">
-      <h4 class="p-3">{{ articles.length }} articles in <em>all collections</em></h4>
+    <div class="collection-group">
+      <b-container class="px-3 py-2 border-bottom border-top">
+        <b-row>
+          <b-col class="text-muted">
+            <b>{{ $tc('articles', articles.length) }}</b>
+          </b-col>
+          <b-col class="text-right">
+            <label class="mr-2">{{$t("label_display")}}</label>
+            <b-form-radio-group v-model="displayStyle" button-variant="outline-primary" size="sm" buttons>
+              <b-form-radio value="list">{{$t("display_button_list")}}</b-form-radio>
+              <b-form-radio value="tiles">{{$t("display_button_tiles")}}</b-form-radio>
+            </b-form-radio-group>
+          </b-col>
+        </b-row>
+      </b-container>
+
       <b-container fluid>
-        <b-row class="pb-5">
-          <b-col cols="6" sm="12" md="4" lg="3" v-for="(article, index) in articles" v-bind:key="article.uid">
+        <b-row v-if="displayStyle === 'list'">
+          <b-col cols="12" v-for="(article, index) in articles">
+            <search-results-list-item v-on:click="gotoArticle(article)" v-model="articles[index]" />
+          </b-col>
+        </b-row>
+
+        <b-row class="pb-5" v-if="displayStyle === 'tiles'">
+          <b-col cols="6" sm="12" md="4" lg="3" v-for="(article, index) in articles">
             <search-results-tiles-item v-on:click="gotoArticle(article)" v-model="articles[index]" />
             <!-- {{ article }} -->
           </b-col>
@@ -16,21 +38,33 @@
       </b-container>
     </div>
 
+    <pre>{{ this.collectionAll.items }}</pre>
+
   </i-layout-section>
 </template>
 
 <script>
 import Collection from '@/models/Collection';
+import SearchResultsListItem from './modules/SearchResultsListItem';
 import SearchResultsTilesItem from './modules/SearchResultsTilesItem';
 
 export default {
   components: {
+    SearchResultsListItem,
     SearchResultsTilesItem,
   },
   data: () => ({
     collectionsMerged: new Collection(),
   }),
   computed: {
+    displayStyle: {
+      get() {
+        return this.$store.state.search.displayStyle;
+      },
+      set(displayStyle) {
+        this.$store.commit('search/UPDATE_SEARCH_DISPLAY_STYLE', displayStyle);
+      },
+    },
     collections: {
       get() {
         return this.$store.getters['collections/collections'];
@@ -97,3 +131,15 @@ export default {
 
 <style lang="css">
 </style>
+
+<i18n>
+{
+  "en": {
+    "collections": "collections",
+    "label_display": "Display As",
+    "display_button_list": "List",
+    "display_button_tiles": "Tiles",
+    "articles": "No articles | One article | {n} articles"
+  }
+}
+</i18n>
