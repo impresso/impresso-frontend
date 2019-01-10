0efed8d41e618879be2114d429dc4b0117c9a113
Daniele Guido <gui.daniele@gmail.com>
2019-01-10 11:28:36 +0100

    store current topic model filter, plus random links

--- src/store/Topics.js
+++ src/store/Topics.js
@@ -1,47 +1,116 @@
 import * as services from '@/services';
+import Article from '@/models/Article';
 import Topic from '@/models/Topic';
+
 
 export default {
   namespaced: true,
   state: {
     orderBy: '-name',
+    topicModel: '*',
   },
   mutations: {
     UPDATE_ORDER_BY(state, orderBy) {
       state.orderBy = orderBy;
     },
+    UPDATE_TOPIC_MODEL(state, topicModel) {
+      state.topicModel = topicModel;
+    },
   },
   actions: {
-    LOAD_TOPICS() {
+    LOAD_TOPICS_GRAPH() {
       return new Promise((resolve) => {
         services.topics.find({
           query: {
-            limit: 150,
+            limit: 500,
           },
         }).then((results) => {
-          resolve(results.data.map(result => new Topic(result)));
+          // enrich with id
+          const nodes = results.data.map((result) => {
+            const t = new Topic(result);
+            t.id = result.uid;
+            return t;
+          });
+
+          // temporary hack: random connections between nodes
+          const links = [];
+
+          nodes.forEach((t, k) => {
+            // number of connections, 1 to 5:
+            const degree = 1 + Math.round(Math.random() * 4);
+            for (let i = 0; i < degree; i += 1) {
+              const idx = Math.round(Math.random() * (nodes.length - 1));
+              links.push({
+                source: k,
+                target: idx,
+              });
+            }
+          });
+
+          resolve({
+            nodes,
+            links,
+          });
         });
       });
     },
-    LOAD_ARTICLES(context, uid) {
+    LOAD_TOPICS(context, {
+      page = 1,
+      limit,
+      q,
+      facets,
+      filters = [],
+    } = {}) {
+      return new Promise((resolve) => {
+        // get rid of empty
+        const query = {
+          limit,
+          page,
+        };
+
+        if (filters.length) {
+          query.filters = filters;
+        }
+        if (q) {
+          query.q = q;
+        }
+        if (facets) {
+          query.facets = facets;
+        }
+        console.log('topics to load', query);
+
+        services.topics.find({
+          query,
+        }).then((res) => {
+          resolve({
+            ...res,
+            data: res.data.map(d => new Topic(d)),
+          });
+        });
+      });
+    },
+    LOAD_ARTICLES(context, uid, params = {}) {
       return new Promise((resolve) => {
         services.search.find({
           query: {
             group_by: 'articles',
-            limit: 10,
+            page: params.page || 1,
             facets: ['year', 'topic'],
-            // filters: [
-            //   {
-            //     type: 'topic',
-            //     context: 'include',
-            //     q: uid,
-            //   },
-            // ],
+            filters: [
+              {
+                type: 'topic',
+                context: 'include',
+                q: uid,
+              },
+            ],
           },
-        }).then((results) => {
+        }).then((res) => {
           // wrap articles
-          console.log('REULTTTTTT', uid, results);
-          resolve([]);
+          console.log('REULTTTTTT', uid, res);
+          resolve({
+            ...res,
+            data: res.data.map(d => new Article(d)),
+          });
         }).catch((err) => {
           console.log('error', uid, err);
         });
