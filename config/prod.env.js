f1b67da904753173adc6fbf60a1fdb752ad20355
Thijs van Beek <thijs@midasweb.nl>
2019-01-28 14:33:29 +0100

    returned to api on uni lux server

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  MIDDLELAYER_API: process.env.API_BASE_URL || '"https://impresso-project.ch/alpha/api"',
+  MIDDLELAYER_API: process.env.API_BASE_URL || '"https://api-impresso.uni.lu"',
   TYPEKIT_ID: '"egx1cmg"'
 }
