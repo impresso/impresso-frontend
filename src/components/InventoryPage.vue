6305e9aef2b7a8c5e446ecd6ec747ef5a65ec60b
Paul <hello@youtag.lu>
2019-02-04 14:21:45 +0100

    remove loading spinner

--- src/components/InventoryPage.vue
+++ src/components/InventoryPage.vue
@@ -1,10 +1,5 @@
 <template lang="html">
   <i-layout id="inventory">
-    <div id="spinner">
-        <div>
-             <img src="https://www.ajaxload.info/images/exemples/25.gif" />
-        </div>
-    </div>
     <iframe src="https://organicdesignlabs.com/impresso/inventory/" width="100%" height="100%" frameborder="0" />
   </i-layout>
 </template>
