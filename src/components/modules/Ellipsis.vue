f374cb83e1460bd1100427cc1eb93e64d2dd72ad
Daniele Guido <gui.daniele@gmail.com>
2019-02-12 22:22:07 +0100

    remove console

--- src/components/modules/Ellipsis.vue
+++ src/components/modules/Ellipsis.vue
@@ -45,8 +45,7 @@ export default {
   methods: {
     onClick() {
       this.isCollapsed = !this.isCollapsed;
-      console.log('CLICKED fuck', this.$refs.contents);
-
+      
       if (!this.collapsedHeight) {
         this.collapsedHeight = this.$refs.contents.clientHeight;
       }
