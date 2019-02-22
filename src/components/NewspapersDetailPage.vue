cea72b4d454bd306433e7fd0a90aa8638295df52
Daniele Guido <gui.daniele@gmail.com>
2019-02-22 11:52:13 +0100

    cleaner style for newspaperdetailpage component
    - add router for issues
    - use issue thumbnail images
    - refine tabs pills

--- src/components/NewspapersDetailPage.vue
+++ src/components/NewspapersDetailPage.vue
@@ -2,7 +2,7 @@
     <i-layout-section>
 
       <div slot="header" class="border-bottom border-tertiary">
-        <b-navbar type="light" variant="light" class='border-bottom'>
+        <b-navbar type="light" variant="light" >
           <section class='pt-2'>
             <span class="label small-caps">
               <router-link :to="{ name: 'newspapers' }">&larr; {{$t("newspapers")}}</router-link>
@@ -17,23 +17,33 @@
           </section>
 
         </b-navbar>
-        <b-navbar type="light" variant="light">
-        <b-navbar-nav class="p-0 small-caps">
+        <b-navbar type="light" variant="light" class="p-0">
+
+        <b-navbar-nav class="px-2 pt-2 small-caps">
+
           <b-tabs pills>
             <!-- Add your b-tab components here-->
             <template slot="tabs">
-              <b-nav-item :to="{ name:'newspaper'}" exact >{{$t('route.newspaper')}}</b-nav-item>
+              <b-nav-item :to="{ name:'newspaper'}" exact >{{$t('route.newspaper', { total: $n(total) })}}</b-nav-item>
               <b-nav-item :to="{ name:'newspaper_metadata'}" exact >{{$t('route.newspaper_metadata')}}</b-nav-item>
             </template>
           </b-tabs>
         </b-navbar-nav>
         </b-navbar>
       </div>
-      <div class='p-2' v-if='$route.name == "newspaper_metadata"'>
-        <b-table :items="newspaper.properties"
+      <div class='px-3 py-2 ' v-if='$route.name == "newspaper_metadata"'>
+      <b-table bordered borderless caption-top :items="newspaper.properties"
              :fields='["name", "property"]'>
-          <template slot="name" slot-scope="row">{{row.item.name}}</template>
-          <template slot="property" slot-scope="row">{{row.item.newspapers_metadata.value}}</template>
+          <template slot="table-caption">List of known metadata for this newspaper</template>
+          <template slot="name" slot-scope="row" class="small-caps">{{row.item.name}}</template>
+          <template slot="property" slot-scope="row">
+            <div v-if="row.item.isUrl">
+              <a href="row.item.value" target="_blank">&rarr; {{row.item.value}}</a>
+            </div>
+            <div v-else class="bold">
+              {{row.item.value}}
+            </div>
+          </template>
         </b-table>
 
       </div>
@@ -41,27 +51,35 @@
         <div class="p-4">
           <b-row>
             <b-col
-              sm="12" lg="3"
+              sm="12" md="4" lg="3"
               v-for="(issue, i) in issues"
               v-bind:key="i"
               class="mb-4">
-              <div class="border">
-                <div class="p-3 border-bottom">
-                  {{issue.uid}}
-                </div>
-                <image-viewer v-bind:image="issues[i].iiif" />
-              </div>
+
+              <b-card
+                :img-src="issue.iiifThumbnail"
+                :img-alt="$d(new Date(issue.date), 'long')"
+                img-top
+                class="mb-2 border-bottom"
+              >
+
+              <b-card-text class="p-0">
+                <div class=''>{{$d(new Date(issue.date), "long")}}</div>
+                <router-link class='small-caps' :to="{ name: 'issue', params: { issue_uid:issue.uid } }">{{$t("view issue")}}</router-link>
+              </b-card-text>
+              </b-card>
             </b-col>
           </b-row>
         </div>
-        <div slot="footer" class="p-2 border-top">
+          <div class="fixed-pagination-footer p-1 mb-2 m-0">
           <pagination
             v-bind:perPage="limit"
             v-bind:currentPage="page"
             v-bind:totalRows="total"
             v-on:change="onInputPagination"
-            v-bind:showDescription="true" />
-        </div>
+            v-bind:showDescription="false" />
+          </div>
+
       </div>
   </i-layout-section>
 </template>
@@ -86,7 +104,7 @@ export default {
       get() {
         const noteGenealogy = this.newspaper.properties.find(d => d.name === 'noteGenealogy');
         if (noteGenealogy) {
-          return noteGenealogy.newspapers_metadata.value;
+          return noteGenealogy.value;
         }
         return false;
       },
@@ -95,7 +113,7 @@ export default {
       get() {
         const notePublicationDates = this.newspaper.properties.find(d => d.name === 'notePublicationDates');
         if (notePublicationDates) {
-          return notePublicationDates.newspapers_metadata.value;
+          return notePublicationDates.value;
         }
         return false;
       },
@@ -132,6 +150,7 @@ export default {
       immediate: true,
       async handler() {
         this.issues = await this.getIssues();
+        console.log('newpspaperDetailpage', this.issues);
         this.newspaper = await this.getNewspaper();
       },
     },
@@ -146,21 +165,28 @@ export default {
 <style scoped lang="scss">
 @import "impresso-theme/src/scss/variables.sass";
 
+.tabs{
+  margin-bottom: -1px;
+}
 .navbar-light .navbar-nav .nav-link{
   padding: 0.125rem 0.5rem 0.25rem;
-
+  border: 1px solid $clr-primary;
+  border-color: transparent;
   &.active {
-    color: white;
-    background-color: $clr-primary;
+    border-color: $clr-primary;
+    border-bottom-color: white;
+
+    background-color: white;
   }
 }
 </style>
 <i18n>
 {
   "en": {
+
     "route": {
-      "newspaper": "show all first pages",
-      "newspaper_metadata": "show newspaper metadata"
+      "newspaper": "list of {total} first pages",
+      "newspaper_metadata": "newspaper metadata"
     }
   }
 }
