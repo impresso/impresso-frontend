959df8d9c7c3236ac266479158d7bc8fb9a62435
Daniele Guido <gui.daniele@gmail.com>
2019-03-01 09:56:09 +0100

    enable highlight

--- src/models/TopicWord.js
+++ src/models/TopicWord.js
@@ -4,9 +4,11 @@ export default class TopicWord {
     w = '',
     p = 0.0,
     l = 0.0,
+    h = false,
   } = {}) {
     this.w = String(w);
     this.p = parseFloat(p);
     this.l = parseFloat(l);
+    this.h = Boolean(h);
   }
 }
