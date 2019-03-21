53f865072ecc8ba4b634938eada3c822db27c7f0
Paul <hello@youtag.lu>
2019-03-21 15:18:13 +0100

    added exporter service and action

--- src/services/index.js
+++ src/services/index.js
@@ -75,3 +75,4 @@ export const collections = app.service('collections');
 export const collectionsItems = app.service('collectable-items');
 export const topics = app.service('topics');
 export const jobs = app.service('jobs');
+export const exporter = app.service('search-exporter');
