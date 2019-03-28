6b2465b7475821953bab2dcba5d44c20bdd4fdb5
Daniele Guido <gui.daniele@gmail.com>
2019-03-28 17:59:14 +0100

    better (more or less) translations

--- src/components/modules/Toast.vue
+++ src/components/modules/Toast.vue
@@ -10,26 +10,27 @@
     <b-row align-v="center">
       <b-col cols="8">
         <div class="m-1">
+          <div>
+            <span v-if="job.type">{{ $t(`jobs.type.${job.type}`) }}</span>
+            <span v-else>{{ $t(`jobs.type.${job.task}`) }}</span>
+            <b-badge>{{ $t(`jobs.status.${job.status}`) }}</b-badge>
+          </div>
           <div v-if="job.extra">
-            {{ $t(job.extra.task) }} (id:{{job.id}})
-            <b-badge>{{job.extra.job_status}}</b-badge>
+            <span>{{ percent(job.extra.progress) }} &mdash; </span>
+            <span v-if="job.creationDate"> {{$d(new Date(job.creationDate), 'precise')}}</span>
           </div>
           <div v-else>
-            {{ $t(job.task) }} (id:{{job.id}}) <b-badge>{{job.status}}</b-badge>
-          </div>
-          <!-- {{job}} -->
-          <div class="small-caps" v-if="job.creationDate">
-            {{$d(new Date(job.creationDate), 'precise')}}
+            <span >{{ percent(job.progress) }}</span>
           </div>
         </div>
       </b-col>
-      <b-col class="text-right">
+      <b-col cols="4" class="text-right">
         <b-button
           v-if="job.status === 'RUN'"
           variant="outline-danger m-1" size="sm"
           v-on:click="onStopJob(job.id)">Stop
         </b-button>
-        <b-button v-if="job.status === 'DON'" variant="outline-success m-1" size="sm" v-on:click="onExport()">Export</b-button>
+        <b-button v-if="job.status === 'DON' && job.type === 'EXP'" variant="outline-success m-1" size="sm" v-on:click="onExport()">{{ $t('download csv') }}</b-button>
       </b-col>
     </b-row>
   </b-alert>
@@ -41,6 +42,10 @@ import * as services from '@/services';
 export default {
   props: ['job'],
   methods: {
+    percent(n) {
+      const p = Math.min(100, Math.round(n * 10000) / 100);
+      return `${p} %`;
+    },
     onStopJob(id) {
       this.$store.dispatch('jobs/PATCH_JOB', {
         id,
@@ -52,7 +57,7 @@ export default {
     onExport() {
       const anchor = document.createElement('a');
       document.body.appendChild(anchor);
-      const file = `/media/jobs/${this.job.id}`;
+      const file = `${services.MIDDLELAYER_API_URL}/media/jobs/${this.job.id}`;
 
       const headers = new Headers();
       headers.append('Authorization', `Bearer ${services.app.passport.storage['feathers-jwt']}`);
@@ -87,8 +92,21 @@ export default {
 <i18n>
 {
   "en": {
-    "execute_solr_query": "Constructing collection",
-    "store_collectable_items": "Indexing collection items"
+    "jobs": {
+      "type": {
+        "EXP": "export search results as csv",
+        "IDX": "Indexing collection items",
+        "store_collectable_items": "Indexing collection items",
+        "TES": "Echo (TEST)",
+        "test": "Echo (TEST)",
+        "BCQ": "Saving items in your collection",
+        "execute_solr_query": "Saving items in your collection"
+      },
+      "status": {
+        "DON": "done",
+        "RUN": "progress"
+      }
+    }
   }
 }
 </i18n>
