5f19ca6c16523ac493ff2bb1621ce741da631f1f
Thijs van Beek <thijs@midasweb.nl>
2019-04-01 15:10:36 +0200

    added collection filter pill

---
+++ src/components/modules/FilterCollection.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <filter-wrapper
+    v-bind:id="filter.key"
+    v-on:remove="remove"
+    icon="archive"
+    v-bind:title="filter.collection.name">
+    <div slot="settings">
+      <filter-setting-context v-model="filter" />
+    </div>
+  </filter-wrapper>
+</template>
+
+<script>
+import FilterWrapper from './FilterWrapper';
+import FilterSettingContext from './FilterSettingContext';
+
+export default {
+  data: () => ({
+    disabled: true,
+  }),
+  model: {
+    prop: 'filter',
+  },
+  props: ['filter'],
+  methods: {
+    remove() {
+      this.$emit('remove');
+    },
+  },
+  components: {
+    FilterWrapper,
+    FilterSettingContext,
+  },
+};
+</script>
