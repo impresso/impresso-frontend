7680df31cca17372f614135bc0c6ecb6ebf46d88
Daniele Guido <gui.daniele@gmail.com>
2019-04-12 12:18:27 +0200

    simplify searchQuery class

--- src/models/SearchQuery.js
+++ src/models/SearchQuery.js
@@ -5,26 +5,51 @@ import FilterFactory from './FilterFactory';
 
 const uuid = require('uuid');
 
+const filterize = (filter) => {
+  if (typeof filter.getHash !== 'function') {
+    return FilterFactory.create(filter);
+  }
+  return filter;
+};
+
 export default class SearchQuery {
   constructor({
-    filters = [],
-    isFront = false,
-    hasTextContents = true,
+    filters = [
+      { type: 'isFront' },
+      { type: 'hasTextContents' },
+    ],
   } = {}) {
-    this.filters = filters.map(filter => FilterFactory.create(filter));
-    this.isFront = isFront;
-    this.hasTextContents = hasTextContents;
     this.uuid = uuid.v4();
+    this.filterIds = [];
+    this.filters = [];
+    // forEach with hash to avoid dupes
+    filters.forEach(d => this.addFilter(d));
+  }
+
+  addFilter(filter) {
+    const filterItem = filterize(filter);
+    const hash = filterItem.getHash();
+    if (this.filtersIds.indexOf(hash) === -1) {
+      this.filtersIds.push(hash);
+      this.filters.push(filter);
+    }
+  }
+
+  removeFilter(filter) {
+    const idx = this.filtersIds.indexOf(filterize(filter).getHash());
+
+    if (idx !== -1) {
+      this.filtersIds.splice(idx, 1);
+      this.filters.splice(idx, 1);
+    }
   }
 
   getFilters(exclude = []) {
+    // const types = this.filters.map(d => d.type);
     let filters = this.filters.map(filter => filter.getQuery());
 
     filters = filters.filter(i => i);
     filters = filters.filter(i => exclude.includes(i.type) === false);
-
-    if (this.isFront) filters.push({ type: 'isFront' });
-    if (this.hasTextContents) filters.push({ type: 'hasTextContents' });
 
     return filters;
   }
