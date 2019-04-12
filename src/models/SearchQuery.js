68394ccc6ef14b2c3a7e1d7d7f53abfb794ea08d
Daniele Guido <gui.daniele@gmail.com>
2019-04-12 22:48:29 +0200

    add dict (group by type) of active filters

--- src/models/SearchQuery.js
+++ src/models/SearchQuery.js
@@ -1,4 +1,5 @@
-import FilterFactory from './FilterFactory';
+import FilterFactory from '@/models/FilterFactory';
+
 /**
  * @class SearchQuery is an object representing Search Query we can send to the api
  */
@@ -15,13 +16,14 @@ const filterize = (filter) => {
 export default class SearchQuery {
   constructor({
     filters = [
-      { type: 'isFront' },
+      // { type: 'isFront' },
       { type: 'hasTextContents' },
     ],
   } = {}) {
     this.uuid = uuid.v4();
-    this.filterIds = [];
+    this.filtersIds = [];
     this.filters = [];
+    this.filtersIndex = {}; // grouped by type
     // forEach with hash to avoid dupes
     filters.forEach(d => this.addFilter(d));
   }
@@ -31,16 +33,29 @@ export default class SearchQuery {
     const hash = filterItem.getHash();
     if (this.filtersIds.indexOf(hash) === -1) {
       this.filtersIds.push(hash);
-      this.filters.push(filter);
+      this.filters.push(filterItem);
+      if (!Array.isArray(this.filtersIndex[filterItem.type])) {
+        this.filtersIndex[filterItem.type] = [];
+      }
+      this.filtersIndex[filterItem.type].push(filterItem);
     }
   }
 
+  updateFilters(filters) {
+    this.filtersIds = [];
+    this.filters = [];
+    this.filtersIndex = {};
+    filters.forEach(d => this.addFilter(d));
+  }
+
   removeFilter(filter) {
-    const idx = this.filtersIds.indexOf(filterize(filter).getHash());
+    const filterItem = filterize(filter);
+    const idx = this.filtersIds.indexOf(filterItem.getHash());
 
     if (idx !== -1) {
       this.filtersIds.splice(idx, 1);
       this.filters.splice(idx, 1);
+      this.filtersIndex[filterItem.type].splice(idx, 1);
     }
   }
 
@@ -53,4 +68,6 @@ export default class SearchQuery {
 
     return filters;
   }
+
+
 }
