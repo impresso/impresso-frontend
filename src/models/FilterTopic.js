b17e9696e9694e5a7828966d85602929f2dc5cb5
Daniele Guido <gui.daniele@gmail.com>
2019-04-16 23:23:55 +0200

    FilterTopic now extends FilterItems class

--- src/models/FilterTopic.js
+++ src/models/FilterTopic.js
@@ -3,22 +3,29 @@
  * @param {String} query The search query
  */
 import Topic from '@/models/Topic';
-import Filter from '@/models/FilterBase';
+import FilterItems from '@/models/FilterItems';
 
-export default class FilterTopic extends Filter {
+export default class FilterTopic extends FilterItems {
   constructor(args) {
     super(args);
     this.h = String(args.h);
-    this.q = String(args.q);
-    this.query = this.q;
-    this.precision = String(args.precision);
-    this.item = new Topic(args.item);
+    this.range = [0, 1];
+    if (!this.items.length) {
+      this.items = this.q.map(uid => new Topic({
+        uid,
+      }));
+    }
+  }
+
+  setItems(items = []) {
+    this.items = items.map(d => new Topic(d));
   }
 
   getQuery() {
     return {
       type: this.type,
-      q: this.query,
+      q: this.q,
+      r: this.range,
       context: this.context,
     };
   }
