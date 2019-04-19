96dca229c372f604b11b37d313a902547c5532a1
Daniele Guido <gui.daniele@gmail.com>
2019-04-19 11:27:03 +0200

    use filterString instead of suggestion string

--- src/models/SuggestionFactory.js
+++ src/models/SuggestionFactory.js
@@ -1,6 +1,6 @@
 import SuggestionMention from '@/models/SuggestionMention';
 import SuggestionDaterange from '@/models/SuggestionDaterange';
-import SuggestionString from '@/models/SuggestionString';
+import FilterString from '@/models/FilterString';
 import SuggestionRegex from '@/models/SuggestionRegex';
 import SuggestionTopic from '@/models/SuggestionTopic';
 import SuggestionCollection from '@/models/SuggestionCollection';
@@ -16,7 +16,8 @@ export default {
     } else if (data.type === 'daterange') {
       return new SuggestionDaterange(data);
     } else if (data.type === 'string') {
-      return new SuggestionString(data);
+      console.log('SuggestionFactory -> FilterString', data);
+      return new FilterString(data);
     } else if (data.type === 'regex') {
       return new SuggestionRegex(data);
     } else if (data.type === 'topic') {
