4a503a4583323db10837454f5f947ce9cdc9ec5f
Daniele Guido <gui.daniele@gmail.com>
2019-04-26 11:15:50 +0200

    add shadowitems, items that need to be added

--- src/models/FilterItems.js
+++ src/models/FilterItems.js
@@ -15,17 +15,37 @@ export default class FilterItems extends Filter {
   } = {}) {
     super({ context, type, touched, q });
     this.q = String(q).split(',');
+    this.op = 'OR';
     this.items = items;
+    this.shadowItems = [];
+    this.shadowQ = [];
   }
 
   setItems() {
     throw new TypeError(`Filter subclass for ${this.type} must implement setItems() method`);
   }
 
+  addShadowItem(item) {
+    console.log('addShadowItem', item);
+    if (this.shadowQ.indexOf(item.uid) === -1) {
+      this.shadowQ.push(item.uid);
+      this.shadowItems.push(item);
+    }
+  }
+
+  removeShadowItem(item) {
+    const idx = this.shadowQ.indexOf(item.uid);
+    if (idx !== -1) {
+      this.shadowQ.splice(idx, 1);
+      this.shadowItems.splice(idx, 1);
+    }
+  }
+
   getQuery() {
     return {
       type: this.type,
       q: this.q,
+      op: this.op,
       context: this.context,
     };
   }
