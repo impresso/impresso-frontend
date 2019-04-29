e7af69669edbc00c8c0a72581715a94a17ae48cb
Daniele Guido <gui.daniele@gmail.com>
2019-04-29 09:23:19 +0200

    add filterMonitor component

--- src/components/modules/FilterFacet.vue
+++ src/components/modules/FilterFacet.vue
@@ -3,7 +3,6 @@
     <base-title-bar>
       {{$t(`label.${facet.type}.title`)}}
       <div slot="options">
-
         <b-button v-show="filtered" size="sm" variant="outline-primary" @click="resetFilterType">
           {{ $t(`label.${facet.type}.clear`) }}
         </b-button>
@@ -13,43 +12,26 @@
           {{$t(`label.${facet.type}.filtered`)}}
         </span>
         <span v-else>
-          <span v-if="selectedIds.length" v-html="$t(`label.${facet.type}.selected`, {count: selectedIds.length})" />
+          <span v-if="selectedIds.length">
+            <span v-html="$t(`label.${facet.type}.selected`, {count: selectedIds.length})" />
+            <b-button size="sm" variant="outline-primary" @click="applyFilter">
+              {{ $t(`label.${facet.type}.apply`) }}
+            </b-button>
+          </span>
           <span v-else>
             {{$t(`label.${facet.type}.description`)}}
           </span>
         </span>
       </div>
-      <!--
-      <b-button v-on:click="removeFilter" class="float-right" variant="link" size="sm" v-show="facet.touched">
-        <icon name="times" />
-      </b-button> -->
     </base-title-bar>
     <div v-for="(filter, index) in included" :key="index" class="bg-light border p-2">
-      <div v-for="(item, idx) in filter.items" :key="idx">
-        <b-form-checkbox v-model="item.checked" @change="toggleFilterItem($event, filter, item.uid)">
-          <span v-if="facet.type === 'topic'" v-html="item.htmlExcerpt"></span>
-          <span v-if="facet.type === 'newspaper'">{{ item.name }}</span>
-          <span v-if="facet.type === 'language'">{{ $t(`languages.${item.uid}`) }}</span>
-        </b-form-checkbox>
-      </div>
-
-      <div v-if="filter.shadowItems.length">
-        <div v-for="item in filter.shadowItems">
-          <span v-if="facet.type === 'topic'" v-html="item.htmlExcerpt"></span>
-          <span v-if="facet.type === 'newspaper'">{{ item.name }}</span>
-          <span v-if="facet.type === 'language'">{{ $t(`languages.${item.uid}`) }}</span>
-        </div>
-      </div>
-
-      <b-button v-if="filter.touched || filter.shadowItems.length" block size="sm" variant="outline-primary" @click="updateFilter(filter)">
-        {{
-          $t(`label.${facet.type}.update`, {
-            added: filter.shadowItems.length,
-            removed: filter.items.length - filter.q.length  
-          })
-        }}
-      </b-button>
-
+      <filter-monitor v-if='index === included.length - 1' :filter="filter" :type="facet.type" :operators="facet.operators"
+        :items-to-add="selectedItems"
+        @filter-applied="clearSelectedItems"/>
+      <filter-monitor v-else :filter="filter" :type="facet.type" :operators="facet.operators" />
+    </div>
+    <div v-for="(filter, index) in excluded" :key="index" class="bg-light border p-2">
+      <filter-monitor :filter="filter" :type="facet.type" :operators="facet.operators" />
     </div>
 
 
@@ -66,12 +48,13 @@ import 'vue-awesome/icons/times';
 import Icon from 'vue-awesome/components/Icon';
 import BaseTitleBar from './../base/BaseTitleBar';
 import FilterFacetBucket from './FilterFacetBucket';
+import FilterMonitor from './FilterMonitor';
 
 export default {
   data: () => ({
     selectedIds: [],
     selectedItems: [],
-    operators: ['or', 'and', 'not'],
+    operators: ['or', 'and'],
   }),
   props: ['facet'],
   computed: {
@@ -82,31 +65,30 @@ export default {
       if (!this.filtered) {
         return [];
       }
-      const included = this.filtered.filter(d => d.context !== 'exclude');
-
+      const included = this.$store.state.search.search
+        .filtersIndex[this.facet.type]
+        .filter(d => d.context !== 'exclude');
+      // enrich included filter with matching buckets.
       included.forEach((filter) => {
-        filter.q.forEach((d, i) => {
-          // enrich included filter with matching buckets.
-          const bucket = this.facet.buckets.find(b => b.val === d);
+        filter.items.forEach((d, i) => {
+          const bucket = this.facet.buckets.find(b => b.val === d.uid);
 
           if (bucket && filter.items[i]) {
             filter.items[i].count = bucket.count;
-            filter.items[i].checked = true;
-            filter.items[i].pristine = true;
           }
         });
       });
       return included;
     },
     includedIds() {
-      return this.included.reduce((acc, filter) => acc.concat(filter.q), []);
+      return this.included.reduce((acc, filter) => acc.concat(filter.qh), []);
     },
-    // excluded() {
-    //   if (!this.filtered) {
-    //     return [];
-    //   }
-    //   return this.filtered.find(filter => filter.context === 'exclude');
-    // },
+    excluded() {
+      if (!this.filtered) {
+        return [];
+      }
+      return this.filtered.filter(d => d.context === 'exclude');
+    },
     /**
      * List facet buckets NOT included in a filter
      * @return {Array} array of buckets
@@ -115,23 +97,12 @@ export default {
       if (!this.filtered || !this.included) {
         return this.facet.buckets;
       }
+      console.log('unfiltered', this.includedIds);
       return this.facet.buckets.filter(b => this.includedIds.indexOf(b.val) === -1);
     },
   },
   methods: {
     toggleBucket(bucket) {
-      console.log('@toggleBucket', bucket);
-
-      if (bucket.operator === 'or') {
-        if (this.included.length) {
-          if (bucket.checked) {
-            this.included[0].addShadowItem(bucket.item);
-          } else {
-            this.included[0].removeShadowItem(bucket.item);
-          }
-        }
-      }
-      // htere is no active filters.
       const idx = this.selectedIds.indexOf(bucket.val);
       if (idx !== -1 && !bucket.checked) { // remove.
         this.selectedIds.splice(idx, 1);
@@ -140,19 +111,20 @@ export default {
       } else if (idx === -1 && bucket.checked) { // add.
         bucket.checked = true;
         this.selectedIds.push(bucket.val);
-        this.selectedItems.push(bucket);
+        if (bucket.item) {
+          this.selectedItems.push({
+            checked: true,
+            ...bucket.item,
+            count: bucket.count,
+          });
+        } else {
+          this.selectedItems.push(bucket);
+        }
       } // nothing else matters
     },
-    toggleFilterItem(checked, filter, itemUid) {
-      if (!filter.hash) {
-        console.log('storing hash somewhere');
-        filter.hash = filter.getHash();
-      }
-      filter.q = filter.items
-        .filter(d => (d.uid === itemUid ? checked : d.checked))
-        .map(d => d.uid);
-      filter.touched = filter.getHash() !== filter.hash;
-      console.log('toggleFilterItem', filter.touched, filter.q);
+    clearSelectedItems() {
+      this.selectedIds = [];
+      this.selectedItems = [];
     },
     applyFilter() {
       console.log('submit', this.facet.type, this.selectedIds);
@@ -160,11 +132,13 @@ export default {
         type: this.facet.type,
         ids: this.selectedIds,
       });
+      this.clearSelectedItems();
     },
     updateFilter(filter) {
       this.$emit('update-filter', filter);
     },
     resetFilterType() {
+      this.clearSelectedItems();
       this.$emit('reset-filter', this.facet.type);
     },
   },
@@ -172,41 +146,40 @@ export default {
     BaseTitleBar,
     Icon,
     FilterFacetBucket,
+    FilterMonitor,
   },
 };
 </script>
 
 <style scoped lang="less">
 </style>
-
 <i18n>
 {
   "en": {
     "label": {
       "topic": {
         "title": "filter by topic",
-        "update": "appply changes (added: {added}, removed: {removed})",
+        "filtered": "results are filtered when:",
+        "selected": "filter results if <b>one of {count} selected</b> topic applies",
+        "description": "check one or more topics to filter results",
         "clear": "reset"
       },
       "newspaper": {
         "title": "filter by newspaper titles",
+        "filtered": "results are filtered when:",
         "selected": "filter results if they appear in <b>one of {count} selected</b> newspapers",
         "description": "check one or more newspaper to filter results",
-        "clear": "reset"
+        "clear": "reset",
+        "apply": "apply"
       },
       "language": {
         "title": "filter by language of articles",
+        "filtered": "results are filtered when:",
         "selected": "filter results if they are written in <b>one of {count} selected</b> languages",
         "description": "check one or more language to filter results",
         "apply": "apply",
         "clear": "reset"
       }
-    }
-  },
-  "nl": {
-    "label": {
-      "newspaper": "Krant",
-      "language": "Taal"
     }
   }
 }
