08d01a1a193586f500112c2e158c33962ecd7964
Daniele Guido <gui.daniele@gmail.com>
2019-05-07 15:38:21 +0200

    remove debugger

--- src/components/modules/SearchResultsSummary.vue
+++ src/components/modules/SearchResultsSummary.vue
@@ -116,7 +116,6 @@ export default {
         type: 'daterange',
         mapper,
         onExist: () => {
-          debugger;
           this.hasDaterange = true;
         },
       }).join('; ');
