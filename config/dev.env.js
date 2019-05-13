38475f5ab433bbec683544ecbcb6d3e19b392103
Daniele Guido <gui.daniele@gmail.com>
2019-05-13 10:12:13 +0200

    remove prod and dev files

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,16 +1,9 @@
 'use strict'
 const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+const defaultEnv = require('./default.env')
 
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
+module.exports = merge(defaultEnv, {
   TYPEKIT_ID: '"egx1cmg"',
-  // MIDDLELAYER_API: '"https://impresso-project.ch"',
-  MIDDLELAYER_MEDIA_PATH: '"/alpha/media"',
-  MIDDLELAYER_API_PATH: '"/"',
-  MIDDLELAYER_API_SOCKET_PATH: '"/socket.io/"',
-  MIDDLELAYER_API: '"http://localhost:3030"',
-  // MIDDLELAYER_API_SOCKET_PATH: '"/socket.io/"',
   TAWK_TO_SITE_ID: false, // if false then wont show
   GITHUB_WIKI_HOME: '"https://raw.githubusercontent.com/wiki/impresso/impresso-frontend/Home.md"',
 });
