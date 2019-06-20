3d897a75255ef5e052b01c997c752874105357ad
Paul <hello@youtag.lu>
2019-06-20 15:06:00 +0200

    added timeline component with demo data

--- src/components/EntitiesExplorerPage.vue
+++ src/components/EntitiesExplorerPage.vue
@@ -9,21 +9,21 @@
         <h3>List of Entity Timelines</h3>
       </section>
     </b-navbar>
-    <b-card v-for="e in entities.entities" class="m-3 clearfix">
-      <div
-        class="ml-3 float-right"
-        v-if="e.wikidata.images">
-        <img :src="`http://commons.wikimedia.org/wiki/Special:FilePath/${e.wikidata.images[0].value}?width=200px`" width="100">
+    <b-card v-for="e in entities.entities" class="m-3">
+      <div class="clearfix">
+        <div
+          class="ml-3 float-right"
+          v-if="e.wikidata.images">
+          <img :src="`http://commons.wikimedia.org/wiki/Special:FilePath/${e.wikidata.images[0].value}?width=200px`" width="100">
+        </div>
+        <strong>{{e.name}}</strong>
       </div>
-      <strong>{{e.name}}</strong>
       <timeline
-            :contrast="true"
+       class="border"
+            :contrast="false"
             :values="values"
-            :domain="[start, end]"
-            :highlight="highlightA"
-            v-on:highlight="onHighlight($event, 'A')">
+            :domain="[start, end]">
       </timeline>
-
     </b-card>
 
   </i-layout-section>
@@ -34,9 +34,10 @@ import Timeline from './modules/Timeline';
 
 export default {
   data: () => ({
-    values: [],
-    start: 1738,
-    end: 2018,
+    values: [{ t: 1900, w: 19 }, { t: 1901, w: 34 }, { t: 1902, w: 34 },
+    { t: 1903, w: 89 }, { t: 1904, w: 44 }, { t: 1905, w: 34 }],
+    start: 1900,
+    end: 1905,
     highlights: ['A', 'B'],
     highlightA: null,
     highlightB: null,
