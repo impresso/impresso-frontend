4a940d67c4708e6bba01a8d1ac314c993cde30e4
Thijs van Beek <thijs@midasweb.nl>
2019-07-01 12:10:30 +0200

    added groups to faq

--- src/components/HomePageFaq.vue
+++ src/components/HomePageFaq.vue
@@ -3,16 +3,21 @@
     <b-row class="justify-content-md-center mb-5">
       <b-col col xl="6" lg="8" md="10">
         <h1 class="text-center mb-5">{{faq.title}}</h1>
-        <div
-          v-for="(question, i) in faq.questions"
-          v-bind:key="i"
-          class="faq-item pb-1">
-          <div class="question">
-              <strong><a href="#" v-b-toggle="`accordion-${i}`" class="my-3 d-block">{{question.question}}</a></strong>
+
+
+        <div v-for="(group, i) in faq.groups">
+          <h2>{{group.title}}</h2>
+          <div
+            v-for="(question, j) in group.questions"
+            v-bind:key="i"
+            class="faq-item pb-1">
+            <div class="question">
+              <strong><a href="#" v-b-toggle="`accordion-${i}-${j}`" class="my-3 d-block">{{question.question}}</a></strong>
+            </div>
+            <b-collapse class="answer" v-bind:id="`accordion-${i}-${j}`" accordion="my-accordion" role="tabpanel">
+              <div class="p-4 bg-light" v-html="question.answer" />
+            </b-collapse>
           </div>
-          <b-collapse class="answer" v-bind:id="`accordion-${i}`" accordion="my-accordion" role="tabpanel">
-            <div class="p-4 bg-light" v-html="question.answer" />
-          </b-collapse>
         </div>
       </b-col>
     </b-row>
