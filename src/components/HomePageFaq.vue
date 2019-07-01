ed4a097fdccc4a478d1db202262b712145e26e1b
Thijs van Beek <thijs@midasweb.nl>
2019-07-01 10:24:30 +0200

    added dynamic faq content from homepagejson

--- src/components/HomePageFaq.vue
+++ src/components/HomePageFaq.vue
@@ -2,18 +2,16 @@
   <div id="faq-items" class="container py-5">
     <b-row class="justify-content-md-center mb-5">
       <b-col col xl="6" lg="8" md="10">
-        <h1 class="text-center mb-5">FAQ</h1>
+        <h1 class="text-center mb-5">{{faq.title}}</h1>
         <div
-          v-for="i in [1, 2, 3]"
+          v-for="(question, i) in faq.questions"
           v-bind:key="i"
           class="faq-item pb-1">
           <div class="question">
-              <strong><a href="#" v-b-toggle="`accordion-${i}`" class="my-3 d-block">Question {{i}} - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a></strong>
+              <strong><a href="#" v-b-toggle="`accordion-${i}`" class="my-3 d-block">{{question.question}}</a></strong>
           </div>
           <b-collapse class="answer" v-bind:id="`accordion-${i}`" accordion="my-accordion" role="tabpanel">
-            <div class="p-4 bg-light">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+            <div class="p-4 bg-light" v-html="question.answer" />
           </b-collapse>
         </div>
       </b-col>
@@ -22,9 +20,11 @@
 </template>
 
 <script>
-export default {
-  date: () => ({
+import content from '@/assets/homepage.json';
 
+export default {
+  data: () => ({
+    faq: content.faq,
   }),
 };
 </script>
