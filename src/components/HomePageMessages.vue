dba96860a74382c86bbdaaa35539950f52642bf4
Thijs van Beek <thijs@midasweb.nl>
2019-07-08 10:51:27 +0200

    added corpus message to homepage and method to dismiss/close messages

--- src/components/HomePageMessages.vue
+++ src/components/HomePageMessages.vue
@@ -1,9 +1,11 @@
 <template lang="html">
   <div>
     <div
-      v-for="(message, i) in content.messages"
+      v-for="(message, i) in messages"
       v-bind:key="i"
+      v-show="message.show"
       v-bind:class="`bg-${message.type || 'info'}`">
+      <a href="#" class="float-right dripicons dripicons-cross m-4 btn btn-link btn-sm" v-on:click.prevent="close(message)" />
       <div class="container py-4">
         <h4 v-html="message.title" />
         <div v-html="message.body" />
@@ -23,9 +25,23 @@
 import content from '@/assets/homepage.json';
 
 export default {
-  data: () => ({
-    content,
-  }),
+  computed: {
+    messages: {
+      get() {
+        return content.messages.map(message => ({
+          ...message,
+          show: !localStorage.getItem(message.id),
+        }));
+      },
+    },
+  },
+  methods: {
+    close(message) {
+      localStorage.setItem(message.id, true);
+      message.show = false;
+      this.$forceUpdate();
+    },
+  },
 };
 </script>
 
