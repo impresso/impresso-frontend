db6b14b912640ecb083e241fe183ec3f71048379
Daniele Guido <gui.daniele@gmail.com>
2019-07-24 22:37:13 +0200

    the trigger

---
+++ src/components/modules/ItemSelector.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <span class="dripicons-enter" v-on:click.prevent.stop="selectItem($event)"></span>
+</template>
+<script>
+/**
+ * Item selector component: given a specific item, display it on the Monitor component
+ * <item-selector :uid="your-item.uid" :type="person" >
+ */
+export default {
+  props: {
+    uid: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    selectItem() {
+      this.$store.dispatch('monitor/SET_ITEM', {
+        item: {
+          uid: this.uid,
+        },
+        type: this.type,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="css">
+</style>
