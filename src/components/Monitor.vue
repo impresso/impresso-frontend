dea6ddf3fa163ee40a3f0d2551d27b8d7aea108c
Daniele Guido <gui.daniele@gmail.com>
2019-07-26 12:24:20 +0200

    add wikidata block and apply filter method

--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -1,52 +1,62 @@
 <template lang="html">
-  <div v-if="isActive" class="monitor drop-shadow p-2 bg-light" v-on:click.stop :class="{'invisible': isDragging}"
+  <div v-if="isActive" class="monitor drop-shadow bg-light" v-on:click.stop :class="{'invisible': isDragging}"
   draggable="true"
   v-on:dragstart="dragstart($event)"
   v-on:dragend="dragend($event)"
   v-bind:style="transformStyle"
   >
-    <b-tabs pills class="border-bottom">
+    <b-tabs pills class="border-bottom px-2 my-2">
       <template slot="tabs">
         <b-nav-item v-for="t in tabs" :key="t" v-on:click="switchTab(t)" :class="{'active': t === tab}">
           <span v-html="$t(`tabs.${t}`)"/>
         </b-nav-item>
       </template>
     </b-tabs>
-    <div v-if="tab === 'currentSearch'" class="mx-2 pt-2">
+    <div v-if="tab === 'currentSearch'" class="pt-2">
       <search-pills />
     </div>
-    <div v-if="tab === 'selectedItem'" class="mx-2 pt-2">
+    <div v-if="tab === 'selectedItem'" class="pt-2">
       <div v-if="isItemSelected">
-        <h2>{{ item.name || item.uid }} <span class="small-caps">{{ type }}</span></h2>
-        <div v-html="statsLabel"/>
+        <div class="mx-2">
+          <h2>{{ item.name || item.uid }} <span class="small-caps">{{ type }}</span></h2>
 
 
-        <!--  timeline vis -->
-        <div v-if="itemTimelineDomain.length && !monitor.isPendingTimeline" style='position:relative'>
-          <timeline class='bg-light pb-2'
-            :values="itemTimeline"
-            :domain="itemTimelineDomain">
-            <div slot-scope="tooltipScope">
-              <div v-if="tooltipScope.tooltip.item">
-                {{ $d(tooltipScope.tooltip.item.t, 'year') }} &middot;
-                <b>{{ tooltipScope.tooltip.item.w }}</b>
-                <!-- <br />
-                <span class="contrast" v-if="tooltipScope.tooltip.item.w1 > 0">
-                &mdash; <b>{{ percent(tooltipScope.tooltip.item.w1, tooltipScope.tooltip.item.w) }}%</b>
-                ({{ tooltipScope.tooltip.item.w1 }}) {{ contrastLabel }}
-                </span> -->
+
+          <!--  timeline vis -->
+          <div v-if="itemTimelineDomain.length && !monitor.isPendingTimeline" style='position:relative'>
+            <timeline class='bg-light pb-2'
+              :values="itemTimeline"
+              :domain="itemTimelineDomain">
+              <div slot-scope="tooltipScope">
+                <div v-if="tooltipScope.tooltip.item">
+                  {{ $d(tooltipScope.tooltip.item.t, 'year') }} &middot;
+                  <b>{{ tooltipScope.tooltip.item.w }}</b> {{ monitor.groupBy }}
+                  <!-- <br />
+                  <span class="contrast" v-if="tooltipScope.tooltip.item.w1 > 0">
+                  &mdash; <b>{{ percent(tooltipScope.tooltip.item.w1, tooltipScope.tooltip.item.w) }}%</b>
+                  ({{ tooltipScope.tooltip.item.w1 }}) {{ contrastLabel }}
+                  </span> -->
+                </div>
               </div>
-            </div>
-          </timeline>
+            </timeline>
+
+          </div>
           <b-form-group class="mx-3">
             <b-form-checkbox v-model="applyCurrentSearchFilters" v-bind:value="true">
-              {{ $t('labels.applyCurrentSearchFilters') }}
+              {{ $t('labels.applyCurrentSearchFilters') }} <br/> <span v-html="statsLabel"/>
             </b-form-checkbox>
           </b-form-group>
         </div>
-
         <div v-if="monitor.isPending" v-html="$t('loading')" />
-        <div v-else>{{ monitor.item }} </div>
+        <div v-else >
+          <div class="text-center m-2">
+            <b-button size="sm" variant="outline-primary" @click="applyFilter">{{ $t('actions.addToCurrentFilters') }}</b-button>
+          </div>
+          <wikidata-block :item="item" v-if="item.wikidataId" class="p-2"/>
+          <div v-else>
+            {{ item }}
+          </div>
+        </div>
       </div>
       <div v-else>there is no selection.</div>
 
@@ -57,6 +67,7 @@
 <script>
 import SearchPills from './SearchPills';
 import Timeline from './modules/Timeline';
+import WikidataBlock from './modules/WikidataBlock';
 
 export default {
   data: () => ({
@@ -84,6 +95,14 @@ export default {
     },
     fadeOut() {
       return this.$store.dispatch('monitor/SET_IS_ACTIVE', false);
+    },
+    applyFilter() {
+      return this.$store.dispatch('search/ADD_FILTER_TO_CURRENT_SEARCH', {
+        type: this.type,
+        q: [this.item.uid],
+        items: [this.item],
+        checked: true,
+      });
     },
   },
   computed: {
@@ -137,6 +156,7 @@ export default {
   components: {
     SearchPills,
     Timeline,
+    WikidataBlock,
   },
   mounted() {
     document.addEventListener('click', this.fadeOut);
@@ -147,7 +167,7 @@ export default {
 
 <style lang="scss" scoped>
   .monitor {
-    border: 1px solid black;
+    border: 1px solid #343a40;
     position: absolute;
     top: 50%;
     left: 50%;
