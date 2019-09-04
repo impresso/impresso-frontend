50deea763930bafaa7dfccbc7243c099c4f83551
Daniele Guido <gui.daniele@gmail.com>
2019-09-04 13:48:01 +0200

    quick fix for article regions

--- src/components/modules/IssueViewerText.vue
+++ src/components/modules/IssueViewerText.vue
@@ -42,19 +42,21 @@
         </div>
         <div
           v-else
-          class="region-row row mt-3 mb-3 bg-white">
-          <div v-for="(region, i) in article.regions" v-bind:key="i"
-            class="col"
-            :class="{ 'col-sm-7': article.isCC, 'col-sm-12': !article.isCC }">
-            <div class='region py-3'>
-              <!-- {{ i }} -->
-              <p v-for="contents in region.g" >
-                <span v-html="contents"></span>
-              </p>
+          class="region-row mt-3 mb-3 bg-white">
+          <div class="row" v-for="(region, i) in article.regions" v-bind:key="i">
+            <div
+              class="col"
+              :class="{ 'col-sm-7': article.isCC, 'col-sm-12': !article.isCC }">
+              <div class='region py-3'>
+                <!-- {{ i }} -->
+                <p v-for="contents in region.g" >
+                  <span v-html="contents"></span>
+                </p>
+              </div>
             </div>
-          </div>
-          <div v-if="article.isCC" class="col border-left bg-white p-0">
-            <img v-bind:src="region.iiifFragment" width="100%" />
+            <div v-if="article.isCC" class="col  bg-white p-0">
+              <img v-bind:src="region.iiifFragment" width="100%" />
+            </div>
           </div>
         </div>
         <hr class="py-4">
