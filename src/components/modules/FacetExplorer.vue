2ab792556625534fd1d51e8d61ddbf1cfe9a64b8
Daniele Guido <gui.daniele@gmail.com>
2019-10-07 16:18:53 +0200

    simplify facetExplorer format

--- src/components/modules/FacetExplorer.vue
+++ src/components/modules/FacetExplorer.vue
@@ -1,47 +1,6 @@
 <template lang="html">
   <div>
-    <b-dropdown size="sm" variant="outline-primary" class="mb-3">
-      <template slot="button-content">
-        <span v-html="$t(`switchTypes.${type}`)"/>
-      </template>
-      <b-dropdown-item
-        v-for="option in typeOptions"
-        v-bind:active="type === option"
-        v-bind:key="option"
-        v-on:click="changeType(option)"
-      ><span class="small" v-html="$t(`switchTypes.${option}`)"></span></b-dropdown-item>
-    </b-dropdown>
-    <!-- <b-dropdown name="orderBy" :text="$t(`orderBy.${orderBy}`)" size="sm" variant="outline-primary" class="mb-3">
-      <b-dropdown-item
-        v-for="option in orderByOptions"
-        v-bind:active="orderBy === option"
-        v-bind:key="option"
-        v-on:click="changeOrder(option)"
-      ><span class="small" v-html="$t(`orderBy.${option}`)"></span></b-dropdown-item>
-    </b-dropdown> -->
-    <!-- search field OR simple message -->
-    <div v-if="isTypeSearchable">
-      <form v-on:submit.prevent="search()" class="mb-3">
-        <b-input-group>
-          <b-form-input
-          :placeholder="$tc('searchField.placeholder', paginationTotalRows)"
-          v-model.trim="q"
-          autofocus
-          />
-          <b-input-group-append>
-            <b-btn class="pt-2 pb-1 px-2"
-              variant="outline-primary"
-              v-on:click="search">
-              <div class="search-submit dripicons-search"></div>
-            </b-btn>
-          </b-input-group-append>
-        </b-input-group>
-      </form>
-      <div v-html="$t('numbers.results', { results: paginationTotalRows })" />
-    </div>
-    <div v-else v-html="$tc('searchField.notAvailable', paginationTotalRows)"/>
-
-    <div class="mb-1 py-2 border-bottom">
+    <div class="mb-1 pb-2 border-bottom">
       <!-- The Loop -->
       <b-form-checkbox-group v-model="selectedIds" class="position-relative" style="min-height: 4em;">
         <div
@@ -54,6 +13,9 @@
           <item-label v-if="bucket.item" :item="bucket.item" :type="type" />
           <span v-if="bucket.count > -1">( {{ $n(bucket.count) }} )</span>
           <item-selector :uid="bucket.val" :item="bucket.item" :type="type"/>
+          <div class="matches" v-if="bucket.item && bucket.item.matches">
+            <span v-for="match in bucket.item.matches" v-html="match" :key="i"/>
+          </div>
         </b-form-checkbox>
       </b-form-checkbox-group>
     </div>
@@ -86,9 +48,12 @@ import ItemSelector from './ItemSelector';
 export default {
   data: () => ({
     selectedIds: [],
-    q: '',
   }),
   props: {
+    q: {
+      type: String,
+      default: '',
+    },
     store: {
       type: String,
       default: 'search',
@@ -138,6 +103,7 @@ export default {
       },
       set(val) {
         this.$store.dispatch('buckets/CHANGE_PAGE', val);
+        this.$store.dispatch('buckets/LOAD_BUCKETS');
       },
     },
     paginationTotalRows: {
@@ -170,12 +136,15 @@ export default {
       } else {
         this.$store.dispatch('buckets/CHANGE_PAGE', 1);
       }
+      this.$store.dispatch('buckets/LOAD_BUCKETS');
     },
     changeType(type) {
       this.$store.dispatch('buckets/CHANGE_TYPE', type);
+      this.search();
     },
     changeOrder(orderBy) {
       this.$store.dispatch('buckets/CHANGE_ORDER_BY', orderBy);
+      this.search();
     },
     applyFilter() {
       console.info('submit', this.type, this.selectedIds);
@@ -199,6 +168,7 @@ export default {
 </script>
 
 <style scoped lang="less">
+
 .fixed-pagination-footer {
   position: absolute;
   bottom: 0;
