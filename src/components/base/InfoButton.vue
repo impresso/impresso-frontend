40c73c65417437a595d2706462599cef4d8df40f
Paul <hello@youtag.lu>
2019-10-14 15:02:07 +0200

    Added component: <info-button>

---
+++ src/components/base/InfoButton.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <span class="info-button">
+    <span class="icon-link dripicons-information" :id="`ib_${target}`"></span>
+    <b-popover :target="`ib_${target}`" triggers="click" placement="right" class="drop-shadow">
+      <template v-slot:title class="text-bold">{{name}}</template>
+      <div v-html="content" />
+    </b-popover>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['target', 'name', 'content'],
+};
+</script>
+
+<style lang="scss">
+@import "impresso-theme/src/scss/variables.sass";
+
+.arrow {
+  border-color: $clr-secondary;
+}
+.popover-header,
+.popover-body {
+background: $clr-secondary;
+color: $clr-white;
+  a {
+    color: $clr-white;
+    text-decoration: underline;
+  }
+}
+</style>
