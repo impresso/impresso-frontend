36ed3d49e94312f39d6192c57dac3762e33fd1d0
Daniele Guido <gui.daniele@gmail.com>
2019-10-22 10:59:10 +0200

    Update MentionItem.vue

--- src/components/modules/lists/MentionItem.vue
+++ src/components/modules/lists/MentionItem.vue
@@ -1,87 +1,26 @@
 <template lang="html">
   <div class="mention-item item">
-    <div v-if="item.article">
-      <b-row>
-        <!-- <pre>{{item.article}}</pre> -->
-
-        <b-col>
-          <div v-if="item.t">
-            …{{item.t}}…
-          </div>
-          <div v-if="item.fn == null">
-            <span class="small-caps">Function</span>
-            <span>{{item.fn}}</span>
-          </div>
-          <div v-if="item.confidence == null">
-            <span class="small-caps">Confidence</span> <span>o{{$t(`confidence.${item.confidence}`)}}</span>
-          </div>
-          <div v-if="item.demonym == null">
-            <div class="small-caps">demonym</div>
-            {{item.demonym}}
-          </div>
-        </b-col>
-
-        <b-col cols="6">
-          <div v-if="item.article.excerpt.length > 0" class="article-excerpt mb-2">
-            {{item.article.excerpt}}
-          </div>
-
-            <router-link :to="{ name: 'article', params: {
-              issue_uid: item.article.issue.uid,
-              page_uid: item.article.pages[0].uid,
-              article_uid: item.article.uid,
-            } }" class="btn btn-sm btn-outline-primary">
-              {{$t('view')}}
-            </router-link>
-        </b-col>
-
-        <b-col>
-          <div class="article-meta mb-2">
-            <strong v-if="item.article.title" class="mb-1">
-              {{item.article.title}}
-            </strong>
-            <router-link :to="{ name: 'newspaper', params: { newspaper_uid: item.article.newspaper.uid }}">
-            <div class="link">{{item.article.newspaper.name}}</div>
-            </router-link>
-            <p class="small-caps">{{$d(new Date(item.article.date), "compact")}}
-              (p. <span>{{item.article.pages.map(page => page.num).join('; ')}}</span>)
-            </p>
-          </div>
-        </b-col>
-
-      </b-row>
-
-      <div class="w-25">
-
-      </div>
-      <div class="w-50 px-2">
-
-
-
-      </div>
-      <div class="w-25">
-
-        <!-- {{mention}} -->
-      </div>
-    </div>
-    <div v-else>
-      <div v-if="item.t">
-        …{{item.t}}…
-      </div>
-      <div v-if="item.fn">
-        <span class="small-caps">Function</span>
-        <span>{{item.fn}}</span>
-      </div>
-      <div v-if="item.confidence">
-        <span class="small-caps">Confidence</span> <span>{{$t(`confidence.${item.confidence}`)}}</span>
-      </div>
-      <div v-if="item.demonym">
-        <div class="small-caps">demonym</div>
-        {{item.demonym}}
-      </div>
-    </div>
+    <blockquote v-html="item.context" />
+    <b-row>
+      <b-col>
+        <div v-if="item.fn == null">
+          <span class="small-caps">Function</span>
+          <span>{{item.fn}}</span>
+        </div>
+      </b-col>
+      <b-col>
+        <div v-if="item.confidence == null">
+          <span class="small-caps">Confidence</span> <span>o{{$t(`confidence.${item.confidence}`)}}</span>
+        </div>
+      </b-col>
+      <b-col>
+        <div v-if="item.demonym == null">
+          <div class="small-caps">demonym</div>
+          {{item.demonym}}
+        </div>
+      </b-col>
+    </b-row>
   </div>
-  <!-- <pre>{{mention}}</pre> -->
 </template>
 
 <script>
@@ -93,5 +32,8 @@ export default {
 };
 </script>
 
-<style lang="css" scoped>
+<style lang="css">
+blockquote span[ref=match]{
+  background-color: gold;
+}
 </style>
