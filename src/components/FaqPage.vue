8b51a963c11b5a14d4416bd168c842e7fb75aa87
Paul <hello@youtag.lu>
2019-11-07 17:07:47 +0100

    added collapsable, style changes

--- src/components/FaqPage.vue
+++ src/components/FaqPage.vue
@@ -5,14 +5,18 @@
         <h1 class="text-center mb-5">{{faq.title}}</h1>
         <div v-for="(term, i) in faq.faq" class="mb-5">
           <a :id="`${term.id}`"></a>
-          <h3>{{term.title}}</h3>
+          <div class="accordion-toggle" v-b-toggle="`accordion-${i}`">
+            <h4>{{term.title}}</h4>
+            <div class="summary my-1 d-block">{{term.summary}}
+              <div class="text-right small-caps">
+                {{$t("more_info")}} &darr;
+              </div>
+            </div>
+          </div>
           <div class="faq-item pb-1">
-            <div class="summary">
-              <strong class="my-1 d-block">{{term.summary}}</strong>
-            </div>
-            <div class="description py-2">
+            <b-collapse class="description py-2" v-bind:id="`accordion-${i}`" accordion="my-accordion" role="tabpanel">
               <p v-for="paragraph in term.description" v-html="paragraph" />
-            </div>
+            </b-collapse>
           </div>
         </div>
       </b-col>
@@ -41,5 +45,17 @@ export default {
 <style lang="scss">
   img {
     max-width: 100%;
+  }
+  .accordion-toggle {
+    h4 {}
+    cursor: n-resize;
+    &.collapsed {
+      cursor: s-resize;
+    }
+    .summary {
+      background: #eee;
+      padding: 0.1em 0.4em;
+      font-style: normal;
+    }
   }
 </style>
