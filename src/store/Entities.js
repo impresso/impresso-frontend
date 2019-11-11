d684c4a652d37d94d58805a6cd0ee98fa0092797
Daniele Guido <gui.daniele@gmail.com>
2019-11-11 08:10:17 +0100

    use searchFacets service

--- src/store/Entities.js
+++ src/store/Entities.js
@@ -110,23 +110,26 @@ export default {
         return new Entity(res);
       });
     },
-    LOAD_TIMELINE() {
-      return services.search.find({
-        query: {
-          facets: 'year',
-          group_by: 'articles',
-          limit: 0,
-        },
+    LOAD_TIMELINE(context, entityId) {
+      const query = {
+        filters: [{
+          type: 'entity',
+          q: entityId,
+        }],
+        group_by: 'articles',
+      };
+      return services.searchFacets.get('year', {
+        query,
       }).then((res) => {
         console.info('entities/LOAD_TIMELINE success:', res);
-        if (!res.info.facets && !res.info.facets.year) {
-          return [];
-        }
-        const values = res.info.facets.year.buckets.map(bucket => ({
-          t: bucket.val,
-          w: bucket.count,
-        })).sort((a, b) => parseInt(a.t, 10) - parseInt(b.t, 10));
-        return this.$helpers.timeline.addEmptyYears(values);
+        // if (!res.info.facets && !res.info.facets.year) {
+        //   return [];
+        // }
+        // const values = res.info.facets.year.buckets.map(bucket => ({
+        //   t: bucket.val,
+        //   w: bucket.count,
+        // })).sort((a, b) => parseInt(a.t, 10) - parseInt(b.t, 10));
+        // return this.$helpers.timeline.addEmptyYears(values);
       });
     },
   },
