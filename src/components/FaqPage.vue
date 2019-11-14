8c97876a1675221ec9c0690fc83deaf9bad868cd
Paul <hello@youtag.lu>
2019-11-14 16:43:12 +0100

    created groups for faq; improved page format

--- src/components/FaqPage.vue
+++ src/components/FaqPage.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="faq-items" class="container py-5">
-    <b-row class="justify-content-md-center mb-5">
+    <b-row class="justify-content-md-center">
       <b-col col xl="6" lg="8" md="10">
-        <h1 class="text-center mb-5">{{faq.title}}</h1>
-        <div v-for="(term, i) in faq.faq" class="mb-5">
-          <a :id="`${term.id}`"></a>
-          <div class="accordion-toggle" v-b-toggle="`accordion-${i}`">
-            <h4>{{term.title}}</h4>
-            <div class="summary my-1 d-block">{{term.summary}}
-              <div class="text-right small-caps">
-                {{$t("more_info")}} &darr;
-              </div>
+        <h1 class="mb-4">{{faq.title}}</h1>
+        <div v-for="(group, i) in faq.groups" class="my-4">
+          <h2>{{group.title}}</h2>
+          <div v-for="(term, i) in group.faq" class="my-3">
+            <a :id="`${term.id}`"></a>
+            <div class="accordion-toggle" v-b-toggle="`accordion-${i}`">
+              <strong>{{term.title}}</strong>
             </div>
-          </div>
-          <div class="faq-item pb-1">
-            <b-collapse class="description py-2" v-bind:id="`accordion-${i}`" accordion="my-accordion" role="tabpanel">
-              <p v-for="paragraph in term.description" v-html="paragraph" />
-            </b-collapse>
+            <div class="faq-item pb-1">
+              <b-collapse class="description py-2" v-bind:id="`accordion-${i}`" accordion="my-accordion" role="tabpanel">
+                <div class="summary my-1 p-3 bg-light border-left">{{term.summary}}</div>
+                <p v-for="paragraph in term.description" v-html="paragraph" />
+              </b-collapse>
+            </div>
           </div>
         </div>
       </b-col>
@@ -51,11 +50,17 @@ export default {
     cursor: n-resize;
     &.collapsed {
       cursor: s-resize;
+      &::before {
+        content: '+';
+      }
     }
-    .summary {
-      background: #eee;
-      padding: 0.1em 0.4em;
-      font-style: normal;
+    &::before {
+      content: '-';
+      position: absolute;
+      margin-left:-1em;
+      font-size: 1.6em;
+      line-height: 0.7;
+      color: gray;
     }
   }
 </style>
