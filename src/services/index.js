7f0d920948ebac36fb844e47839fa379058314c5
Daniele Guido <gui.daniele@gmail.com>
2019-12-17 14:56:14 +0100

    load version from middle layer service

--- src/services/index.js
+++ src/services/index.js
@@ -112,6 +112,7 @@ app.service('logs').on('created', (payload) => {
 });
 
 // repeat this line for every service in our backend
+export const version = app.service('version');
 export const suggestions = app.service('suggestions');
 export const articles = app.service('articles');
 export const images = app.service('images').hooks(imagesHooks);
