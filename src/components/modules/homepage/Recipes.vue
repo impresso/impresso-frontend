525fae704204ad7c0391d6e17b5bb1a2fd8b169e
Paul <hello@youtag.lu>
2019-12-31 15:25:30 +0100

    styles correction

--- src/components/modules/homepage/Recipes.vue
+++ src/components/modules/homepage/Recipes.vue
@@ -14,7 +14,7 @@
         <h3 class="text-white">{{recipe.caption}}</h3>
         <p>{{recipe.text}}</p>
         <a v-bind:href="recipe.query" class="btn border-primary text-dark bg-light small-caps mr-1" v-if="recipe.query">{{$t('perform_query')}}</a>
-        <a v-bind:href="recipe.video" target="_blank" class="btn bg-dark text-light small-caps">{{$t('watch_video')}}</a>
+        <a v-bind:href="recipe.video" target="_blank" class="btn btn-secondary text-light small-caps">{{$t('watch_video')}}</a>
       </b-col>
     </b-row>
   </div>
