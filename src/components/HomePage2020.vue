12640562156da8907e287a671abd5f96a4bef99c
Daniele Guido <gui.daniele@gmail.com>
2020-01-06 14:58:59 +0100

    add darkmode and lines property

--- src/components/HomePage2020.vue
+++ src/components/HomePage2020.vue
@@ -1,8 +1,8 @@
 <template>
-<i-layout id="HomePage2020">
-  <i-layout-section width="400px" class="">
+<i-layout id="HomePage2020" :class="{ 'pt-1px bg-light border-top': showLines, 'bg-dark border-tertiary': darkMode }" >
+  <i-layout-section width="400px" :class="{ 'mt-1px mr-1px border-top border-right': showLines,'border-tertiary': darkMode }">
     <!--  header -->
-    <div slot="header" class="border-bottom border-secondary bg-dark">
+    <div slot="header" :class="{ 'border-bottom border-secondary': showLines }">
       <search-tabs />
       <div class="py-3 px-3">
         <autocomplete v-on:submit="onSuggestion" />
@@ -10,43 +10,68 @@
     </div>
     <!--  body -->
 
-    <div class="bg-dark text-tertiary p-3 stats">
+    <div class="text-tertiary p-3 stats">
+      <!-- <p>The impresso database is growing day-by-day. Currently there are </p> -->
+      <p class="small-caps mt-3">Impresso data rundown</p>
+      <p>
+        <span class="number">29</span>
+        newspapers collected,<br>
+        <span class="number">1.2</span>
+        billion pages scanned,<br>
+        <span class="number">137</span>
+        million articles identified
+      </p>
+      <p>
+        <span class="number">5</span> languages,<br>
+        <span class="number">6</span> countries of publication<br>
+        <span class="number">5.2</span> named entities disambiguated
+      </p>
 
-      <br>
+      <div class="mb-2">
+        <b-button
+          :variant="showLines  ? 'primary' : 'outline-primary'" size="sm"
+          @click="showLines = !showLines">
+          <div v-if="showLines">{{$t('toggle_lines_on')}}</div>
+          <div v-else>{{$t('toggle_lines_off')}}</div>
+        </b-button>
+      </div>
+      <div>
+        <b-button
+          :variant="darkMode ? 'primary' : 'outline-primary'" size="sm"
+          @click="darkMode = !darkMode">
+          <div class="d-flex flex-row align-items-center">
+            <div class="d-flex dripicons dripicons-brightness-medium mr-2" />
+            <div v-if="darkMode">{{$t('toggle_darkmode_on')}}</div>
+            <div v-else>{{$t('toggle_darkmode_off')}}</div>
+          </div>
+        </b-button>
+      </div>
+      <!-- <br>
       <viz-bar-multi title="Langues" variant="compact" :items="[{name: 'Français', count: 70}, {name: 'Deutsch', count: 20}, {name: 'English', count: 10}]" />
       <viz-bar-multi title="OCR / OLR" variant="compact" :items="[{name: 'OCR', count: 20}, {name: 'OLR', count: 80}]" />
       <viz-bar-multi title="Statistiques" variant="compact" :items="[{name: 'Ravioli', count: 110}, {name: 'Spaghetti', count: 20}, {name: 'Penne', count: 70}]" />
       <viz-bar-multi title="Autres" :items="[{name: 'ita', count: 35}, {name: 'FRançais', count: 60}, {name: 'DEutsch', count: 25}, {name: 'ENglish newsppapa', count: 10}]" />
-      <viz-bar-multi :items="[{name: 'FRançais', count: 20}, {name: 'DEutsch', count: 70}, {name: 'ENglish newsppapa', count: 10}]" />
+      <viz-bar-multi :items="[{name: 'FRançais', count: 20}, {name: 'DEutsch', count: 70}, {name: 'ENglish newsppapa', count: 10}]" /> -->
 
       <br>
-
-      <p>The impresso database is growing day-by-day. Currently there are </p>
-      <p class="small-caps mt-3">Impresso data rundown</p>
-      <b-row>
-        <b-col>
-          <i>29<span>newspapers collected</span></i>
-          <i>1.2<span>billion pages scanned</span></i>
-          <i>137<span>million articles identified</span></i>
-        </b-col>
-        <b-col>
-          <i>5<span>languages <br>written</span></i>
-          <i>6<span>countries <br>covered</span></i>
-          <i>5.2<span>million entities disambiguated</span></i>
-        </b-col>
-      </b-row>
-
     </div>
 
   </i-layout-section>
-  <i-layout-section>
-    <div class="bg-dark text-tertiary p-3">
-      <h1 class="text-white huge">Media Monitoring of the <span class="text-accent">Past</span></h1>
-      <h1 class="text-white mt-5">Today, 100 years ago.</h1>
-      This section lists front pages with the most important events.
-      <h1 class="text-white mt-5">Some examples to get you started</h1>
-      <recipes />
+  <i-layout-section :class="{ 'mt-1px ml-1px border-top border-left': showLines, 'border-tertiary': darkMode }">
+    <div class="text-tertiary p-3">
+      <h1 class="huge m-4 mt-2">Media Monitoring of the <span class="text-accent">Past</span></h1>
+      <!-- <h1 class="text-white mt-5">Today, 100 years ago.</h1>
+      This section lists front pages with the most important events. -->
+
+      <h2 class="p-4 m-0">Mining 200 years <br/>of historical newspapers</h2>
+      <p class="px-4 pb-4">
+        How can newspapers help understand the past? How to explore them?
+        <br>
+        Some examples to get you started!
+      </p>
+      <recipes :dark-mode="darkMode"/>
     </div>
+    <home-page-footer />
   </i-layout-section>
 </i-layout>
 </template>
@@ -56,8 +81,19 @@ import Autocomplete from './Autocomplete';
 import SearchTabs from './modules/SearchTabs';
 import VizBarMulti from './base/VizBarMulti';
 import Recipes from './modules/homepage/Recipes';
+import HomePageFooter from './HomePageFooter';
 
 export default {
+  props: {
+    showLines: {
+      type: Boolean,
+      default: false,
+    },
+    darkMode: {
+      type: Boolean,
+      default: true,
+    },
+  },
   methods: {
     onSuggestion(suggestion) {
       this.$store.commit('search/ADD_FILTER', suggestion);
@@ -75,12 +111,18 @@ export default {
     SearchTabs,
     VizBarMulti,
     Recipes,
+    HomePageFooter,
   },
 };
 </script>
 
 <style lang="scss">
 @import "impresso-theme/src/scss/variables.sass";
+
+#HomePage2020{
+  background-size: cover;
+  height: 100vh;
+}
 .bg-dark {
   ul.nav.nav-pills .nav-item.active .nav-link {
     color: $clr-white;
@@ -91,78 +133,63 @@ export default {
   .btn-primary,
   .input-group > .form-control, .input-group > .custom-select, .input-group > .custom-file {
     color: $clr-white;
-    background-color: $clr-secondary;
-    border-color: $clr-tertiary !important;
+    background-color: #343a40;
+    border-color: $clr-white !important;
     &::placeholder {
-      color: $clr-tertiary;
+      color: $clr-white;
+    }
+    &:hover{
+      background-color: black;
+    }
+  }
+  .btn-outline-primary {
+    border-color: #73777a;
+    color: #ffffff87;
+    &:hover{
+      border-color: $clr-white !important;
+      color: $clr-white;
     }
   }
   .btn-primary {
-    border-width: 0 0 1px 0;
+    // border-width: 0 0 1px 0;
   }
+
   ul.nav.nav-pills {
     border-color: $clr-secondary;
   }
 }
 
-  .stats {
-    i {
-      line-height: 1.05;
-      font-style: normal;
-      align-items: baseline;
-      color: $clr-white;
-      font-size: 500%;
-      padding-bottom: 0.25rem;
-      span {
-        display: block;
-        font-size: 22%;
-        line-height: 1;
-        font-variant: small-caps;
-        text-transform: lowercase;
-        color: $clr-tertiary;
-      }
-    }
+h1.huge {
+  font-size: 800%;
+  line-height: 0.9;
+}
+.stats span.number{
+  color: #343a40;
+}
+
+.bg-dark {
+  h1.huge, h2{
+    color: white;
+  }
+  .stats span.number{
+    color: white;
   }
 
-  .huge {
-    font-size: 800%;
-    line-height: 0.7;
+  &.border-tertiary, .border-tertiary{
+    border-color: #ffffff47 !important;
   }
+}
 
-
-  // .col {
-  // }
-  // div:first-child {
-  //   text-align: right;
-  //   color: $clr-white;
-  //   font-size: 500%;
-  //   span {
-  //     font-size: 30%;
-  //   }
-  // }
-
-
-//   p {
-//     float: right;
-//     width: 50%;
-//   }
-//   span {
-//     float: left;
-//     display: block;
-//     width: 50%;
-//     text-align: right;
-//     margin-right: 0.5rem;
-//     color: $clr-white;
-//     font-size: 200%;
-//   }
-// }
 
 </style>
 
 <i18n>
 {
   "en": {
-
+    "toggle_lines_off": "lines: off",
+    "toggle_lines_on": "lines: on",
+    "toggle_darkmode_off": "dark mode: off",
+    "toggle_darkmode_on": "dark mode: on"
   }
 }
 </i18n>
--- src/components/modules/homepage/Recipes.vue
+++ src/components/modules/homepage/Recipes.vue
@@ -1,22 +1,28 @@
 <template lang="html">
-  <div class="container mb-5">
-    <b-row
-      v-for="(recipe, i) in recipes"
-      v-if="recipe.img_src"
-      v-bind:key="i"
-      class="recipe justify-content-md-center mb-4">
-      <b-col class="image-col">
-        <a v-bind:href="recipe.video" target="_blank">
-          <img v-bind:src="recipe.img_src" class="img-fluid" :title="recipe.query">
-        </a>
-      </b-col>
-      <b-col class="text-col p-3">
-        <h3 class="text-white">{{recipe.caption}}</h3>
-        <p>{{recipe.text}}</p>
-        <a v-bind:href="recipe.query" class="btn border-primary text-dark bg-light small-caps mr-1" v-if="recipe.query">{{$t('perform_query')}}</a>
-        <a v-bind:href="recipe.video" target="_blank" class="btn btn-secondary text-light small-caps">{{$t('watch_video')}}</a>
-      </b-col>
-    </b-row>
+  <div class="recipes">
+    <div class="border-top border-tertiary" v-for="(recipe, i) in recipes" v-bind:key="i">
+      <div class="container mb-5 ">
+        <b-row
+          v-if="recipe.img_src"
+          class="recipe  mb-4 d-flex flex-wrap align-items-center">
+
+          <b-col class="text-col p-3 my-5">
+            <h3 :class="{ 'text-white': darkMode }"><i>{{recipe.caption}}</i></h3>
+            <p>{{recipe.text}}</p>
+            <a v-bind:href="recipe.query" class="btn border-primary text-dark bg-light small-caps mr-1" v-if="recipe.query">{{$t('perform_query')}}</a>
+            <a v-bind:href="recipe.video" target="_blank" class="btn btn-secondary text-light small-caps">{{$t('watch_video')}}</a>
+          </b-col>
+
+          <b-col>
+            <div class="image-col">
+              <a v-bind:href="recipe.video" target="_blank">
+                <img v-bind:src="recipe.img_src" class="img-fluid" :title="recipe.query">
+              </a>
+            </div>
+          </b-col>
+        </b-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +30,9 @@
 import content from '@/assets/homepage.json';
 
 export default {
+  props: {
+    darkMode: Boolean,
+  },
   data: () => ({
     recipes: content.recipes,
   }),
@@ -31,22 +40,6 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "impresso-theme/src/scss/variables.sass";
-
-  .recipe:nth-child(odd) {
-    background-color: $clr-primary;
-  }
-  .recipe:nth-child(even) {
-    // background-color: $clr-secondary;
-    overflow: hidden;
-    .image-col {
-      left: 50%;
-    }
-    .text-col {
-      left: -50%;
-    }
-  }
-
 </style>
 
 <i18n>
