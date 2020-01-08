22e0f6aff5777a91352aa72c6b4bd60fb739e146
Daniele Guido <gui.daniele@gmail.com>
2020-01-08 11:51:22 +0100

    main listener for eventBus ADD_FILTER_TO_SEARCH_QUERY in main component

--- src/App.vue
+++ src/App.vue
@@ -50,10 +50,22 @@ export default {
       return this.$store.state.processingLocked;
     },
   },
+  methods: {
+    onEventBusAddFilter({ filter, searchQueryId }) {
+      console.info('@eventBus.ADD_FILTER_TO_SEARCH_QUERY', searchQueryId, 'filter:', filter);
+      if (!searchQueryId || !searchQueryId.length) {
+        this.$store.dispatch('search/ADD_FILTER', { filter });
+      }
+    },
+  },
   mounted() {
     window.addEventListener('click', () => {
       this.$root.$emit('bv::hide::popover');
     });
+    this.$eventBus.$on(this.$eventBus.ADD_FILTER_TO_SEARCH_QUERY, this.onEventBusAddFilter);
+  },
+  beforeDestroy() {
+    this.$eventBus.$off(this.$eventBus.ADD_FILTER_TO_SEARCH_QUERY, this.onEventBusAddFilter);
   },
   created() {
     // load typekit
