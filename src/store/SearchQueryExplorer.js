db923fc3930bd194a61d8790d320b386d499512c
Daniele Guido <gui.daniele@gmail.com>
2020-01-14 11:30:32 +0100

    reload results every time (to be improved)

--- src/store/SearchQueryExplorer.js
+++ src/store/SearchQueryExplorer.js
@@ -46,34 +46,34 @@ export default {
     },
     GET_CONTEXT_SEARCH_RESULT({ state, commit, dispatch }, { filters, page }) {
       const hash = SearchQuery.serialize({ filters }, 'protobuf');
-      if (state.searchQueryHash === hash && page === state.pagination.currentPage) {
-        console.info('GET_CONTEXT_SEARCH_RESULT: SearchQuery already loaded. Skipping.', hash);
-      } else {
-        commit('SET_IS_LOADING', true);
+      // if (state.searchQueryHash === hash && page === state.pagination.currentPage) {
+      //   console.info('GET_CONTEXT_SEARCH_RESULT: SearchQuery already loaded. Skipping.', hash);
+      // } else {
+      commit('SET_IS_LOADING', true);
+      commit('SET_PAGINATION', {
+        page,
+      });
+      commit('SET_SEARCH_QUERY_HASH', hash);
+      services.search.find({
+        lock: false,
+        query: {
+          filters,
+          group_by: 'articles',
+          page: state.pagination.currentPage,
+          limit: state.pagination.perPage,
+        },
+      }).then((res) => {
         commit('SET_PAGINATION', {
           page,
+          total: res.total,
         });
-        commit('SET_SEARCH_QUERY_HASH', hash);
-        services.search.find({
-          lock: false,
-          query: {
-            filters,
-            group_by: 'articles',
-            page: state.pagination.currentPage,
-            limit: state.pagination.perPage,
-          },
-        }).then((res) => {
-          commit('SET_PAGINATION', {
-            page,
-            total: res.total,
-          });
-          console.info('SearchQueryExplorer/GET_CONTEXT_SEARCH_RESULT', res);
-          dispatch('search/UPDATE_QUERY_COMPONENTS', res.info.queryComponents, { root: true });
-          commit('SET_RESULTS', res.data.map(result => new Article(result)));
-        }).finally(() => {
-          commit('SET_IS_LOADING', false);
-        });
-      }
+        console.info('SearchQueryExplorer/GET_CONTEXT_SEARCH_RESULT', res);
+        dispatch('search/UPDATE_QUERY_COMPONENTS', res.info.queryComponents, { root: true });
+        commit('SET_RESULTS', res.data.map(result => new Article(result)));
+      }).finally(() => {
+        commit('SET_IS_LOADING', false);
+      });
+      // }
     },
   },
 };
