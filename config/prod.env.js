7f95349273e42c279406dcd194f2a637016d1d65
theorm <theorm@gmail.com>
2020-01-20 10:03:17 +0100

    reverted changes in prod config

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,10 +2,11 @@
 module.exports = {
   NODE_ENV: '"production"',
   TYPEKIT_ID: '"egx1cmg"',
-  MIDDLELAYER_API: '"https://impresso-project.ch"',
-  MIDDLELAYER_MEDIA_PATH: '"/app/media"',
-  MIDDLELAYER_API_PATH: '"/"',
-  MIDDLELAYER_API_SOCKET_PATH: '"/socket.io/"',
+  BASE_URL: '"/app"',
+  MIDDLELAYER_API: '""',
+  MIDDLELAYER_MEDIA_PATH: '"/api/media"',
+  MIDDLELAYER_API_PATH: '"/api"',
+  MIDDLELAYER_API_SOCKET_PATH: '"/api/socket.io/"',
   TAWK_TO_SITE_ID: '"5c49872f51410568a1081bed"',
   GITHUB_WIKI_HOME: '"https://raw.githubusercontent.com/wiki/impresso/impresso-frontend/Home.md"',
 };
