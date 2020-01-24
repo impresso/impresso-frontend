76e717e31ff37ad6699622a0792a758f5ba83ea8
Daniele Guido <gui.daniele@gmail.com>
2020-01-24 13:59:31 +0100

    enrich metaproperties

--- src/plugins/MetaTags.js
+++ src/plugins/MetaTags.js
@@ -1,4 +1,4 @@
-const OPENGRAPH_PROPERTIES = [
+const OG_PROPERTIES = [
   'og:title',
   'og:site_name',
   'og:type',
@@ -9,11 +9,18 @@ const OPENGRAPH_PROPERTIES = [
 const DC_PROPERTIES = [
   'DC.title',
   'DC.creator',
+  'DC.type',
+  'DC.identifier',
+  'DC.rights',
+  'DC.language',
+  'DC.publisher',
+  'DC.isPartOf',
   'DCTERMS.issued',
   'DCTERMS.publisher',
 ];
 
 const META_PROPERTY = [
+  'citation_newspaper_title',
   'description',
 ];
 
@@ -32,28 +39,42 @@ const MetaTags = {
     Vue.prototype.$renderMetaTags = ({
       title = 'title',
       // objects { property: content }
-      opengraph = {},
+      og = {},
       // objects { name: content }
       dc = {},
       // objects { name: content }
       meta = {},
+      updateZotero = false,
     } = {}) => {
-      // console.info('$metaTags setting title:', title, description, openGraph);
+      console.info('$metaTags setting title:', title, updateZotero);
       document.title = suffix ? [title, suffix].join(' - ') : title;
-      console.info('Vue.prototype.$renderMetaTag', title);
+      // console.info('Vue.prototype.$renderMetaTag', title);
       META_PROPERTY.forEach((name) => {
+        // console.info('Vue.prototype.$renderMetaTag META_PROPERTY', `meta[name=${name}]`, meta[name]);
         const el = getOrCreate(`meta[name=${name}]`);
-        el.setAttribute('content', meta[name] || '');
+        el.setAttribute('content', meta[name] || 'ciu');
       });
       DC_PROPERTIES.forEach((name) => {
-        const el = getOrCreate(`meta[name=${name}]`);
+        // console.info('Vue.prototype.$renderMetaTag DC_PROPERTIES', `meta[name=${name}]`, dc[name]);
+        const el = getOrCreate(`meta[name="${name}"]`);
         el.setAttribute('content', dc[name] || '');
+        el.setAttribute('name', name);
       });
-      OPENGRAPH_PROPERTIES.forEach((prop) => {
+      OG_PROPERTIES.forEach((prop) => {
+        // console.info('Vue.prototype.$renderMetaTag OG_PROPERTIES', `meta[property=${prop}]`,  og[prop]);
         // <meta property="og:title" content="The Rock" />
-        const el = getOrCreate(`meta[property=${prop}]`);
-        el.setAttribute('content', opengraph[prop] || '');
+        const el = getOrCreate(`meta[property="${prop}"]`);
+        el.setAttribute('content', og[prop] || '');
+        el.setAttribute('property', prop);
       });
+
+
+      if (updateZotero) {
+        document.dispatchEvent(new Event('ZoteroItemUpdated', {
+          bubbles: true,
+          cancelable: true,
+        }));
+      }
     };
   },
 };
