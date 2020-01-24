c5877882d52b3108d7da0835701c189047ce2498
Daniele Guido <gui.daniele@gmail.com>
2020-01-24 15:43:25 +0100

    fix labels on home page

--- src/components/HomePage2020.vue
+++ src/components/HomePage2020.vue
@@ -16,18 +16,22 @@
       <p>
         <span class="number">76</span>
         newspapers collected,<br>
-        <span class="number">5,445,822</span>
-        billion pages scanned,<br>
-        <span class="number">47,876,994</span>
-        million content items identified,<br>
-        <span class="number">12,508,779,241</span>
+        <span class="number"> 600,919</span>
+        issues,<br>
+        <span class="number">5,429,656</span>
+         pages scanned,<br>
+        <span class="number">47,798,468</span>
+         content items identified,<br>
+         <span class="number"> 3,462,799</span>
+          images,<br>
+        <span class="number">12,493,358,703</span>
         words.<br>
       </p>
       <p>
         <span class="number">2</span> countries of publication<br>
         <span class="number">530,086</span> named entities disambiguated
       </p>
-      <p>More? Check on our <a href="https://impresso-project.ch/news/2020/01/05/state-corpus-january2020.html">blog</a></p>
+      <p>More? Check on our <a class="text-white" href="https://impresso-project.ch/news/2020/01/23/state-corpus-january2020.html">blog</a></p>
       <div class="mb-2">
         <b-button
           :variant="showLines  ? 'primary' : 'outline-primary'" size="sm"
