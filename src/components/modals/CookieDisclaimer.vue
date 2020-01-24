b159ee9f06b8c9ea930dfcdef52edc9dc5766869
Daniele Guido <gui.daniele@gmail.com>
2020-01-24 17:49:41 +0100

    Create CookieDisclaimer.vue

---
+++ src/components/modals/CookieDisclaimer.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div class="cookie-disclaimer" v-if="!cookiesAccepted">
+    <div class="d-flex p-4">
+      <div class="flex-grow-1">
+        {{cookiesAccepted}}
+        This website uses cookies to ensure you get the best experience on our website.
+        <a href="https://www.cookiesandyou.com/">Learn more</a>
+      </div>
+      <b-button variant="primary" size="sm" @click="accept()">{{ $t('actions.accept') }}</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    accept() {
+      this.$store.dispatch('settings/ACCEPT_COOKIES');
+    },
+  },
+  computed: {
+    cookiesAccepted() {
+      return this.$store.state.settings.cookiesAccepted;
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.cookie-disclaimer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #FFEB78;
+  z-index: 1090;
+}
+
+.cookie-disclaimer a {
+  text-decoration: underline;;
+}
+</style>
