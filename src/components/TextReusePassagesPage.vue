bf8c43d63d7fbd15ccbdeda029b454671cfd8d2d
Roman Kalyakin <roman@kalyakin.com>
2020-02-04 16:58:43 +0100

    WIP

--- src/components/TextReusePassagesPage.vue
+++ src/components/TextReusePassagesPage.vue
@@ -8,36 +8,93 @@
         :passage="passage"
         class="p-2"/>
     </section>
+    <div class="fixed-pagination-footer p-1 m-0"
+         v-if="paginationTotalRows > paginationPerPage">
+      <pagination
+        :perPage="paginationPerPage"
+        :currentPage="paginationCurrentPage"
+        :totalRows="paginationTotalRows"
+        v-on:change="handlePaginationPageChanged"
+        class="float-left small-caps" />
+    </div>
   </div>
 </template>
 
 <script>
 import ClusterAspectsTab from '@/components/modules/textReuse/ClusterAspectsTab'
 import PassageDetailsPanel from '@/components/modules/textReuse/PassageDetailsPanel'
+import Pagination from './modules/Pagination';
 
 import { textReuseClusterPassages } from '@/services'
+import Newspaper from '@/models/Newspaper'
+
+const PageQueryParameter = 'passagePage'
 
 export default {
   data: () => ({
     passages: [],
+    paginationInfo: {
+      limit: 20,
+      offset: 0,
+      total: 0
+    }
   }),
+  props: {
+    paginationPerPage: {
+      type: Number,
+      default: 20
+    }
+  },
   components: {
     ClusterAspectsTab,
     PassageDetailsPanel,
+    Pagination
   },
   computed: {
     clusterId() {
       return this.$route.query.clusterId
+    },
+    paginationCurrentPage() {
+      const passagePage = this.$route.query[PageQueryParameter] || 0
+      return parseInt(passagePage, 10) + 1
+    },
+    paginationTotalRows() {
+      const { total } = this.paginationInfo
+      return total
     }
   },
   watch: {
     clusterId: {
       async handler() {
-        this.passages = await textReuseClusterPassages
-          .find({ query: { clusterId: this.clusterId }})
-          .then(result => result.passages)
+        return this.executeSearch()
       },
       immediate: true
+    },
+    paginationCurrentPage: {
+      async handler() {
+        return this.executeSearch()
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    handlePaginationPageChanged(page) {
+      const { query } = this.$route
+      const updatedQuery = Object.assign({}, query, { [PageQueryParameter]: page - 1 })
+      this.$router.replace({ query: updatedQuery }).catch(() => {})
+    },
+    async executeSearch() {
+      [this.passages, this.paginationInfo] = await textReuseClusterPassages
+        .find({ query: { clusterId: this.clusterId }})
+        .then(({ passages, info }) => {
+          return [
+            passages.map(passage =>({
+              ...passage,
+              newspaper: new Newspaper(passage.newspaper)
+            })),
+            info
+          ]
+        })
     }
   }
 }
--- src/components/modules/textReuse/PassageDetailsPanel.vue
+++ src/components/modules/textReuse/PassageDetailsPanel.vue
@@ -1,16 +1,71 @@
 <template>
-  <div>
-    {{passage.id}}
+  <div class="d-flex flex-column">
+    <!-- image and text -->
+    <div class="d-flex flex-row">
+      <div class="image">
+        [image {{passage.pageRegions}}]
+      </div>
+      <div class="content">
+        {{passage.content}}
+      </div>
+    </div>
+    <!-- title and meta -->
+    <div class="d-flex flex-column">
+      <h2>
+        <router-link
+          :to="{name: 'article', params: articleParams}">
+          {{ passage.title }}
+        </router-link>
+      </h2>
+      <!-- TODO: extract this and same code in ArticleItem into a separate component -->
+      <div class="article-meta">
+        <router-link :to="{ name: 'newspaper', params: { newspaper_uid: passage.newspaper.uid }}" class="article-newspaper">
+          {{ passage.newspaper.name }}
+        </router-link>
+        <item-selector :uid="passage.newspaper.uid" :item="passage.newspaper" type="newspaper"/> &nbsp;
+        <span class="date">{{ $d(new Date(passage.date), "long") }}</span>
+        <span>{{ pages }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ItemSelector from '../ItemSelector';
+import { getArticleParameters } from '@/logic/ids'
+
 export default {
   props: {
     passage: {
       type: Object,
       required: true
     }
+  },
+  components: {
+    ItemSelector
+  },
+  computed: {
+    articleParams() {
+      const { articleId, pageNumbers } = this.passage
+      const firstPageNumber = pageNumbers[0]
+      return getArticleParameters(articleId, firstPageNumber)
+    },
+    pages() {
+      return this.$tc(
+        'pp',
+        this.passage.pageNumbers.length,
+        { pages: this.passage.pageNumbers.join(',') }
+      );
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .content {
+    flex-basis: 60%;
+  }
+  .image {
+    flex-basis: 40%;
+  }
+</style>
