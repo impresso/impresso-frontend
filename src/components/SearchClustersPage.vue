d0d9d77e16bbebe655fe2a34200a96b68f569b70
Roman Kalyakin <roman@kalyakin.com>
2020-02-17 13:34:21 +0100

    Merge

---
+++ src/components/SearchClustersPage.vue
@@ -0,0 +1,191 @@
+<template lang="html">
+  <i-layout id="ConnectedTextReuseClustersPage">
+    <i-layout-section width="400px" class="border-right">
+      <div slot="header" class="border-bottom p-2 pb-4">
+	<cluster-text-search-panel
+	  @submit="handleSearchInputSubmitted"
+	  :value="searchText"/>
+      </div>
+      <div :class="`pl-1 pr-2 mt-2 mb-2 d-flex flex-row \
+		  ${isClusterSelected(item.cluster.id) ? 'active' : ''} \
+		  ${isLastItem(index, clusterItems.length) ? 'pb-4' : ''}`"
+	   v-for="(item, index) in clusterItems"
+	   :key="item.cluster.id"
+	   v-on:click="handleClusterSelected(item.cluster.id)">
+	<span class="d-flex align-self-stretch flex-shrink-0 selection-indicator"/>
+	<cluster-details-panel
+	  class="pl-2 pt-1 pr-3 pb-2 details-panel"
+	  :cluster="item.cluster"
+	  :textSample="item.textSample"/>
+      </div>
+      <div class="fixed-pagination-footer p-1 m-0">
+	<pagination
+	  :perPage="paginationPerPage"
+	  :currentPage="paginationCurrentPage"
+	  :totalRows="paginationTotalRows"
+	  v-on:change="handlePaginationPageChanged"
+	  class="float-left small-caps" />
+      </div>
+    </i-layout-section>
+    <i-layout-section class="pt-2">
+      <div v-if="selectedCluster !== undefined">
+	<!-- main header -->
+	<b-navbar>
+	  <section>
+	    <span class="label small-caps">
+	      <span>&larr; {{$t("clustersLabel")}}</span>
+	    </span>
+	    <h3>{{$t('clusterLabel')}} #{{selectedCluster.id}}</h3>
+	  </section>
+	</b-navbar>
+	<!-- main page -->
+	<router-view />
+      </div>
+
+      <div v-if="selectedCluster === undefined" style="height: 100%">
+	<div class="d-flex flex-row justify-content-center" style="height: 100%">
+	  <div class="d-flex flex-column justify-content-center">
+	    <span>[no cluster selected placeholder]</span>
+	  </div>
+	</div>
+      </div>
+    </i-layout-section>
+  </i-layout>
+</template>
+
+<script>
+import ClusterTextSearchPanel from '@/components/modules/textReuse/ClustersSearchPanel'
+import ClusterDetailsPanel from '@/components/modules/textReuse/ClusterDetailsPanel'
+import Pagination from './modules/Pagination';
+import { textReuseClusters } from '@/services';
+
+const isLastItem = (index, total) => total - 1 === index
+
+export default {
+  data: () => ({
+    clusterItems: [],
+    searchInfo: {
+      limit: 20,
+      offset: 0,
+      total: 0
+    }
+  }),
+  props: {
+    paginationPerPage: {
+      type: Number,
+      default: 20
+    }
+  },
+  components: {
+    ClusterTextSearchPanel,
+    ClusterDetailsPanel,
+    Pagination,
+  },
+  methods: {
+    isClusterSelected(clusterId) {
+      return clusterId === this.selectedClusterId;
+    },
+    handleClusterSelected(clusterId) {
+      const { query } = this.$route
+      const updatedQuery = Object.assign({}, query, { clusterId })
+      this.$router.replace({ query: updatedQuery }).catch(() => {})
+    },
+    handleSearchInputSubmitted(searchText) {
+      if (searchText !== '') this.$router.push({ query: { q: searchText } }).catch(() => {})
+    },
+    handlePaginationPageChanged(page) {
+      const { query, params } = this.$route
+      const updatedQuery = Object.assign({}, query, { page: page - 1 })
+      this.$router.replace({ params, query: updatedQuery }).catch(() => {})
+    },
+    async executeSearch() {
+      const pageNumber = this.paginationCurrentPage - 1
+
+      if (!this.searchText) {
+	this.clusterItems = []
+	this.searchInfo = {
+	  limit: this.paginationPerPage,
+	  offset: 0,
+	  total: 0
+	}
+      } else {
+	[this.clusterItems, this.searchInfo] = await textReuseClusters
+	  .find({ query: {
+	    text: this.searchText,
+	    skip: this.paginationPerPage * pageNumber,
+	    limit: this.paginationPerPage
+	  }})
+	  .then(result => {
+	    return [result.clusters, result.info]
+	  })
+      }
+    },
+    isLastItem
+  },
+  computed: {
+    selectedClusterId() {
+      return this.$route.query.clusterId
+    },
+    searchText() {
+      return this.$route.query.q
+    },
+    paginationCurrentPage() {
+      const { page = 0 } = this.$route.query
+      return parseInt(page, 10) + 1
+    },
+    paginationTotalRows() {
+      const { total } = this.searchInfo
+      return total
+    },
+    selectedCluster() {
+      const filteredClusters = this.clusterItems
+	.filter(({ cluster }) => cluster.id === this.selectedClusterId)
+      return filteredClusters.length > 0 ? filteredClusters[0].cluster : undefined
+    }
+  },
+  watch: {
+    searchText: {
+      async handler() {
+	return this.executeSearch()
+      },
+      immediate: true
+    },
+    paginationCurrentPage: {
+      async handler() {
+	return this.executeSearch()
+      },
+      immediate: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "impresso-theme/src/scss/variables.sass";
+
+  .active {
+    .selection-indicator {
+      background-color: $clr-accent-secondary;
+    }
+    .details-panel {
+      background-color: $clr-bg-secondary;
+    }
+  }
+
+  .selection-indicator {
+    width: 0.25em;
+  }
+
+  .details-panel {
+    cursor: pointer;
+  }
+</style>
+
+<i18n>
+{
+  "en": {
+    "clustersLabel": "Text Reuse Clusters",
+    "clusterLabel": "Text Reuse Cluster"
+  }
+}
+</i18n>
