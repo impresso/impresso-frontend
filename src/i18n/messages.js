2944c30ab226bcc8c02fb30bff70c8a67832a852
Daniele Guido <gui.daniele@gmail.com>
2020-03-05 11:18:23 +0100

    fix translation count with no items

--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -99,7 +99,7 @@ export default {
       articles: 'Search in <span class="number">{count}</span> published articles',
     },
     numbers: {
-      options: '| (1 option) | ({n} options)',
+      options: ' | (1 option) | ({n} options)',
       items: '0|<span class="number">1</span> term | <span class="number">{n}</span> terms',
       articles: 'no articles | <span class="number">1</span> article | <span class="number">{n}</span> articles',
       images: 'no images | <span class="number">1</span> image | <span class="number">{n}</span> images',
