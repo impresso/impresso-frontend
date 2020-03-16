a815c2487368fca9a065be3036185983c1acabaf
Paul <hello@youtag.lu>
2020-03-16 18:44:19 +0100

    added component modal

---
+++ src/components/modals/CopyToClipboard.vue
@@ -0,0 +1,83 @@
+<template lang="html">
+
+  <b-modal hide-footer visible v-bind:title="$t('modal_title_share_article')">
+
+    <iframe :src="widgetLink"
+      class="widgetLink border">
+      <p>Your browser does not support iframes.</p>
+    </iframe>
+
+    <b-input-group prepend="URL">
+      <b-form-input id="inputLink" ref="inputLink" readonly v-model="widgetLink" />
+      <template v-slot:append>
+        <b-button
+          variant="outline-primary" size="sm"
+          v-on:click="copyArticleUrlToClipboard()"
+          >{{ $t('copy_to_clipboard') }}
+        </b-button>
+      </template>
+    </b-input-group>
+
+  </b-modal>
+
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'article',
+  },
+  props: ['article'],
+  computed: {
+    widgetLink() {
+      // const a = document.createElement('a');
+      // a.href='.';
+      // const baseURL = a.href;
+      const baseURL = window.location.origin + '/';
+      return baseURL + `widget/#/p/${this.article.pages[0].uid}/a/${this.article.uid}/?&backgroundSize=contain&backgroundColor=#eeeeee&cssFilter=contrast-1.5&overlayBackgroundColor=rgba(86,204,242,0.2)`;
+    },
+  },
+  mounted() {
+    this.$root.$on('bv::modal::hidden', () => {
+      this.$emit('closed');
+    })
+  },
+  methods: {
+    copyArticleUrlToClipboard() {
+
+      this.$refs.inputLink.select();
+      document.execCommand('copy');
+
+      const url = window.getSelection();
+
+      const title = this.$t('url_copied_title');
+      const message = this.$tc('url_copied_message', url);
+
+      this.$bvToast.toast(message, {
+        title: title,
+        variant: 'success',
+        toaster: 'b-toaster-bottom-center',
+        solid: true,
+      })
+
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .widgetLink {
+    width:100%;
+    height: 400px;
+  }
+</style>
+
+<i18n>
+{
+  "en": {
+    "copy_to_clipboard" : "Copy to clipboard",
+    "url_copied_title" : "URL copied to clipboard",
+    "url_copied_message" : "{n} copied to clipboard"
+  }
+}
+</i18n>
