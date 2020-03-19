044c70429b7334fa00a2a704f2922c1ec0b7ae92
Daniele Guido <gui.daniele@gmail.com>
2020-03-19 22:31:18 +0100

    prevent error when change page (when indeed, "article" is null.)

--- src/components/modules/TableOfContents.vue
+++ src/components/modules/TableOfContents.vue
@@ -59,7 +59,7 @@
             </div>
             <div class="ml-3 mb-3"
               v-if="art.uid === selectedArticleUid
-                && article.accessRight === 'OpenPublic'">
+                && art.accessRight === 'OpenPublic'">
               <b-button
                 variant="outline-success" size="sm"
                 v-on:click.prevent="showModalShareArticle()"
