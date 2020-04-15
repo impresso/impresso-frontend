ab6c593574352b8d388789b00c75768edaf7976b
Daniele Guido <gui.daniele@gmail.com>
2020-04-15 13:07:32 +0200

    get and propagate the searchQuery

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -24,7 +24,9 @@
               </span>
             </span>
           </template>
-          <b-button class="ml-3 my-2" size="sm" variant="outline-primary outline-primary-contrast" :disabled="$route.name === 'search'" v-bind:to="{ name: 'search', query: currentSearchQueryParams }">
+          <b-button class="ml-3 my-2" size="sm" variant="outline-primary outline-primary-contrast"
+            :disabled="$route.name === 'search'"
+            :to="getRouteWithSearchQuery({ name: 'search' })">
             {{$t('actions.searchMore')}}
           </b-button>
           <!-- <b-button class="ml-2 my-2" size="sm" variant="outline-primary bg-light" v-bind:to="{ name: 'search' }">
@@ -157,13 +159,15 @@ import Icon from 'vue-awesome/components/Icon';
 import Toast from './modules/Toast';
 import Pagination from './modules/Pagination';
 import SearchQueryExplorer from './modals/SearchQueryExplorer';
+import { searchQueryGetter } from '@/logic/queryParams';
 
 Icon.register({slack:{width:1664,height:1792,d:'M1519 776q62 0 103.5 40.5t41.5 101.5q0 97-93 130l-172 59 56 167q7 21 7 47 0 59-42 102t-101 43q-47 0-85.5-27t-53.5-72l-55-165-310 106 55 164q8 24 8 47 0 59-42 102t-102 43q-47 0-85-27t-53-72l-55-163-153 53q-29 9-50 9-61 0-101.5-40t-40.5-101q0-47 27.5-85t71.5-53l156-53-105-313-156 54q-26 8-48 8-60 0-101-40.5t-41-100.5q0-47 27.5-85t71.5-53l157-53-53-159q-8-24-8-47 0-60 42-102.5t102-42.5q47 0 85 27t53 72l54 160 310-105-54-160q-8-24-8-47 0-59 42.5-102t101.5-43q47 0 85.5 27.5t53.5 71.5l53 161 162-55q21-6 43-6 60 0 102.5 39.5t42.5 98.5q0 45-30 81.5t-74 51.5l-157 54 105 316 164-56q24-8 46-8zM725 1038l310-105-105-315-310 107z'}})
 
+
 export default {
-  props: {
-    searchQuery: Object,
-  },
+  // props: {
+  //   searchQuery: Object,
+  // },
   data: () => ({
     languages: {
       de: {
@@ -200,6 +204,7 @@ export default {
     }
   },
   computed: {
+    searchQuery: searchQueryGetter(),
     loginRouteParams() {
       return {
         name: 'login',
@@ -213,11 +218,6 @@ export default {
     },
     countActiveItems() {
       return this.searchQuery.countActiveItems();
-    },
-    currentSearchHash() {
-      return this.searchQuery.getSerialized({
-        serializer: 'protobuf',
-      });
     },
     jobs() {
       return this.$store.state.jobs.items;
@@ -233,9 +233,6 @@ export default {
     },
     activeLanguageCode() {
       return this.$store.state.settings.language_code;
-    },
-    currentSearchQueryParams() {
-      return this.$store.state.search.search.getSerialized();
     },
     showAlert() {
       return this.$store.state.errorMessages.length > 0;
@@ -314,11 +311,12 @@ export default {
       });
     },
     getRouteWithSearchQuery(route) {
+      const qs = this.searchQuery.getSerialized({ serializer: 'protobuf' });
       return {
         ...route,
         query: {
           ...route.query,
-          sq: this.currentSearchHash,
+          qs,
         },
       };
     },
