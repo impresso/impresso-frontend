b966e7847de781123f6d32c5c7d05781982431ab
PaulSchroeder <32929425+PaulSchroeder@users.noreply.github.com>
2020-06-09 16:28:32 +0200

    update logic to add/remove entity suggestions

--- src/components/modules/EntitiesSuggestionsList.vue
+++ src/components/modules/EntitiesSuggestionsList.vue
@@ -1,60 +1,60 @@
 <template>
   <div class="reduced">
-    <!-- {{entitiesToAdd}} -->
     <spinner v-if="isLoading" class="text-center pt-2" />
     <div v-else v-for="entity in suggestedEntities" :key="entity.uid">
-      <div v-if="!entitiesToAdd.includes(entity)">
-        <div class="d-flex p-2 align-items-center">
-          <div :class="`px-2 dripicons-${getEntityIcon(entity)}`" />
-          <div class="px-2 mr-auto w-100">
-            <div>
-              {{ entity.name }}
-              <item-selector :uid="entity.uid" :item="entity" :type="entity.type"/>
-            </div>
-            <small>{{ $tc('items', entity.countItems)}}; {{$tc('mentions', entity.countMentions)}}</small>
-            <!-- <div class="">
-              <div class="viz-bars">
-                <small>{{entity.countItems}} items</small>
-                <div class="bg-white">
-                  <div class="bg-tertiary viz-bar"
-                  :title="`${entity.countItems} items`"
-                  :style="`width:${entity.countItems/maxItems * 100}%;`" />
-                </div>
-                <small>{{entity.countMentions}} mentions</small>
-                <div class="bg-white">
-                  <div class="bg-medium viz-bar"
-                  :title="`${entity.countMentions} $t('mentions')`"
-                  :style="`width:${entity.countMentions/maxMentions * 100}%;`" />
-                </div>
+      <div class="">
+        <div :class="`border-bottom ${entitiesToAdd.includes(entity) ?
+          'bg-accent-secondary-light border-white' : ''}`">
+
+          <div class="d-flex p-2 align-items-center">
+            <div :class="`pr-2 dripicons-${getEntityIcon(entity)}`" />
+            <div class="mr-auto w-100">
+              <div>
+                {{ entity.name }}
+                <item-selector :uid="entity.uid" :item="entity" :type="entity.type"/>
               </div>
-            </div> -->
+              <small>{{ $tc('items', entity.countItems)}}; {{$tc('mentions', entity.countMentions)}}</small>
+              <!-- <div class="">
+                <div class="viz-bars">
+                  <small>{{entity.countItems}} items</small>
+                  <div class="bg-white">
+                    <div class="bg-tertiary viz-bar"
+                    :title="`${entity.countItems} items`"
+                    :style="`width:${entity.countItems/maxItems * 100}%;`" />
+                  </div>
+                  <small>{{entity.countMentions}} mentions</small>
+                  <div class="bg-white">
+                    <div class="bg-medium viz-bar"
+                    :title="`${entity.countMentions} $t('mentions')`"
+                    :style="`width:${entity.countMentions/maxMentions * 100}%;`" />
+                  </div>
+                </div>
+              </div> -->
+            </div>
+            <b-button
+              v-if="!entitiesToAdd.includes(entity)"
+              size="sm"
+              :title="$t('actions.addToCurrentFilters')"
+              variant="outline-primary"
+              style="min-width: 2em"
+              @click.prevent.stop="handleEntityClicked(entity)">
+              +
+            </b-button>
+            <b-button
+              v-else
+              size="sm"
+              :title="$t('actions.removeFromCurrentFilters')"
+              variant="outline-primary"
+              style="min-width: 2em"
+              @click.prevent.stop="handleEntityClicked(entity)">
+              -
+            </b-button>
           </div>
-          <b-button
-            size="sm"
-            :title="$t('actions.addToCurrentFilters')"
-            variant="outline-primary"
-            style="padding-right: 0.75em"
-            @click.prevent.stop="handleEntityClicked(entity)">
-            +
-          </b-button>
+
         </div>
-        <div v-if="entity !== suggestedEntities[suggestedEntities.length - 1]" class="border-bottom mx-2" />
+        <!-- <div v-if="entity !== suggestedEntities[suggestedEntities.length - 1]" class="border-bottom my-0" /> -->
       </div>
     </div>
-    <div class="border-bottom border-tertiary" />
-        <!-- <b-col cols="10" class="text-align-left">
-        <b-badge>{{ entity.name }}</b-badge>
-        <b-button
-          variant="link"
-          class="text-align-left suggest-button"
-          @click="handleEntityClicked(entity)">
-          {{ entity.name }}
-        </b-button>
-
-      </b-col>
-      <b-col cols="2" class="text-align-right align-self-center">
-        <span :class="`dripicons-${getEntityIcon(entity)}`"/>
-      </b-col> -->
   </div>
 </template>
 
