df4f42d48c5a74a952a7a2e41ea74e70f98ea2a9
Daniele Guido <gui.daniele@gmail.com>
2020-06-15 16:29:45 +0200

    clean component

--- src/components/EntitiesExplorerPage.vue
+++ src/components/EntitiesExplorerPage.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <i-layout-section>
+  <i-layout-section main>
     <b-navbar type="light" variant="light" class="border-bottom" slot="header">
       <section>
         <!-- <pre>{{entities}}</pre> -->
@@ -9,24 +9,6 @@
         <h3>List of Entity Timelines</h3>
       </section>
     </b-navbar>
-    <router-link v-for="(e, idx) in entities.entities"
-      v-bind:key="idx"
-      v-bind:to="{name: 'entity', params: {entity_id: e.id}}">
-      <b-card class="m-3">
-        <div class="clearfix">
-          <div
-            class="ml-3 float-right"
-            v-if="e.wikidata.images">
-            <img :src="`http://commons.wikimedia.org/wiki/Special:FilePath/${e.wikidata.images[0].value}?width=200px`" width="100">
-          </div>
-          <p v-if="e.wikidata && e.wikidata.labels">
-            <strong>{{ e.wikidata.labels[activeLanguageCode] }}</strong>
-            <em>{{ e.wikidata.descriptions[activeLanguageCode] }}</em>
-          </p>
-          <p v-else><strong>{{e.name}}</strong></p>
-        </div>
-      </b-card>
-    </router-link>
   </i-layout-section>
 </template>
 
@@ -34,14 +16,6 @@
 export default {
   data: () => ({
   }),
-  computed: {
-    entities() {
-      return this.$store.state.entities;
-    },
-    activeLanguageCode() {
-      return this.$store.state.settings.language_code;
-    },
-  },
 };
 </script>
 
