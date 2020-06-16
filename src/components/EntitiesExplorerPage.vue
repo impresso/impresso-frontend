f042cd1db46b32bdc6896124eb4d995429533734
PaulSchroeder <32929425+PaulSchroeder@users.noreply.github.com>
2020-06-16 17:13:34 +0200

    update timeline on route change

--- src/components/EntitiesExplorerPage.vue
+++ src/components/EntitiesExplorerPage.vue
@@ -2,31 +2,33 @@
   <i-layout-section main>
     <template v-slot:header>
       <b-navbar type="light" variant="light" class="border-bottom" slot="header">
-        <section>
+        <section v-if="observingList.length">
           <div class="label small-caps">
             Entities
           </div>
           <h3>{{ $t('title') }}</h3>
           <div v-if="countActiveFilters > 0">
-            <b-form-checkbox class="ml-auto" @change="useCurrentSearchChanged" switch>
+            <b-form-checkbox @change="useCurrentSearchChanged" switch>
               {{ $t('label.useCurrentSearch') }}
+              <a @click.prevent.stop="toggleQueryExplorerVisible">
+                ({{ $tc('counts.filters', countActiveFilters) }})
+              </a>
+              <div class="bg-dark" v-if="searchQueryExplorerVisible">
+                <search-query-explorer :search-query="searchQuery" dark-mode/>
+              </div>
             </b-form-checkbox>
-            <a href="#" size="sm" @click.prevent.stop="toggleQueryExplorerVisible">
-              ({{ $tc('counts.filters', {count: countActiveFilters}) }})
-            </a>
-            <div class="bg-dark" v-if="searchQueryExplorerVisible">
-              <search-query-explorer :search-query="searchQuery" dark-mode/>
-            </div>
           </div>
         </section>
       </b-navbar>
     </template>
     <template v-slot:default>
-      <section>
-        <timeline>
-        </timeline>
-        <pre>{{entities}}</pre>
-        <pre>{{searchQuery}}</pre>
+      <section v-if="$route.query.items.length" class="p-3">
+        <timeline
+          :values="timevalues"
+          :domain="domain"
+          height="120px"
+          />
+        <!-- <pre>{{searchQuery}}</pre> -->
       </section>
     </template>
   </i-layout-section>
@@ -40,6 +42,8 @@ import { searchQueryGetter } from '@/logic/queryParams';
 export default {
   data: () => ({
     searchQueryExplorerVisible: false,
+    timevalues: [],
+    domain: [1800, 2000],
   }),
   components: {
     Timeline,
@@ -50,13 +54,29 @@ export default {
     countActiveFilters() {
       return this.searchQuery.countActiveFilters();
     },
+    observingList() {
+      return this.$route.query.items;
+    }
   },
   methods: {
     useCurrentSearchChanged() {
       console.log('useCurrentSearchChanged');
     },
     toggleQueryExplorerVisible() {
-      this.searchQueryExplorerVisible !== this.searchQueryExplorerVisible;
+      this.searchQueryExplorerVisible = !this.searchQueryExplorerVisible;
+    },
+    loadFacets(items) {
+      return this.$store.dispatch('entities/LOAD_TIMELINE', items).then((values) => {
+        this.timevalues = values;
+      });
+    },
+  },
+  watch: {
+    $route: {
+      immediate: true,
+      async handler() {
+        this.loadFacets(this.observingList);
+      },
     },
   },
 };
