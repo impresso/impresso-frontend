673268911ade3715d1a70f03cc77b746b37cd871
Roman Kalyakin <roman@kalyakin.com>
2020-07-14 14:57:47 +0200

    summary string

--- src/components/TextReuseClusterIdCardPage.vue
+++ src/components/TextReuseClusterIdCardPage.vue
@@ -6,7 +6,19 @@
       </cluster-page-header>
     </div>
 
-    <div>
+
+    <div class="my-3 mx-2">
+      <b-row>
+        <b-col>
+          <div v-html="$t(labelId, {
+            from: $d(fromTime, 'short'),
+            to: $d(toTime, 'short'),
+            span: timeSpan,
+            lexicalOverlap: $n(lexicalOverlap / 100, 'percent')
+          })"/>
+        </b-col>
+      </b-row>
+
       <b-row>
         <b-col>
           <timeline
@@ -22,9 +34,7 @@
           </timeline>
         </b-col>
       </b-row>
-    </div>
 
-    <div class="my-3 mx-2">
       <b-row>
         <b-col sm="12" md="12" lg="12" xl="6"
           v-for="(facet, index) in barFacets"
@@ -89,6 +99,37 @@ export default {
       const dateFacet = this.facets.find(({ type }) => type === 'date')
       if (dateFacet == null) return []
       return Helpers.timeline.fromBuckets(dateFacet?.buckets, this.resolution)
+    },
+    /** @returns {string} */
+    labelId() {
+      if (this.resolution === 'day') return 'overviewLabelDays'
+      if (this.resolution === 'month') return 'overviewLabelMonths'
+      return 'overviewLabelYears'
+    },
+    /** @returns {number} */
+    timeSpan() {
+      if (this.cluster == null || this.cluster?.timeCoverage == null) return 0
+      const { from, to } = this.cluster?.timeCoverage ?? {}
+      const fromDate = new Date(from)
+      const toDate = new Date(to)
+      const diffMs = toDate.getTime() - fromDate.getTime()
+      if (this.resolution === 'day') return Math.ceil(diffMs / (1000 * 60 * 60 * 24))
+      if (this.resolution === 'month') return Math.ceil(diffMs / (1000 * 60 * 60 * 24 * 30))
+      return Math.ceil(diffMs / (1000 * 60 * 60 * 24 * 365))
+    },
+    /** @returns {Date} */
+    fromTime() {
+      if (this.cluster?.timeCoverage == null) return new Date()
+      return new Date(this.cluster?.timeCoverage?.from)
+    },
+    /** @returns {Date} */
+    toTime() {
+      if (this.cluster?.timeCoverage == null) return new Date()
+      return new Date(this.cluster?.timeCoverage?.to)
+    },
+    /** @returns {number} */
+    lexicalOverlap() {
+      return this.cluster?.lexicalOverlap ?? 0
     }
   },
   watch: {
@@ -110,3 +151,13 @@ export default {
   }
 }
 </script>
+
+<i18n>
+{
+  "en": {
+    "overviewLabelDays": "<span class='date'>{from}</span> to <span class='date'>{to}</span> (<span class='number'>{span}</span> days) with <span class='number'>{lexicalOverlap}</span> lexical overlap",
+    "overviewLabelMonths": "<span class='date'>{from}</span> to <span class='date'>{to}</span> (<span class='number'>{span}</span> months) with <span class='number'>{lexicalOverlap}</span> lexical overlap",
+    "overviewLabelYears": "<span class='date'>{from}</span> to <span class='date'>{to}</span> (<span class='number'>{span}</span> years) with <span class='number'>{lexicalOverlap}</span> lexical overlap"
+  }
+}
+</i18n>
