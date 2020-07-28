b56c9d6628e09e99012aa08020b3fb38350969cd
Roman Kalyakin <roman@kalyakin.com>
2020-07-28 18:01:35 +0200

    added regions

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -19,7 +19,10 @@
         :data="testChartData"/> -->
       <open-seadragon-article-viewer
         :pages="testArticlePages"
-        @page-changed="handlePageChanged"/>
+        :regions="testRegions"
+        :defaultCurrentPageIndex="19"
+        @page-changed="handlePageChanged"
+        @article-selected="handleArticleSelected"/>
     </i-layout-section>
   </i-layout>
 </template>
@@ -55,22 +58,448 @@ export default {
     }),
     sliderBuckets: Array.from({length: 50}, (_, i) =>  ({ val: i, count: Math.random() * 1000 })),
     testArticlePages: [
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0001',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0002',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0003',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0004',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0005',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0006',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0007',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0008',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0009',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0010',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0011',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0012',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0013',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0014',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0015',
-      'https://impresso-project.ch/api/proxy/iiif/SMZ-1978-01-11-a-p0016'
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0001',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0002',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0003',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0004',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0005',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0006',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0007',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0008',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0009',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0010',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0011',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0012',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0013',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0014',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0015',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0016',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0017',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0018',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0019',
+      'https://impresso-project.ch/api/proxy/iiif/EXP-1973-08-27-a-p0020'
+    ],
+    testRegions: [
+      {
+        articleUid: 'EXP-1973-08-27-a-i0370',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 31,
+          y: 1451,
+          w: 554,
+          h: 84
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0370',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 24,
+          y: 1546,
+          w: 271,
+          h: 528
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0370',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 317,
+          y: 1549,
+          w: 268,
+          h: 181
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0373',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 310,
+          y: 1779,
+          w: 276,
+          h: 56
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0373',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 310,
+          y: 1835,
+          w: 276,
+          h: 307
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0377',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 37,
+          y: 468,
+          w: 849,
+          h: 56
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0377',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 34,
+          y: 532,
+          w: 269,
+          h: 360
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0377',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 324,
+          y: 534,
+          w: 269,
+          h: 358
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0377',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 612,
+          y: 534,
+          w: 271,
+          h: 363
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0368',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1195,
+          y: 588,
+          w: 545,
+          h: 46
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0368',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1193,
+          y: 648,
+          w: 266,
+          h: 115
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0368',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1481,
+          y: 648,
+          w: 268,
+          h: 321
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0371',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 51,
+          y: 2139,
+          w: 215,
+          h: 59
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0371',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 51,
+          y: 2106,
+          w: 215,
+          h: 33
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0371',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 22,
+          y: 2200,
+          w: 269,
+          h: 339
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0376',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 901,
+          y: 881,
+          w: 268,
+          h: 282
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0376',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1190,
+          y: 883,
+          w: 269,
+          h: 280
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0376',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 939,
+          y: 1485,
+          w: 513,
+          h: 30
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0375',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1487,
+          y: 1059,
+          w: 246,
+          h: 90
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0375',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1493,
+          y: 1005,
+          w: 240,
+          h: 54
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0375',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1488,
+          y: 1163,
+          w: 247,
+          h: 552
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0375',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1484,
+          y: 1720,
+          w: 248,
+          h: 397
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0375',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1479,
+          y: 2117,
+          w: 249,
+          h: 394
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0372',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 612,
+          y: 2074,
+          w: 828,
+          h: 43
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0372',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 600,
+          y: 2140,
+          w: 269,
+          h: 395
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0372',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 886,
+          y: 2135,
+          w: 276,
+          h: 193
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0372',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 885,
+          y: 2343,
+          w: 274,
+          h: 196
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0372',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1178,
+          y: 2140,
+          w: 269,
+          h: 395
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0367',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 42,
+          y: 110,
+          w: 1129,
+          h: 156
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0367',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 39,
+          y: 270,
+          w: 268,
+          h: 159
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0367',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 329,
+          y: 271,
+          w: 267,
+          h: 165
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0367',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 615,
+          y: 273,
+          w: 275,
+          h: 161
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0367',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 901,
+          y: 275,
+          w: 277,
+          h: 500
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0367',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1361,
+          y: 522,
+          w: 396,
+          h: 34
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0374',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 624,
+          y: 1547,
+          w: 815,
+          h: 102
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0374',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 603,
+          y: 1659,
+          w: 271,
+          h: 402
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0374',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 893,
+          y: 1657,
+          w: 269,
+          h: 402
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0374',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 1181,
+          y: 1659,
+          w: 269,
+          h: 402
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0369',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 36,
+          y: 936,
+          w: 838,
+          h: 50
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0369',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 29,
+          y: 990,
+          w: 271,
+          h: 413
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0369',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 319,
+          y: 992,
+          w: 271,
+          h: 415
+        }
+      },
+      {
+        articleUid: 'EXP-1973-08-27-a-i0369',
+        pageUid: 'EXP-1973-08-27-a-p0020',
+        coords: {
+          x: 608,
+          y: 995,
+          w: 271,
+          h: 520
+        }
+      }
     ]
   }),
   methods: {
@@ -82,6 +511,9 @@ export default {
     },
     handlePageChanged(pageIndex) {
       console.info('Current page', pageIndex)
+    },
+    handleArticleSelected(articleUid) {
+      console.info('Article selected', articleUid)
     }
   },
   components: {
--- src/components/modules/OpenSeadragonArticleViewer.vue
+++ src/components/modules/OpenSeadragonArticleViewer.vue
@@ -5,14 +5,70 @@
 <script>
 import initViewer from 'openseadragon'
 
+
 /**
  * @typedef {import('openseadragon').Viewer} Viewer
+ * @typedef {import('openseadragon').TiledImage} TiledImage
+ * @typedef {import('openseadragon').Rect} Rect
+ * @typedef {{
+ *  articleUid: string,
+ *  pageUid: string,
+ *  coords: { x: number, y: number, w: number, h: number }
+ * }} Region
  */
+
+/**
+ * @param {TiledImage} tiledImage
+ * @param {Region} region
+ * @param {(articleUid: string) => void} clickHandler
+ * @returns {{ overlay: any, rect: Rect }}
+ */
+function createRegionOverlay(tiledImage, region, clickHandler) {
+
+  // @ts-ignore
+  const overlay = window.document.createElement('div')
+  overlay.setAttribute('class', 'overlay-region')
+  overlay.dataset.articleUid = region.articleUid
+
+  overlay.addEventListener('mouseenter', (event) => {
+    const articleUid = event.target.dataset.articleUid
+
+    event.target.parentNode.querySelectorAll(`[data-article-uid=${articleUid}]`).forEach((item) => {
+      item.classList.add('selected')
+    })
+  })
+
+  overlay.addEventListener('click', (event) => {
+    const articleUid = event.target.dataset.articleUid
+    event.stopPropagation()
+    clickHandler(articleUid)
+  })
+
+  overlay.addEventListener('mouseleave', (event) => {
+    const articleUid = event.target.dataset.articleUid
+
+    event.target.parentNode.querySelectorAll(`[data-article-uid=${articleUid}]`).forEach((item) => {
+      item.classList.remove('selected')
+    })
+  })
+
+  const rect = tiledImage.imageToViewportRectangle(
+    region.coords.x,
+    region.coords.y,
+    region.coords.w,
+    region.coords.h
+  )
+
+  return { overlay, rect }
+}
 
 export default {
   data: () => ({
     viewer: /** @type {Viewer|undefined} */ (undefined),
-    currentPageIndex: 0
+    currentPageIndex: 0,
+    tilesAreReady: false,
+    currentOverlays: [],
+    isDragging: false
   }),
   props: {
     /** @type {import('vue').PropOptions<string[]>} */
@@ -22,6 +78,11 @@ export default {
     defaultCurrentPageIndex: {
       type: Number,
       default: 0
+    },
+    /** @type {import('vue').PropOptions<Region[]>} */
+    regions: {
+      type: Array,
+      default: () => []
     }
   },
   methods: {
@@ -29,14 +90,19 @@ export default {
      * @returns {Promise<Viewer>}
      */
     async getViewer() {
-      if (this.viewer != null) return this.viewer
       return new Promise((resolve) => {
         setTimeout(() => {
+          if (this.viewer != null) return resolve(this.viewer)
+
           this.viewer = initViewer({
             element: this.$el,
             immediateRender: true,
             showNavigationControl: false,
             // animationTime: 0,
+            gestureSettingsMouse: {
+              clickToZoom: false,
+              dblClickToZoom: true,
+            },
             defaultZoomLevel: 0.001,
             collectionMode:       true,
             collectionRows:       1,
@@ -44,6 +110,45 @@ export default {
             collectionTileMargin: 16,
             debugMode: true
           })
+
+
+          this.viewer.addHandler('viewport-change', () => {
+            const viewer = /** @type {Viewer} */ (this.viewer)
+            if (viewer.world.getItemCount() === 0) return
+            let { x: currentX } = viewer.viewport.getCenter()
+            const firstItemX = viewer.world.getItemAt(0).getBounds().x
+
+            if (currentX < firstItemX) currentX = firstItemX
+
+            const itemsBounds = [...Array(viewer.world.getItemCount()).keys()].map(i => viewer.world.getItemAt(i).getBounds())
+            const currentItemIndex = itemsBounds.findIndex((item, index, bounds) => {
+              if (index + 1 >= bounds.length) return true
+
+              const previousItem = index > 0 ? bounds[index-1] : undefined
+              const nextItem = bounds[index+1]
+
+              const leftBoundary = previousItem
+                ? (previousItem.x + previousItem.width) + (item.x - (previousItem.x + previousItem.width)) / 2
+                : item.x
+              const rightBoundary = item.x + item.width + (nextItem.x - (item.x + item.width)) / 2
+
+              return currentX >= leftBoundary && currentX < rightBoundary
+            })
+
+            if (currentItemIndex !== this.currentPageIndex) {
+              this.currentPageIndex = currentItemIndex
+              this.$emit('page-changed', this.currentPageIndex)
+            }
+          })
+
+          this.viewer.addHandler('canvas-drag', () => {
+            this.isDragging = true;
+          });
+
+          this.viewer.addHandler('canvas-drag-end', () => {
+            setTimeout(() => { this.isDragging = false }, 0);
+          });
+
           resolve(this.viewer)
         }, 0)
       })
@@ -52,6 +157,12 @@ export default {
   mounted() {
     this.currentPageIndex = this.defaultCurrentPageIndex
   },
+  computed: {
+    readyRegions() {
+      if (this.tilesAreReady) return this.regions
+      return []
+    }
+  },
   watch: {
     pages: {
       /** @param {string[]} pages */
@@ -59,38 +170,41 @@ export default {
         if (pages == null) return
         const viewer = await this.getViewer()
 
+        this.tilesAreReady = false
         viewer.addOnceHandler('tile-loaded', () => {
           viewer.viewport.fitBounds(viewer.world.getItemAt(this.currentPageIndex).getBounds(), true)
+          this.tilesAreReady = true
         })
 
-        viewer.addHandler('viewport-change', () => {
-          if (viewer.world.getItemCount() === 0) return
-          let { x: currentX } = viewer.viewport.getCenter()
-          const firstItemX = viewer.world.getItemAt(0).getBounds().x
-
-          if (currentX < firstItemX) currentX = firstItemX
-
-          const itemsBounds = [...Array(viewer.world.getItemCount()).keys()].map(i => viewer.world.getItemAt(i).getBounds())
-          const currentItemIndex = itemsBounds.findIndex((item, index, bounds) => {
-            if (index + 1 >= bounds.length) return true
-
-            const previousItem = index > 0 ? bounds[index-1] : undefined
-            const nextItem = bounds[index+1]
-
-            const leftBoundary = previousItem
-              ? (previousItem.x + previousItem.width) + (item.x - (previousItem.x + previousItem.width)) / 2
-              : item.x
-            const rightBoundary = item.x + item.width + (nextItem.x - (item.x + item.width)) / 2
-
-            return currentX >= leftBoundary && currentX < rightBoundary
-          })
-
-          if (currentItemIndex !== this.currentPageIndex) {
-            this.currentPageIndex = currentItemIndex
-            this.$emit('page-changed', this.currentPageIndex)
-          }
-        })
         viewer.open(pages)
+      },
+      immediate: true,
+      deep: true
+    },
+    readyRegions: {
+      /**
+       * @param {Region[]} regions
+       */
+      async handler(regions) {
+        const viewer = await this.getViewer()
+
+        this.currentOverlays.forEach(overlay => viewer.removeOverlay(overlay))
+        this.currentOverlays = []
+
+        const tiledImage = viewer.world.getItemAt(this.currentPageIndex)
+
+        this.currentOverlays = /**  @type {never[]} */ (regions.map(region => {
+          const { overlay, rect } = createRegionOverlay(
+            tiledImage,
+            region,
+            articleUid => {
+              if (this.isDragging) return
+              this.$emit('article-selected', articleUid)
+            }
+          )
+          viewer.addOverlay(overlay, rect)
+          return overlay
+        }))
       },
       immediate: true,
       deep: true
@@ -104,5 +218,11 @@ export default {
 .os-article-viewer {
   height: 100%;
   width: 100%;
+}
+
+@supports (mix-blend-mode: multiply) {
+  div.overlay-region {
+    mix-blend-mode: multiply;
+  }
 }
 </style>
