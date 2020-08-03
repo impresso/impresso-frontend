41958ed05ac19798936c8a67d9ad82daa20e5d5c
Daniele Guido <gui.daniele@gmail.com>
2020-08-03 11:19:49 +0200

    add header with controllers to switch page

--- src/pages/IssueViewerPage.vue
+++ src/pages/IssueViewerPage.vue
@@ -3,24 +3,57 @@
     <!-- main section -->
     <i-layout-section main>
       <!-- header -->
-      <div slot="header" class="border-bottom">
-        [Issue header]
+      <div slot="header" class="border-bottom" v-if="issue">
+        <b-navbar type="light" variant="light" class="px-0 py-0">
+          <section class='p-2 pl-3'>
+            <h3 class="m-0">
+              <b>{{ issue.newspaper.name }}</b> &middot;
+              <span class="date">
+              {{ $d(issue.date, 'long') }}
+              </span>
+            </h3>
+          </section>
+          <b-navbar-nav class=" border">
+            <b-button variant="light" size="sm"
+              :disabled="currentPageIndex === 0"
+              @click="changeCurrentPageIndex(currentPageIndex - 1)">
+              <div class="dripicons dripicons-media-previous pt-1"></div>
+            </b-button>
+            <div class="px-2 pt-1">{{ $tc('pp', 1, { pages: page.num }) }}</div>
+            <b-button variant="light" size="sm"
+              :disabled="(currentPageIndex + 1) === issue.pages.length"
+              @click="changeCurrentPageIndex(currentPageIndex + 1)">
+              <div class="dripicons dripicons-media-next pt-1"></div>
+            </b-button>
+          </b-navbar-nav>
+        </b-navbar>
       </div>
       <!-- content -->
-      <div class="d-flex h-100 justify-content-center">
+      <div class="d-flex h-100 justify-content-center"  v-if="issue">
         <open-seadragon-article-viewer
           :pages="pagesIIIFUrls"
           :regions="regions"
           :defaultCurrentPageIndex="currentPageIndex"
-          @page-changed="handlePageChanged"
+          @page-changed="changeCurrentPageIndex"
           @article-selected="handleArticleSelected"/>
       </div>
+    </i-layout-section>
+    <i-layout-section width="120px" class="border-left">
+      <list hide-pagination v-if="issue">
+        <div v-for="(item, i) in issue.pages" :key="i" @click="changeCurrentPageIndex(i)">
+          <page-item
+          :active="pageId === item.uid"
+          :item="item" />
+        </div>
+      </list>
     </i-layout-section>
   </i-layout>
 </template>
 
 <script>
 import OpenSeadragonArticleViewer from '@/components/modules/OpenSeadragonArticleViewer'
+import PageItem from '@/components/modules/lists/PageItem'
+import List from '@/components/modules/lists/List'
 import { issues as issuesService, articles as articlesService } from '@/services'
 import { getQueryParameter } from '@/router/util'
 import { getPageId, getShortArticleId, getLongArticleId } from '@/logic/ids'
@@ -36,7 +69,9 @@ export default {
     pagesArticles: /** @type {{[key: string] : Article[] }} */ ({})
   }),
   components: {
-    OpenSeadragonArticleViewer
+    OpenSeadragonArticleViewer,
+    PageItem,
+    List,
   },
   computed: {
     /** @returns {string} */
@@ -53,6 +88,10 @@ export default {
       const shortArticleId = getQueryParameter(this, QueryParams.ArticleId)
       if (shortArticleId == null) return undefined
       return getLongArticleId(this.issueId, shortArticleId)
+    },
+    page() {
+      if (this.currentPageIndex === -1) return undefined
+      return this.issue.pages[this.currentPageIndex]
     },
     /** @returns {string[]|undefined} */
     pagesIIIFUrls() {
@@ -73,14 +112,18 @@ export default {
     currentPageIndex: {
       /** @returns {number} */
       get() {
+        if (!this.issue) return -1;
         const pageNumber = parseInt(/** @type {string} */ (getQueryParameter(this, QueryParams.PageNumber)), 10)
-        if (isNaN(pageNumber)) return 0
-        return pageNumber - 1
+        if (isNaN(pageNumber)) return 0;
+        // handle missing pages.
+        return this.issue.pages.findIndex(p => p.num === pageNumber);
       },
       /** @param {number} index */
       set(index) {
+        // set page num in query fro the page corresponding to the right currentPageIndex
+        const pageNumber = this.issue.pages[index]?.num;
         this.$navigation.updateQueryParameters({
-          [QueryParams.PageNumber]: index + 1
+          [QueryParams.PageNumber]: pageNumber
         })
       }
     }
@@ -89,9 +132,7 @@ export default {
     issueId: {
       /** @param {string} id */
       async handler(id) {
-        if (id == null) throw new Error('Issue ID must always be defined on this page')
         this.issue = new Issue(await issuesService.get(id))
-        await this.loadRegions(this.currentPageIndex)
       },
       immediate: true
     },
@@ -104,8 +145,9 @@ export default {
     }
   },
   methods: {
-    handlePageChanged(pageIndex) {
-      this.currentPageIndex = pageIndex
+    changeCurrentPageIndex(pageIndex) {
+      console.info('@changeCurrentPageIndex', pageIndex, this.currentPageIndex);
+      this.currentPageIndex = pageIndex;
     },
     handleArticleSelected(articleUid) {
       this.$navigation.updateQueryParameters({
