b43fbf7773e58de9bfea1e6e3baaffe9e62a177f
Daniele Guido <gui.daniele@gmail.com>
2022-03-11 12:07:12 +0100

    Create EntitiesTemporaryPage.vue

---
+++ src/components/EntitiesTemporaryPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <i-layout-section main>
+    <template v-slot:header>
+      <b-navbar type="light" variant="light" slot="header">
+        <section class="top-section">
+          <h3>{{ $t('title') }}</h3>
+        </section>
+      </b-navbar>
+      <b-navbar class="pt-0 border-top">
+      </b-navbar>
+    </template>
+  </i-layout-section>
+</template>
+
+<i18n>
+{
+  "en": {
+    "title": "Entities"
+  }
+}
+</i18n>
