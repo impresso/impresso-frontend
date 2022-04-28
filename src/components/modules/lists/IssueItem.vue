52f495a92a8c33a9cd1be8aeeb5daa8f18fdf9d5
Daniele Guido <gui.daniele@gmail.com>
2022-04-28 16:20:54 +0200

    Create IssueItem.vue

---
+++ src/components/modules/lists/IssueItem.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <b-card class="IssueItem">
+    <router-link :to="routerLinkUrl">
+      <b-card-img-lazy
+        class="bg-light border"
+        :src="item.frontPage.getIiifThumbnail({ dim: 700 })"
+        :alt="$d(item.date, 'long')" top />
+    </router-link>
+    <b-card-body>
+      <router-link :to="routerLinkUrl">
+        <span class="date">{{ $d(new Date(item.date), "long") }}</span>
+      </router-link>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    routerLinkUrl() {
+      return {
+        name: 'issue-viewer',
+        params: {
+          issue_uid: this.item.uid,
+        },
+        query: {
+          ...this.$route.query,
+          p: this.item.frontPage.num,
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.date {
+  text-transform: lowercase;
+  font-variant: small-caps;
+}
+</style>
