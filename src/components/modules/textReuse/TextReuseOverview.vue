45d6e8de417c4d902647f05f4f75c6168bc1ed97
Daniele Guido <gui.daniele@gmail.com>
2023-01-13 10:31:04 +0100

    add dropdown menu to select the visualisation to display

--- src/components/modules/textReuse/TextReuseOverview.vue
+++ src/components/modules/textReuse/TextReuseOverview.vue
@@ -1,7 +1,21 @@
 <template>
   <PowerVisBase :data="data" :loading="loading" @item:click="itemClicked">
     <template v-slot:header>
-      <div>TextReuseOverview Header</div>
+      <b-navbar-nav class="p-3 mb-3 ml-auto border-bottom">
+        <label class="mr-2">{{ $t('visualisationType') }}</label>
+        <i-dropdown
+          v-model="visualisation"
+          :options="
+            visualisationOptions.map(value => ({
+              value,
+              text: $t(`use_${value}`),
+            }))
+          "
+          class="mr-auto"
+          size="sm"
+          variant="outline-primary"
+        ></i-dropdown>
+      </b-navbar-nav>
     </template>
     <template v-slot:footer>
       <div>TextReuseOverview Footer</div>
@@ -13,24 +27,54 @@
 import { defineComponent } from 'vue'
 import PowerVisBase from '@/components/modules/vis/PowerVisBase'
 
+const Visualisations = {
+  tr_vs_newspapers: {},
+  troverlap_vs_newspapers: {},
+  trdelta_vs_newspapers: {},
+  trcount_vs_newspapers: {},
+  trcount_vs_time: {},
+}
+const VisualisationOptions = Object.keys(Visualisations)
+
 export default defineComponent({
   name: 'TextReuseOverview',
   components: {
-    PowerVisBase
+    PowerVisBase,
   },
   props: {
     data: {
       type: Object,
-      default: () => {}
+      default: () => {},
     },
     loading: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
+  data: () => ({
+    items: [],
+    visualisation: 'tr_vs_newspapers',
+    visualisationOptions: VisualisationOptions,
+  }),
   setup() {
     const itemClicked = () => {}
     return { itemClicked }
-  }
+  },
 })
 </script>
+<i18n>
+{
+  "en": {
+    "visualisationType": "Visualisation Type",
+    "use_tr_vs_newspapers" :"Text Reuse between Newspaper Titles",
+    "use_tr_vs_time": "Text Reuse over Time",
+    "use_trcsize_vs_newspapers": "Cluster Sizes per Newspaper Title",
+    "use_trcsize_vs_newspapers": "Cluster Sizes over Time",
+    "use_troverlap_vs_newspapers" :"Text Reuse lexical Overlap between Newspaper Titles",
+    "use_troverlap_vs_time": "Lexical Overlap over Time",
+    "use_trdelta_vs_newspapers": "Publication Time Delta per Newspaper Title",
+    "use_trcount_vs_newspapers": " Sum of Passages per Newspaper Title",
+    "use_trcount_vs_time": "Sum of Passages over Time"
+  }
+}
+</i18n>
