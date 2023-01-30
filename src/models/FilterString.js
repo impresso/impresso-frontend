ebb2b9109869dc148e127cae6fc3ab06d9639111
Daniele Guido <gui.daniele@gmail.com>
2023-01-30 15:32:24 +0100

    Update FilterString.js

--- src/models/FilterString.js
+++ src/models/FilterString.js
@@ -1,41 +1,39 @@
+/*eslint import/namespace: ['error', { allowComputed: true }]*/
 /**
  * FilterString object
  * @param {String} context either 'include' or 'exclude'
  * @param {String} query The search query
  */
 
-import FilterItems from '@/models/FilterItems';
-import * as precisions from './Precisions';
-import * as contexts from './Contexts';
+import FilterItems from '@/models/FilterItems'
+import * as precisions from './Precisions'
+import * as contexts from './Contexts'
 
 class StringItem {
-  constructor({
-    uid = '',
-    checked = true,
-  }) {
-    this.uid = uid;
-    this.checked = Boolean(checked);
-    this.isValid = typeof this.uid === 'string' && this.uid.trim().length > 0;
+  constructor({ uid = '', checked = true }) {
+    this.uid = uid
+    this.checked = Boolean(checked)
+    this.isValid = typeof this.uid === 'string' && this.uid.trim().length > 0
   }
 }
 
 export default class FilterString extends FilterItems {
   constructor(args) {
-    super(args);
-    this.precision = precisions[(args.precision || 'exact').toUpperCase()];
-    this.distance = Math.max(0, Math.min(parseInt(args.distance || 0, 10), 10));
+    super(args)
+    this.precision = precisions[(args.precision || 'exact').toUpperCase()]
+    this.distance = Math.max(0, Math.min(parseInt(args.distance || 0, 10), 10))
     // console.info('FilterString built', this.q, this.items, args);
   }
 
   setItems(items = []) {
     this.items = items
-      .map((uid) => {
+      .map(uid => {
         if (uid instanceof StringItem) {
-          return uid;
+          return uid
         }
-        return new StringItem({ uid });
+        return new StringItem({ uid })
       })
-      .filter(item => item.isValid);
+      .filter(item => item.isValid)
   }
 
   getQuery() {
@@ -44,14 +42,14 @@ export default class FilterString extends FilterItems {
       op: this.op,
       q: this.items.map(d => d.uid),
       precision: this.precision,
-    };
+    }
 
     if (this.distance !== 0) {
-      query.distance = this.distance;
+      query.distance = this.distance
     }
     if (this.context !== contexts.INCLUDE) {
-      query.context = this.context;
+      query.context = this.context
     }
-    return query;
+    return query
   }
 }
