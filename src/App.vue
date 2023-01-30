0c0a54a74db8a3c0b7f4d84e49d3ae41463e8b03
Daniele Guido <gui.daniele@gmail.com>
2023-01-30 14:46:49 +0100

    add shadow to buttons

--- src/App.vue
+++ src/App.vue
@@ -312,9 +312,9 @@ $clr-grey-900: #ddd;
   background-color: $clr-tertiary !important;
 }
 
-.btn {
+.btn-outline-primary {
   border-radius: 2px;
-  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
+  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 3px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
 }
 
 // --- input range sliders ---
