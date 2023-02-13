1360ef6437c222e4899c644e5045e6ba771fda77
Daniele Guido <gui.daniele@gmail.com>
2023-02-13 16:36:31 +0100

    add textreusecluster model

--- src/models/Bucket.js
+++ src/models/Bucket.js
@@ -1,8 +1,9 @@
-import Entity from '@/models/Entity';
-import Topic from '@/models/Topic';
-import Newspaper from '@/models/Newspaper';
-import Year from '@/models/Year';
-import Collection from '@/models/Collection';
+import Entity from '@/models/Entity'
+import Topic from '@/models/Topic'
+import Newspaper from '@/models/Newspaper'
+import Year from '@/models/Year'
+import Collection from '@/models/Collection'
+import TextReuseCluster from '@/models/TextReuseCluster'
 
 /**
  * @class Bucket is an object representing a Solr search engine facet bucket
@@ -12,43 +13,40 @@ import Collection from '@/models/Collection';
  * @param {Object} item Optional object of for instance type Newspaper or Entity
  */
 export default class Bucket {
-  constructor({
-    val = '',
-    count = -1,
-    item = {},
-    included = true,
-    type = '',
-  } = {}) {
-    this.val = String(val);
-    this.count = parseInt(count, 10);
-    this.included = included;
+  constructor({ val = '', count = -1, item = {}, included = true, type = '' } = {}) {
+    this.val = String(val)
+    this.count = parseInt(count, 10)
+    this.included = included
 
     switch (type) {
-    case 'topic':
-      this.item = new Topic(item);
-      break;
-    case 'person':
-    case 'location':
-      this.item = new Entity(item);
-      break;
-    case 'newspaper':
-      this.item = new Newspaper(item);
-      break;
-    case 'collection':
-      this.item = new Collection(item);
-      break;
-    case 'year':
-      this.item = new Year(item);
-      break;
-    default:
-      this.item = {
-        uid: this.val,
-      };
-      break;
+      case 'topic':
+        this.item = new Topic(item)
+        break
+      case 'person':
+      case 'location':
+        this.item = new Entity(item)
+        break
+      case 'newspaper':
+        this.item = new Newspaper(item)
+        break
+      case 'collection':
+        this.item = new Collection(item)
+        break
+      case 'year':
+        this.item = new Year(item)
+        break
+      case 'textReuseCluster':
+        this.item = TextReuseCluster.fromTextReusePassage(item)
+        break
+      default:
+        this.item = {
+          uid: this.val,
+        }
+        break
     }
 
     if (!this.val.length) {
-      throw new Error('Bucket should have a valid value "val", empty value given');
+      throw new Error('Bucket should have a valid value "val", empty value given')
     }
   }
 }
