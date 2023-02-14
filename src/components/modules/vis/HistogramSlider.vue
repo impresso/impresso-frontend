5a17e61caf0a0a4230a2e39b79b86955efc017e3
Daniele Guido <gui.daniele@gmail.com>
2023-02-14 11:27:17 +0100

    polish label in range slider

--- src/components/modules/vis/HistogramSlider.vue
+++ src/components/modules/vis/HistogramSlider.vue
@@ -14,7 +14,8 @@
           :silent="true"
           :enable-cross="false"
           :tooltip-formatter="formatTooltip"
-          :tooltip-placement="onlyRangeLabels ? 'bottom' : 'top'"/>
+          :tooltip-placement="onlyRangeLabels ? 'bottom' : 'top'"
+        />
       </b-row>
     </b-col>
   </b-container>
@@ -25,7 +26,6 @@ import * as d3 from 'd3'
 import VueSlider from 'vue-slider-component'
 import 'vue-slider-component/theme/default.css'
 
-
 /**
  * NOTE: Only works with integers. If you need to do fractions you
  * will need to normalise them.
@@ -33,32 +33,32 @@ import 'vue-slider-component/theme/default.css'
 export default {
   model: {
     prop: 'value',
-    event: 'change'
+    event: 'change',
   },
   props: {
     /** @type {import('vue').PropOptions<Number[]>} */
     value: {
-      type: Array
+      type: Array,
     },
     /** @type {import('vue').PropOptions<Number[]>} */
     range: {
-      type: Array
+      type: Array,
     },
     /** @type {import('vue').PropOptions<import('@/models').Bucket[]>} */
     buckets: {
-      type: Array
+      type: Array,
     },
     chartHeight: {
       type: Number,
-      default: 50
+      default: 50,
     },
     onlyRangeLabels: {
       type: Boolean,
-      default: false
+      default: false,
     },
     scaleType: {
       type: String,
-      default: 'linear'
+      default: 'linear',
     },
   },
   mounted() {
@@ -79,7 +79,7 @@ export default {
       /** @param {undefined|number[]} value */
       set(value) {
         this.$emit('change', value)
-      }
+      },
     },
     /** @returns {{[key:string]: string}|string[]|undefined} */
     sliderMarks() {
@@ -94,16 +94,21 @@ export default {
 
       if (this.buckets && this.buckets.length > 0) {
         const step = Math.floor(this.buckets.length / marksCount)
-        return this.buckets.reduce((acc, { val }, index) => {
-          if (index % step === 0) acc.push(val);
-          return acc;
-        }, /** @type {string[]} */ ([])).concat([this.sliderRange[1].toString()]);
+        return this.buckets
+          .reduce(
+            (acc, { val }, index) => {
+              if (index % step === 0) acc.push(val)
+              return acc
+            },
+            /** @type {string[]} */ ([]),
+          )
+          .concat([this.sliderRange[1].toString()])
       }
       const [min, max] = this.sliderRange
       const step = (max - min) / marksCount
       if (Number.isNaN(step)) return undefined
 
-      return Array.from({length: marksCount + 1}, (_, i) =>  this.$n(min + i * step))
+      return Array.from({ length: marksCount + 1 }, (_, i) => this.$n(min + i * step))
     },
     /** @returns {number[]} */
     sliderRange() {
@@ -113,11 +118,8 @@ export default {
 
       if (this.range == null) return [min, max]
 
-      return [
-        min < this.range[0] ? min : this.range[0],
-        max > this.range[1] ? max : this.range[1],
-      ]
-    }
+      return [min < this.range[0] ? min : this.range[0], max > this.range[1] ? max : this.range[1]]
+    },
   },
   methods: {
     renderChart() {
@@ -131,22 +133,25 @@ export default {
 
       const counts = this.buckets.map(({ count }) => count)
 
-      const x = d3.scaleBand()
+      const x = d3
+        .scaleBand()
         .domain(this.buckets.map(({ val }) => val))
         .range([0, width])
         .paddingInner(0.05)
 
-      const yScaler = {
-        linear: d3.scaleLinear,
-        sqrt: d3.scaleSqrt,
-        symlog: d3.scaleSymlog,
-      }[this.scaleType] ?? d3.scaleLinear;
+      const yScaler =
+        {
+          linear: d3.scaleLinear,
+          sqrt: d3.scaleSqrt,
+          symlog: d3.scaleSymlog,
+        }[this.scaleType] ?? d3.scaleLinear
 
       const y = yScaler()
         .domain([Math.min(...counts), Math.max(...counts)])
         .range([0, this.chartHeight - topMargin])
 
-      svg.selectAll('g.bars')
+      svg
+        .selectAll('g.bars')
         .data([null])
         .join('g')
         .attr('class', 'bars')
@@ -157,34 +162,37 @@ export default {
         .attr('x', d => x(d.val) ?? 0)
         .attr('width', x.bandwidth())
         .attr('y', d => this.chartHeight - y(d.count))
-        .attr('height', d =>  y(d.count))
+        .attr('height', d => y(d.count))
 
       const maxCountBucketIndex = counts.indexOf(Math.max(...counts))
 
-      const maxval = svg.selectAll('g.maxval')
+      const maxval = svg
+        .selectAll('g.maxval')
         .data(maxCountBucketIndex >= 0 ? [this.buckets[maxCountBucketIndex]] : [])
         .join('g')
         .attr('class', 'maxval')
         .attr('transform', bucket => {
-          const xOffset = (x(bucket.val) ?? 0) + (x.bandwidth() / 2)
+          const xOffset = (x(bucket.val) ?? 0) + x.bandwidth() / 2
           const yOffset = this.chartHeight - y(bucket.count)
           return `translate(${xOffset}, ${yOffset})`
         })
 
-      maxval.selectAll('text')
+      maxval
+        .selectAll('text')
         .data(d => [d])
         .join('text')
         .attr('dy', -5)
-        .text(bucket => Math.round(bucket.count))
+        .text(bucket => this.$t('maxval', {n : this.$n(Math.round(bucket.count)), v:bucket.val}))
         .attr('text-anchor', bucket => {
-          const xOffset = (x(bucket.val) ?? 0) + (x.bandwidth() / 2)
+          const xOffset = (x(bucket.val) ?? 0) + x.bandwidth() / 2
           const oneThirdWidth = width / 3
           if (xOffset <= oneThirdWidth) return 'start'
-          if (xOffset >= 2*oneThirdWidth) return 'end'
+          if (xOffset >= 2 * oneThirdWidth) return 'end'
           return 'middle'
         })
 
-      maxval.selectAll('circle.point')
+      maxval
+        .selectAll('circle.point')
         .data(d => [d])
         .join('circle')
         .attr('class', 'point')
@@ -192,38 +200,44 @@ export default {
     },
     formatTooltip(d) {
       return this.$n(d)
-    }
+    },
   },
   watch: {
     buckets() {
       this.renderChart()
-    }
+    },
   },
   components: {
-    VueSlider
-  }
+    VueSlider,
+  },
 }
 </script>
 
 <style lang="scss">
-  @import "impresso-theme/src/scss/variables.sass";
-  .histogram-slider {
-    .slider {
-      width: 100% !important;
-      margin-bottom: 1.4em; // slider ticks
-    }
-    .chart {
-      .bars {
-        .bar {
-          fill: #d8d8d8;
-        }
+@import 'impresso-theme/src/scss/variables.sass';
+.histogram-slider {
+  .slider {
+    width: 100% !important;
+    margin-bottom: 1.4em; // slider ticks
+  }
+  .chart {
+    .bars {
+      .bar {
+        fill: #d8d8d8;
       }
-      .maxval {
-        font-size: 12px;
-        .point {
-          fill: $clr-primary;
-        }
+    }
+    .maxval {
+      font-size: 12px;
+      .point {
+        fill: $clr-primary;
       }
     }
   }
+}
 </style>
+<i18n>
+{
+  "en": {
+    "maxval": "{v} ({n} results)"
+  }
+}
