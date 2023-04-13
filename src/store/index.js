811bdd793e1c6ad508f91aab2a228540082ec3ee
Daniele Guido <gui.daniele@gmail.com>
2023-04-13 16:18:15 +0200

    add badRequest as error not to forward

--- src/store/index.js
+++ src/store/index.js
@@ -21,7 +21,7 @@ import selectionMonitor from './SelectionMonitor'
 Vue.use(Vuex)
 
 const ERRORS_DO_NOT_DISPLAY = ['NavigationDuplicated'] // error names not to display to the user
-const ERRORS_DO_NOT_FORWARD = ['BadGateway', 'TransportError', 'NotAuthenticated'] // error to avoid loopholes
+const ERRORS_DO_NOT_FORWARD = ['BadGateway', 'TransportError', 'NotAuthenticated', 'BadRequest'] // error to avoid loopholes
 
 let processingTimer // timeout object
 
