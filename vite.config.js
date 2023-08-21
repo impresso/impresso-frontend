2f178d2c59e001e097e2a7222a01d38a39ba5bfe
Daniele Guido <gui.daniele@gmail.com>
2023-08-21 17:10:15 +0200

    fix messages i18n

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,23 @@ import path from 'path'
 // import vue from '@vitejs/plugin-vue'
 import vue from '@vitejs/plugin-vue2'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
-
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.PUBLIC_PATH || '/',
   plugins: [
     vue(),
     vueI18n({
-      compositionOnly: false,
+      // compositionOnly: false,
       include: path.resolve(__dirname, './src/i18n/**'),
     }),
+    // VueI18nPlugin({
+    //   include: path.resolve(__dirname, './src/i18n/**'),
+    //   strictMessage: false,
+    //   compositionOnly: false,
+    // }),
   ],
+
   // esbuild: {
   //   jsxFactory: 'h',
   //   jsxFragment: 'Fragment',
