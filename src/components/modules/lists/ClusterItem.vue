71f3c2bbb5ed3612671501bc7201a41de7c42222
Daniele Guido <gui.daniele@gmail.com>
2023-11-14 12:19:15 +0100

    Update ClusterItem.vue

--- src/components/modules/lists/ClusterItem.vue
+++ src/components/modules/lists/ClusterItem.vue
@@ -1,33 +1,48 @@
 <template>
   <div class="ClusterItem">
     <span class="d-flex align-self-stretch flex-shrink-0 selection-indicator" />
-    <pre>{{ JSON.stringify(item, null, 2) }}</pre>
-    <div class="d-flex">
-      <h2 class="flex-grow-1 sans">
-        <span>#{{ item.shortId }}</span>
-      </h2>
-      <div class="lexical-overlap" v-b-tooltip.hover :title="$t('lexicalOverlap')">
-        {{$n(item.lexicalOverlap / 100, { style: 'percent', maximumFractionDigits: 0 })}}
-      </div>
+    <TextReusePassageItemLabel
+      :item="textReusePassageItem"
+      class="border-left pl-2 my-2 small border-tertiary"
+    />
 
-      <span class="px-1">–</span>
-
-      <div class="small-caps" v-html="$tc('numbers.articles', item.clusterSize, {
-        n: $n(item.clusterSize),
-      })"/>
-
+    <div class="rounded border border-tertiary bg-white shadow-sm p-1">
+      <Ellipsis
+        v-bind:max-height="300"
+        v-bind:initialHeight="200"
+        :additional-height="50"
+        @click.prevent.stop
+      >
+        <p class="text-sample p-2">
+          <span>{{ item.textSampleContent }}</span>
+        </p>
+      </Ellipsis>
     </div>
-    </div>
+    <div class="small text-muted mb-2" v-html="textReuseClusterSummary"></div>
+
+    <b-button
+      variant="outline-secondary"
+      size="sm"
+      class="TextReusePassageItem_compareBtn float-left rounded shadow-sm"
+      @click="handleClusterClick"
+    >
+      {{ $t('seeTextReuseCluster') }}
+    </b-button>
   </div>
 </template>
 
 <script>
 import ClusterDetailsPanel from '@/components/modules/textReuse/ClusterDetailsPanel'
 
+import TextReusePassageItemLabel from './TextReusePassageItemLabel'
+import Ellipsis from '../Ellipsis'
+
 export default {
   name: 'ClusterItem',
   components: {
     ClusterDetailsPanel,
+    TextReusePassageItemLabel,
+    Ellipsis,
   },
   props: {
     selected: Boolean,
@@ -39,7 +54,73 @@ export default {
       type: Object,
     },
   },
-  computed: {},
+  methods: {
+    handleClusterClick() {
+      // <ItemSelector
+      //     :uid="item.textReuseCluster.id"
+      //     :item="item.textReuseCluster"
+      //     type="textReuseCluster"
+      //     context="textReuse"
+      //   >
+      //     {{ $t('seeTextReuseCluster') }}
+      //   </ItemSelector>
+      this.$store.dispatch('selectionMonitor/show', {
+        type: 'textReusePassage',
+        item: this.textReusePassageItem,
+        context: 'textReuse',
+        scope: 'comparePassages',
+        applyCurrentSearchFilters: false,
+        displayTimeline: false,
+        displayActionButtons: false,
+        displayCurrentSearchFilters: false,
+      })
+    },
+  },
+  computed: {
+    textReusePassageItem() {
+      return {
+        id: this.item.textSampleArticle.id,
+        title: this.item.textSampleTitle,
+        newspaper: {
+          id: this.item.textSampleArticle.id.split('-')[0],
+        },
+        date: this.item.textSampleDate,
+        pageNumbers: [1],
+        issue: {
+          id: this.item.textSampleArticle.id.split('-i')[0],
+        },
+        article: {
+          id: this.item.textSampleArticle.id,
+        },
+        content: this.item.textSampleContent,
+        textReuseCluster: {
+          id: this.item.id,
+          clusterSize: this.item.clusterSize,
+          lexicalOverlap: this.item.lexicalOverlap,
+          timeDifferenceDay: this.item.timeDifferenceDay,
+          article: {
+            id: this.item.textSampleArticle.id,
+          },
+        },
+      }
+    },
+    textReuseClusterSummary() {
+      const clusterSizeLabel = this.$tc('numbers.clusterSize', this.item.clusterSize, {
+        n: this.$n(this.item.clusterSize),
+      })
+      const lexicalOverlapLabel = this.$tc('numbers.lexicalOverlap', this.item.lexicalOverlap, {
+        n: this.$n(Math.round(this.item.lexicalOverlap * 100) / 100),
+      })
+
+      return this.$t('textReuseClusterSummary', {
+        clusterSize: clusterSizeLabel,
+        lexicalOverlap: lexicalOverlapLabel,
+        timespan: this.$tc('numbers.days', this.item.timeDifferenceDay, {
+          n: this.item.timeDifferenceDay,
+        }),
+      })
+    },
+  },
 }
 </script>
 
@@ -50,4 +131,21 @@ export default {
 .ClusterItem {
   max-width: 350px;
 }
+.ClusterItem .number {
+  font-weight: bold;
+}
 </style>
+
+<i18n>
+  {
+    "en": {
+      "numbers": {
+        "days": "<span class='number'>{n}</span> day|<span class='number'>{n}</span> days"
+      },
+      "fromArticle": "From article",
+      "seeTextReuseCluster": "Compare passages in cluster",
+      "textReuseClusterSummary": "Cluster size: {clusterSize} with {lexicalOverlap} over {timespan}."
+    }
+  }
+
+</i18n>
