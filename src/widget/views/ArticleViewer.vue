45ae0389f4ead3868c0880c9355b119af56c53ca
Daniele Guido <gui.daniele@gmail.com>
2024-02-26 16:28:51 +0100

    use coordinates

--- src/widget/views/ArticleViewer.vue
+++ src/widget/views/ArticleViewer.vue
@@ -75,6 +75,25 @@ export default {
     },
   },
   methods: {
+    hasValidCoords() {
+      return /^[0-9]+,[0-9]+,[0-9]+,[0-9]+$/.test(this.coords)
+    },
+    drawHighlightRectangle(viewer, coords) {
+      const overlay = window.document.createElement('div')
+      overlay.setAttribute('class', 'overlay-region')
+      Object.keys(this.getOverlayStyle).forEach(cssProp => {
+        overlay[cssProp] = this.getOverlayStyle[cssProp]
+      })
+      const rect = viewer.viewport.imageToViewportRectangle(...coords)
+      viewer.addOverlay(overlay, rect)
+    },
+    fitBounds(viewer, coords) {
+      const margin = isNaN(this.coordsMargin) ? 10 : parseInt(this.coordsMargin, 10)
+      const rect = viewer.viewport.imageToViewportRectangle(
+        ...[coords[0] - margin, coords[1] - margin, coords[2] + margin * 2, coords[3] + margin * 2],
+      )
+      viewer.viewport.fitBoundsWithConstraints(rect)
+    },
     initViewer() {
       const self = this
       this.isOSVieverLoaded = false
@@ -104,8 +123,17 @@ export default {
             self.article.regions.length,
             'style:',
             self.getOverlayStyle,
+            self.coords,
           )
           self.isOSVieverLoaded = true
+
+          if (self.hasValidCoords()) {
+            const coords = self.coords.split(',').map(v => parseInt(v, 10))
+            self.drawHighlightRectangle(viewer, coords)
+            self.fitBounds(viewer, coords)
+
+            return
+          }
 
           self.article.regions.forEach(region => {
             // create one overlay per region
@@ -124,29 +152,6 @@ export default {
             viewer.addOverlay(overlay, rect)
             console.info('overlay-region', region.coords)
           })
-
-          const coords = validateOrIgnore('coords', self.coords, false)
-
-          if (coords) {
-            const margin = parseInt(validateOrIgnore('coordsMargin', self.coordsMargin), 10)
-            setTimeout(function() {
-              const args = coords.split(',').map((v, k) => {
-                const v0 = parseInt(v, 10)
-                return k > 1 ? v0 + margin * 2 : v0 - margin
-              })
-              // args[0] = args[0] - margin;
-              // args[1] = args[1] - margin;
-              // args[2] = args[2] + (margin * 2);
-              // args[3] = args[3] + (margin * 2);
-              viewer.viewport.fitBounds(
-                viewer.viewport.imageToViewportRectangle(
-                  // 75,203,1291,626
-                  ...args,
-                  // ...coords.split(','), // -45,163,1351,666
-                ),
-              )
-            }, 0)
-          }
         })
       })
     },
