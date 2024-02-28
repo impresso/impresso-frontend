b65c9f326fd07638209288f46a6f77e18fa369fb
Daniele Guido <gui.daniele@gmail.com>
2024-02-28 09:34:30 +0100

    highlight login/register area

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -49,9 +49,9 @@
         <b-nav-item :to="getRouteWithSearchQuery({ name: 'newspapers' })" active-class="active">
           <span>{{ $t('label_newspapers') }}</span>
         </b-nav-item>
-        <b-nav-item :to="getRouteWithSearchQuery({ name: 'topics' })" active-class="active">
+        <!-- <b-nav-item :to="getRouteWithSearchQuery({ name: 'topics' })" active-class="active">
           <span>{{ $t('label_topics') }}</span>
-        </b-nav-item>
+        </b-nav-item> -->
         <!-- b-nav-item :to="getRouteWithSearchQuery({ name: 'entities' })" active-class="active">
           <span>{{$t("label_entities")}}</span>
         </b-nav-item -->
@@ -179,10 +179,13 @@
 
           <b-dropdown-text class="px-3" v-html="$t('current_version', { version })" />
         </b-nav-item-dropdown>
-        <b-nav-item class="small-caps ml-2" v-else :to="loginRouteParams">
+      </b-navbar-nav>
+      <b-navbar-nav v-if="!user" class="TheHeader__loginArea border border-light mx-2">
+        <b-nav-item class="small-caps" :to="loginRouteParams">
           <span>{{ $t('login') }}</span>
         </b-nav-item>
-        <b-nav-item class="small-caps mx-2" v-if="!user" :to="registerRouteParams">
+        <BNavText class="small-caps mx-1 py-1">|</BNavText>
+        <b-nav-item class="small-caps" :to="registerRouteParams">
           <span>{{ $t('register') }}</span>
         </b-nav-item>
       </b-navbar-nav>
@@ -223,7 +226,8 @@ Icon.register({
   slack: {
     width: 1664,
     height: 1792,
-    d: 'M1519 776q62 0 103.5 40.5t41.5 101.5q0 97-93 130l-172 59 56 167q7 21 7 47 0 59-42 102t-101 43q-47 0-85.5-27t-53.5-72l-55-165-310 106 55 164q8 24 8 47 0 59-42 102t-102 43q-47 0-85-27t-53-72l-55-163-153 53q-29 9-50 9-61 0-101.5-40t-40.5-101q0-47 27.5-85t71.5-53l156-53-105-313-156 54q-26 8-48 8-60 0-101-40.5t-41-100.5q0-47 27.5-85t71.5-53l157-53-53-159q-8-24-8-47 0-60 42-102.5t102-42.5q47 0 85 27t53 72l54 160 310-105-54-160q-8-24-8-47 0-59 42.5-102t101.5-43q47 0 85.5 27.5t53.5 71.5l53 161 162-55q21-6 43-6 60 0 102.5 39.5t42.5 98.5q0 45-30 81.5t-74 51.5l-157 54 105 316 164-56q24-8 46-8zM725 1038l310-105-105-315-310 107z',
+    d:
+      'M1519 776q62 0 103.5 40.5t41.5 101.5q0 97-93 130l-172 59 56 167q7 21 7 47 0 59-42 102t-101 43q-47 0-85.5-27t-53.5-72l-55-165-310 106 55 164q8 24 8 47 0 59-42 102t-102 43q-47 0-85-27t-53-72l-55-163-153 53q-29 9-50 9-61 0-101.5-40t-40.5-101q0-47 27.5-85t71.5-53l156-53-105-313-156 54q-26 8-48 8-60 0-101-40.5t-41-100.5q0-47 27.5-85t71.5-53l157-53-53-159q-8-24-8-47 0-60 42-102.5t102-42.5q47 0 85 27t53 72l54 160 310-105-54-160q-8-24-8-47 0-59 42.5-102t101.5-43q47 0 85.5 27.5t53.5 71.5l53 161 162-55q21-6 43-6 60 0 102.5 39.5t42.5 98.5q0 45-30 81.5t-74 51.5l-157 54 105 316 164-56q24-8 46-8zM725 1038l310-105-105-315-310 107z',
   },
 })
 
@@ -301,7 +305,7 @@ export default {
       return this.$store.state.jobs.totalItems
     },
     runningJobs() {
-      return this.jobs.filter((d) => d.status === 'RUN')
+      return this.jobs.filter(d => d.status === 'RUN')
     },
     currentSearchResults() {
       return this.$store.state.search.paginationTotalRows
@@ -413,7 +417,7 @@ export default {
       handler(jobs) {
         if (jobs.length && this.$refs.ddownJobs) {
           const lastModifiedDate = jobs
-            .map((d) => d.lastModifiedDate.getTime())
+            .map(d => d.lastModifiedDate.getTime())
             .sort()
             .pop()
           if (this.$store.getters['settings/lastNotificationDate'] - lastModifiedDate < 0) {
@@ -445,6 +449,10 @@ export default {
 
 #TheHeader {
   height: 56px;
+}
+
+.TheHeader__loginArea {
+  border-radius: 2px;
 }
 #app-header {
   .Cookie--blood-orange {
@@ -536,8 +544,8 @@ export default {
   .navbar-nav .nav-link {
     padding-top: 0;
     padding-bottom: 0;
-    line-height: 56px;
-    height: 56px;
+    // line-height: 56px;
+    // height: 56px;
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow: hidden;
@@ -708,9 +716,9 @@ export default {
 <i18n>
 {
   "en": {
-    "login": "Login",
-    "register": "Register",
-    "logout": "Logout",
+    "login": "login",
+    "register": "register",
+    "logout": "logout",
     "dashboard": "Dashboard",
     "collections": "Collections",
     "profile": "Profile",
