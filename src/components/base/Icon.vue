8bd4d4ea67b3510688695f04d4fff9194d01aed1
Daniele Guido <gui.daniele@gmail.com>
2024-06-04 16:33:34 +0200

    remove from recipe

--- src/components/base/Icon.vue
+++ src/components/base/Icon.vue
@@ -1,7 +1,19 @@
 <template>
-  <svg :width="width" :height="height" :viewBox="computedSvgViewbox">
+  <svg
+    :width="width * scale"
+    :height="height * scale"
+    :viewBox="computedSvgViewbox"
+    :class="['Icon', props.class]"
+    :style="props.style"
+  >
     <g>
-      <path v-for="path in computedPaths" :key="path.d" :d="path.d" :strokeWidth="strokeWidth" />
+      <path
+        v-for="path in computedPaths"
+        :key="path.d"
+        :d="path.d"
+        :style="path.style"
+        :strokeWidth="strokeWidth"
+      />
       <polygon v-for="poly in computedPolygons" :key="poly.points" :points="poly.points" />
     </g>
   </svg>
@@ -12,7 +24,10 @@ import { computed, defineProps } from 'vue'
 interface IconData {
   width: number
   height: number
-  paths: { d: string }[]
+  paths: {
+    d: string
+    style?: string
+  }[]
   polygons?: { points: string }[]
 }
 
@@ -27,12 +42,35 @@ const Icons: Record<string, IconData> = {
       },
     ],
   },
+  play: {
+    width: 24,
+    height: 24,
+    paths: [
+      {
+        style: 'fill:white; stroke:black; stroke-width:1px;',
+        d:
+          'M6.90588 4.53682C6.50592 4.2998 6 4.58808 6 5.05299V18.947C6 19.4119 6.50592 19.7002 6.90588 19.4632L18.629 12.5162C19.0211 12.2838 19.0211 11.7162 18.629 11.4838L6.90588 4.53682Z',
+      },
+    ],
+  },
 }
 
 const props = defineProps({
   name: {
     type: String,
     default: 'slack',
+  },
+  scale: {
+    type: Number,
+    default: 1,
+  },
+  class: {
+    type: String,
+    default: '',
+  },
+  style: {
+    type: String,
+    default: '',
   },
   paths: {
     type: Array,
