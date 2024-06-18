753b74d654a4bae473aaecc39c092742b01c15b4
Daniele Guido <gui.daniele@gmail.com>
2024-06-18 11:42:33 +0200

    add TutorialMonitor component and its story

---
+++ src/components/TutorialMonitor.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="TutorialMonitor">Hello! {{ title }}</div>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  /** @type {string} */
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
---
+++ src/components/stories/TutorialMonitor.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import TutorialMonitor from '@/components/TutorialMonitor.vue'
+
+const meta: Meta<typeof TutorialMonitor> = {
+  title: 'Components/TutorialMonitor',
+  component: TutorialMonitor,
+  tags: ['autodocs'],
+
+  args: {
+    title: 'Tutorial Monitor'
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    title: 'Tutorial Monitor'
+  }
+}
