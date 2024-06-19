9c427132e29451b820b053a37ebd1e4fe1b78ddc
Daniele Guido <gui.daniele@gmail.com>
2024-06-19 16:25:42 +0200

    Update TutorialTaskPanel.vue

--- src/components/TutorialTaskPanel.vue
+++ src/components/TutorialTaskPanel.vue
@@ -1,18 +1,23 @@
 <template>
-  <CollapsiblePanel class="TutorialTask" v-model="state" :title="task.title"
-    @heightChanged="e => emit('heightChanged', e)">
+  <CollapsiblePanel
+    class="TutorialTask"
+    v-model="state"
+    :title="task.title"
+    @heightChanged="(e) => emit('heightChanged', e)"
+  >
     <template v-slot:header>
       <div class="d-flex align-items-center">
         <div class="TutorialTask__num py-3 px-2 mb-0">task {{ taskNum }}</div>
         <h4 class="mb-0">{{ task.title }}</h4>
       </div>
     </template>
+    <img v-if="task.coverUrl" :src="task.coverUrl" alt="task screencast video" class="img-fluid" />
     <div v-html="task.description"></div>
   </CollapsiblePanel>
 </template>
 
 <script setup lang="ts">
-import { computed, PropType } from 'vue';
+import { computed, PropType } from 'vue'
 import CollapsiblePanel from './CollapsiblePanel.vue'
 import { ITutorialTask } from '@/models/TutorialTask'
 
