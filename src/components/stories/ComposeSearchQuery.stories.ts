e5107a50f1ff9bf4488b929bc028095351e4da0c
Daniele Guido <gui.daniele@gmail.com>
2024-08-01 11:33:15 +0200

    Create ComposeSearchQuery.stories.ts

---
+++ src/components/stories/ComposeSearchQuery.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ComposeSearchQuery from '../ComposeSearchQuery.vue'
+
+const meta: Meta<typeof ComposeSearchQuery> = {
+  title: 'Components/ComposeSearchQuery',
+  component: ComposeSearchQuery,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+  render: args => {
+    return {
+      components: { ComposeSearchQuery },
+      setup() {
+        return { args }
+      },
+      template:
+        '<div style="height: 200px; width: 100%; text-align:end">test<ComposeSearchQuery /></div>'
+    }
+  },
+  decorators: []
+} satisfies Meta<typeof ComposeSearchQuery>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    filters: [
+      {
+        type: 'string',
+        q: ['lune', 'soleil']
+      },
+      {
+        type: 'isFront'
+      }
+    ]
+  }
+}
