db831d06ca2c98e370d1ebf46a6901e4c6d01820
Roman Kalyakin <roman@kalyakin.com>
2024-09-06 09:48:22 +0200

    added non-empty plural option

--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -2,17 +2,17 @@ export default {
   en: {
     connectivityStatus: {
       offline: '⚡ offline!',
-      online: '',
+      online: ''
     },
     untitled: '(untitled)',
     language: 'Language',
     languages: {
       de: 'German',
       fr: 'French',
-      en: 'English',
+      en: 'English'
     },
     counts: {
-      filters: 'no filter | 1 filter | {count} filters',
+      filters: 'no filter | 1 filter | {count} filters'
     },
     buckets: {
       type: {
@@ -26,7 +26,7 @@ export default {
         death_notice: 'obituary (other)',
         weather: 'weather forecast',
         w: 'weather news (other)',
-        picture: 'picture',
+        picture: 'picture'
       },
       language: {
         de: 'German',
@@ -35,34 +35,34 @@ export default {
         lb: 'Luxembourgish',
         'n/a': 'Undefined language',
         nl: 'Dutch',
-        undefined: 'Undefined language',
+        undefined: 'Undefined language'
       },
       accessRight: {
         na: 'not specified (no export)',
         OpenPrivate: 'Personal use',
         Closed: 'Personal use (no export)',
-        OpenPublic: 'Public domain',
+        OpenPublic: 'Public domain'
       },
       country: {
         CH: 'Switzerland',
-        LU: 'Luxembourg',
+        LU: 'Luxembourg'
       },
       dataProvider: {
         SNL: 'Swiss National Library',
         BNL: 'Luxembourg National Library',
         NZZ: 'NZZ',
-        Migros: 'Migros',
+        Migros: 'Migros'
       },
       partner: {
         SNL: 'Swiss National Library',
         BNL: 'Luxembourg National Library',
         NZZ: 'NZZ',
-        Migros: 'Migros',
-      },
+        Migros: 'Migros'
+      }
     },
     op: {
       or: 'or',
-      and: 'and',
+      and: 'and'
     },
     actions: {
       search: 'Search',
@@ -115,32 +115,32 @@ export default {
       useCurrentQuery: 'Use most recent search query',
       addCurrentSearch: 'Add filters from your current search query',
       updateCurrentFilters: 'modify filter in current search',
-      viewTopic: 'go to topic',
+      viewTopic: 'go to topic'
     },
     filters: {
       daterange: {
-        item: 'from <span class="date">{start}</span> to <span class="date">{end}</span>',
-      },
+        item: 'from <span class="date">{start}</span> to <span class="date">{end}</span>'
+      }
     },
     types: {
       location: 'location',
       person: 'person',
-      human: 'human',
+      human: 'human'
     },
     groupBy: {
       images: 'images',
       articles: 'articles',
-      contents: 'contents',
+      contents: 'contents'
     },
     browse: {
       images: 'Search in <span class="number">{count}</span> published images',
-      articles: 'Search in <span class="number">{count}</span> published articles',
+      articles: 'Search in <span class="number">{count}</span> published articles'
     },
     numbers: {
       options: '&nbsp; | (1 option) | ({n} options)',
       moreOptions: '&nbsp; | (1 more option) | ({n} more options)',
       items: '0|<span class="number">1</span> term | <span class="number">{n}</span> terms',
-      clusterSize: ' | single cluster | <span class="number">{n}</span> passages',
+      clusterSize: '&nbsp; | single cluster | <span class="number">{n}</span> passages',
       lexicalOverlap: '<span class="number">{n}%</span> lexical overlap',
       ofPassages:
         '&nbsp; | of <span class="number">{n}</span> passages | of <span class="number">{n}</span> passages',
@@ -173,7 +173,7 @@ export default {
         'no related topics | <span class="number">1</span> related topic | <span class="number">{n}</span> related topics',
       resultsParenthesis: '(empty, no results) | (1 result) | ({n} results)',
       collection: {
-        countItems: '{countItems} saved items',
+        countItems: '{countItems} saved items'
       },
       of: '<span class="number">{index}</span> of <span class="number">{total}</span>',
       unigramMentions:
@@ -183,7 +183,7 @@ export default {
         "no message | * 1 search filter can't be applied. | * {n} search filters can't be applied.",
       ignoredFiltersDetailed:
         // eslint-disable-next-line quotes
-        "no message | * 1 search filter can't be applied ({detail}). | * {n} search filters can't be applied ({detail}).",
+        "no message | * 1 search filter can't be applied ({detail}). | * {n} search filters can't be applied ({detail})."
     },
     dates: {
       lastModifiedDate: 'last modified',
@@ -194,17 +194,17 @@ export default {
       notYetAvailable: 'not yet available',
       fromTo: 'from <span class="date">{from}</span> to <span class="date">{to}</span>',
       allResultsFallBetween:
-        'All results fall between <span class="date">{from}</span> and <span class="date">{to}</span>',
+        'All results fall between <span class="date">{from}</span> and <span class="date">{to}</span>'
     },
     result: {
       label: {
         image: {
-          untitled: '(untitled image)',
+          untitled: '(untitled image)'
         },
         entity: {
-          untitled: '(untitled)',
-        },
-      },
+          untitled: '(untitled)'
+        }
+      }
     },
     pp: 'no pages | p.{pages} | pp.{pages} ({n} pages)',
     ppOf: 'p.{num} of {pages}',
@@ -215,7 +215,7 @@ export default {
     errors: {
       changePassword: {
         wrongcredentials: 'Wrong credentials provided...',
-        NotValidRegex: 'The new password is too easy to guess!',
+        NotValidRegex: 'The new password is too easy to guess!'
       },
       loggedInOnly: 'You need to be logged in to access this content.',
       TypeError: 'TypeError:',
@@ -224,44 +224,44 @@ export default {
       BadRequest: 'Bad request.',
       NotImplemented: 'Not implemented.',
       Conflict: {
-        UsernameExistError: 'This username is already taken!',
+        UsernameExistError: 'This username is already taken!'
       },
       MethodNotAllowed: 'Method not allowed.',
       BadGateway: {
         SequelizeConnectionRefusedError:
           'Please reload the page. Connection troubles (network) with the impresso database',
         SequelizeConnectionError:
-          'Please reload the page. Connection troubles (timeout) with the impresso database',
+          'Please reload the page. Connection troubles (timeout) with the impresso database'
       },
       LoginFailed: 'Login Failed! The username and/or password was incorrect. Please try again.',
       Notauthenticated: 'Please logout, then login again. Authentication failed',
       Invalidauthenticationinformationnostrategyset:
         'Please reload the page, a couple of errors occurred',
       ResetPasswordGeneralError:
-        'The reset password service is not is currently unavailable. Please try again later. <br/><br/>If the problem persists, please contact us at <a href="mailto:info@impresso-project.ch">info@impresso-project.ch</a>. <br/><br/> Message received: <b>{error}</b>',
+        'The reset password service is not is currently unavailable. Please try again later. <br/><br/>If the problem persists, please contact us at <a href="mailto:info@impresso-project.ch">info@impresso-project.ch</a>. <br/><br/> Message received: <b>{error}</b>'
     },
     paths: {
       newspapers: {
-        find: 'while loading list of newspapers',
+        find: 'while loading list of newspapers'
       },
       authentication: {
         remove: 'during logout.',
-        create: 'while checking your credentials',
+        create: 'while checking your credentials'
       },
       jobs: {
-        find: 'while loading user activities',
+        find: 'while loading user activities'
       },
       images: {
-        get: 'while loading contents',
+        get: 'while loading contents'
       },
       entities: {
-        find: 'while loading named entities',
-      },
+        find: 'while loading named entities'
+      }
     },
     pages: {
       powervis: {
-        title: 'XY visualisation',
-      },
+        title: 'XY visualisation'
+      }
     },
     label: {
       useCurrentSearch: 'consider only results matching current search',
@@ -269,17 +269,17 @@ export default {
       year: {
         title: 'Year | Year | Years',
         optionsTitle: 'Year of publication',
-        optionsDescription: 'Total number of articles per year',
+        optionsDescription: 'Total number of articles per year'
       },
       month: {
         title: 'Month | Month | Months',
         optionsTitle: 'Month of publication',
-        optionsDescription: 'Total number of articles per month',
+        optionsDescription: 'Total number of articles per month'
       },
       day: {
         title: 'Day | Day | Days',
         optionsTitle: 'Day of publication',
-        optionsDescription: 'Total number of articles per day',
+        optionsDescription: 'Total number of articles per day'
       },
       type: {
         title: 'Article content type | Article content types | Article content types',
@@ -291,8 +291,8 @@ export default {
         empty: 'No articles types available',
         context: {
           include: 'tagged as',
-          exclude: '<b>NOT</b> tagged as',
-        },
+          exclude: '<b>NOT</b> tagged as'
+        }
       },
       topic: {
         title: 'Topic | Topic | Topics',
@@ -300,7 +300,7 @@ export default {
         filtered: 'results are filtered when:',
         selected: 'filter results if <b>one of {count} selected</b> topic applies',
         description: 'check one or more topics to filter results',
-        empty: 'There is no topic available',
+        empty: 'There is no topic available'
       },
       person: {
         title: 'Person | Person | Persons',
@@ -308,7 +308,7 @@ export default {
         filtered: 'results are filtered when:',
         selected: 'filter results if <b>one of {count} selected</b> people are mentioned',
         description: 'check one or more persons to filter results',
-        empty: 'No person has been recognized in results',
+        empty: 'No person has been recognized in results'
       },
       location: {
         title: 'Location | Location | Locations',
@@ -316,7 +316,7 @@ export default {
         filtered: 'results are filtered when:',
         selected: 'filter results if <b>one of {count} selected</b> locations are mentioned',
         description: 'check one or more locations to filter results',
-        empty: 'There is no location available',
+        empty: 'There is no location available'
       },
       collection: {
         title: 'Collection | Collection | Collections',
@@ -325,7 +325,7 @@ export default {
         selected: 'filter results if <b>one of {count} selected</b> collection applies',
         description: 'check one or more collection to filter results',
         // eslint-disable-next-line quotes
-        empty: "... you haven't saved any result item in your collection",
+        empty: "... you haven't saved any result item in your collection"
       },
       newspaper: {
         title: 'Newspaper | Newspaper | Newspapers',
@@ -333,7 +333,7 @@ export default {
         filtered: 'results are filtered when:',
         selected: 'filter results if they appear in <b>one of {count} selected</b> newspapers',
         description: 'check one or more newspaper to filter results',
-        empty: '(no results)',
+        empty: '(no results)'
       },
       language: {
         title: 'Language | Language | Languages',
@@ -341,7 +341,7 @@ export default {
         filtered: 'results are filtered when:',
         selected: 'filter results if they are written in <b>one of {count} selected</b> languages',
         description: 'check one or more language to filter results',
-        empty: '(no results)',
+        empty: '(no results)'
       },
       country: {
         title: 'Country | Country | Countries',
@@ -350,7 +350,7 @@ export default {
         selected:
           'filter results if they are published in <b>one of {count} selected</b> countries',
         description: 'check one or more countries to filter results',
-        empty: '(no results)',
+        empty: '(no results)'
       },
       accessRight: {
         title: 'Access right | Access right | Access Rights',
@@ -361,8 +361,8 @@ export default {
         empty: '(no results)',
         context: {
           include: 'content available as',
-          exclude: 'content <b>NOT</b> available as',
-        },
+          exclude: 'content <b>NOT</b> available as'
+        }
       },
       partner: {
         title: 'Data Provider | Data Provider | Data Providers',
@@ -370,7 +370,7 @@ export default {
         filtered: 'results are filtered when:',
         selected: 'filter results if they are published in <b>one of {count} selected</b> archives',
         description: 'check one or more data provider to filter results',
-        empty: '(no results)',
+        empty: '(no results)'
       },
       contentLength: {
         title: 'Content Length | Content Length | Content Lengths',
@@ -378,10 +378,10 @@ export default {
         filtered: 'results are filtered when:',
         selected: 'filter results if they are within the range',
         description: 'total number of articles per content length',
-        empty: '(no results)',
+        empty: '(no results)'
       },
       textReuseClusterDayDelta: {
-        filterTitle: 'Time span in days',
+        filterTitle: 'Time span in days'
       },
       textReuseClusterSize: { filterTitle: 'Cluster size' },
       textReuseClusterLexicalOverlap: { filterTitle: 'Lexical overlap' },
@@ -394,9 +394,9 @@ export default {
         empty: '(no results)',
         context: {
           include: 'included in selected clusters',
-          exclude: '<b>NOT</b> included in selected clusters',
-        },
-      },
+          exclude: '<b>NOT</b> included in selected clusters'
+        }
+      }
     },
     tabs: {
       powervis: 'apply filters to visualisation',
@@ -406,41 +406,41 @@ export default {
       articles: '... articles | 1 article | {count} articles',
       entities: {
         observingList: 'observing list (<span style="color: blue">{n}</span>)',
-        browseList: 'browse {n} entities',
-      },
+        browseList: 'browse {n} entities'
+      }
     },
     sort: {
       name: {
         asc: 'order by name, A-Z',
-        desc: 'order by name, Z-A',
+        desc: 'order by name, Z-A'
       },
       publicationDate: {
         asc: 'order by publication date, oldest',
-        desc: 'order by publication date, most recent first',
+        desc: 'order by publication date, most recent first'
       },
       relevanceArticles: {
         asc: 'order by relevance in articles, lowest first',
-        desc: 'order by relevance in articles, highest first',
+        desc: 'order by relevance in articles, highest first'
       },
       countArticles: {
         asc: 'order by n. of articles, lowest first',
-        desc: 'order by n. of articles, highest first',
+        desc: 'order by n. of articles, highest first'
       },
       countMentions: {
         asc: 'order by n. of mentions, lowest first',
-        desc: 'order by n. of mentions, highest first',
+        desc: 'order by n. of mentions, highest first'
       },
       idMentions: {
         asc: 'order by publication date, oldest first',
-        desc: 'order by publication date, most recent first',
+        desc: 'order by publication date, most recent first'
       },
       relevance: {
-        desc: 'relevance',
+        desc: 'relevance'
       },
       date: {
         asc: 'publication date, oldest first',
-        desc: 'publication date, most recent first',
-      },
+        desc: 'publication date, most recent first'
+      }
     },
     sortBy: 'order by',
     more_info: 'More Info',
@@ -448,17 +448,17 @@ export default {
     lexicalOverlap: 'lexical overlap',
     notFound: '(this is an empty list)',
     facsimileView: 'Facsimile',
-    closeReadingView: 'Transcript',
+    closeReadingView: 'Transcript'
   },
   nl: {
     language: 'Taal',
     languages: {
       de: 'Duits',
       fr: 'Frans',
-      en: 'Engels',
-    },
+      en: 'Engels'
+    }
   },
   fr: {
-    language: 'Langue',
-  },
+    language: 'Langue'
+  }
 }
