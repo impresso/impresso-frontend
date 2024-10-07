31cff3b735489e63a168d646080e612f5e53f72e
Roman Kalyakin <roman@kalyakin.com>
2024-10-07 19:57:59 +0200

    no double click (#1325)

---
+++ src/components/legacy/bootstrap/stories/BDropdownItem.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { fn } from '@storybook/test'
+import BNavItemDropdown from '../BNavItemDropdown.vue'
+import BDropdownItem from '../BDropdownItem.vue'
+
+const meta: Meta = {
+  title: 'Components/Legacy/Bootstrap/BNavItemDropdown',
+  component: BNavItemDropdown,
+  subcomponents: { BDropdownItem },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {
+    onClick: fn()
+  },
+  render: args => {
+    return {
+      components: { BNavItemDropdown, BDropdownItem },
+      setup() {
+        return { args }
+      },
+      template: `
+        <b-nav-item-dropdown text="Dropdown" right>
+          <b-dropdown-item v-bind="args">Item 1</b-dropdown-item>
+        </b-nav-item-dropdown>
+      `
+    }
+  }
+} satisfies Meta<typeof BNavItemDropdown>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
