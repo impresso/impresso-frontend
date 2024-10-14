f34b64cd3290f9f535f8cfd544ef70cf8f218ed7
Roman Kalyakin <roman@kalyakin.com>
2024-10-14 08:50:56 +0200

    Improved error capture details (#1335)
    Displays Error ID to be used to provide feedback on UI failures

---
+++ src/services/types.ts
@@ -0,0 +1,23 @@
+import type { ServiceMethods } from '@feathersjs/feathers'
+
+interface ErrorsCollectorPayload {
+  id: string
+  url: string
+  errorMessage: string
+  stackTrace?: string
+  origin?: string
+  className?: string
+  type?: string
+}
+export interface ErrorsCollectorService
+  extends Pick<ServiceMethods<{}, ErrorsCollectorPayload>, 'create'> {}
+
+type UntypedService = Partial<ServiceMethods<{}, any>>
+
+interface UntypedServices {
+  [key: string]: UntypedService
+}
+
+export interface Services extends UntypedServices {
+  ['errors-collector']: ErrorsCollectorService
+}
