68195d59e83ddb95b04ca972d9db1e0de3dca855
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2024-10-25 14:13:51 +0200

    hotfix: Buffer not defined

--- src/components/base/ImgAuthentified.vue
+++ src/components/base/ImgAuthentified.vue
@@ -8,7 +8,8 @@
   </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from 'axios'
+import { Buffer } from 'buffer'
 
 export default {
   data: () => ({
@@ -22,9 +23,9 @@ export default {
   props: {
     height: Number,
     src: String,
-    headers: Object,
+    headers: Object
   },
-  computed:{
+  computed: {
     style() {
       if (this.height) {
         return {
@@ -42,62 +43,62 @@ export default {
       return {
         height: '100%',
         opacity: this.isReady ? 1 : 0,
-        backgroundImage: this.imageSrc ? `url(${this.imageSrc})` : null,
+        backgroundImage: this.imageSrc ? `url(${this.imageSrc})` : null
       }
     }
   },
   mounted() {
-    this.observer.observe(this.$el);
+    this.observer.observe(this.$el)
     const rect = this.$el.getBoundingClientRect()
     this.width = rect.width
   },
   beforeUnmount() {
-    this.observer.disconnect();
+    this.observer.disconnect()
   },
   created() {
-    this.observer = new IntersectionObserver(
-      this.onElementObserved,
-      {
-        root: this.$el,
-        threshold: 1.0,
-      }
-    );
+    this.observer = new IntersectionObserver(this.onElementObserved, {
+      root: this.$el,
+      threshold: 1.0
+    })
   },
   methods: {
     onElementObserved(entries) {
-      entries.forEach(({ target, isIntersecting}) => {
+      entries.forEach(({ target, isIntersecting }) => {
         if (!isIntersecting) {
-          return;
+          return
         }
         this.isVisible = true
-        this.observer.unobserve(target);
-      });
+        this.observer.unobserve(target)
+      })
     }
   },
   watch: {
     isVisible: {
       async handler() {
         const imageUrl = this.src
-        await axios.get(imageUrl, {
-          responseType: 'arraybuffer',
-          headers: this.headers
-        })
+
+        await axios
+          .get(imageUrl, {
+            responseType: 'arraybuffer',
+            headers: this.headers
+          })
           .then(({ data, status, headers }) => {
             if (status === 200) {
               const base64 = Buffer.from(data, 'binary').toString('base64')
 
               this.isReady = true
               // build up the image in mmory
-              var i = new Image();
+              var i = new Image()
 
               i.onload = () => {
                 this.imageWidthHeightRatio = i.naturalWidth / i.naturalHeight
                 this.imageSrc = i.src
-              };
+              }
 
-              i.src = `data:${headers['content-type']};charset=utf-8;base64,${base64}`;
+              i.src = `data:${headers['content-type']};charset=utf-8;base64,${base64}`
             }
-          }).catch((err) => {
+          })
+          .catch(err => {
             // notably CORS
             console.warn(err.message, imageUrl)
             if (err.response) {
@@ -106,33 +107,33 @@ export default {
             } else {
               this.errorMessage = [err.message, err.code].join('. ')
             }
-          });
+          })
       }
     }
   }
 }
 </script>
 <style lang="scss">
-.ImgAuthentified{
+.ImgAuthentified {
   position: relative;
   background-color: transparent;
-  .ImgAuthentified_image{
+  .ImgAuthentified_image {
     width: 100%;
     opacity: 0;
-    transition: opacity .5s ease-in-out;
+    transition: opacity 0.5s ease-in-out;
     background-size: contain;
     background-repeat: no-repeat;
     background-color: transparent;
-    z-index:1;
+    z-index: 1;
     position: relative;
   }
-  .ImgAuthentified_loader{
+  .ImgAuthentified_loader {
     position: absolute;
-    top:0;
-    z-index:0;
-    left:0;
-    right:0;
-    bottom:0;
+    top: 0;
+    z-index: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
   }
 }
 </style>
