95f814fef3a3295549893fecc702fca782881193
Roman Kalyakin <roman@kalyakin.com>
2024-11-20 17:15:54 +0100

    fixed tooltip (#1375)

--- src/components/modules/searchQueriesComparison/DivergingBarsChartPanel.vue
+++ src/components/modules/searchQueriesComparison/DivergingBarsChartPanel.vue
@@ -1,34 +1,61 @@
 <template>
   <div class="diverging-bars-chart-panel">
     <div class="w-100">
-    <div v-for="facet in facets"
-      v-bind:key="`dbc-${facet.id}`"
-      class="text-center">
-      <span class="small-caps font-weight-bold">{{ facet.id }} {{ $tc('numbers.options', facet.items.length, { n: facet.items.length }) }}</span>
-      <diverging-bars-chart :items="facet.items" :round-value-fn="roundValueFn" :scale="scale">
-        <template v-slot="tooltipScope">
-          <div v-if="tooltipScope.tooltip.isActive">
-            <b>{{tooltipScope.tooltip.item.label}}</b> &middot; {{roundValueFn(tooltipScope.tooltip.item.intersection)}}
-            <div v-html="$t('tooltip.common.left', { value: getIntersection(tooltipScope.tooltip.item.intersection, tooltipScope.tooltip.item.left)})"/>
-            <div v-html="$t('tooltip.common.right', { value: getIntersection(tooltipScope.tooltip.item.intersection, tooltipScope.tooltip.item.right)})"/>
-          </div>
-        </template>
-      </diverging-bars-chart>
+      <div v-for="facet in facets" v-bind:key="`dbc-${facet.id}`" class="text-center">
+        <span class="small-caps font-weight-bold"
+          >{{ facet.id }}
+          {{ $tc('numbers.options', facet.items.length, { n: facet.items.length }) }}</span
+        >
+        <diverging-bars-chart :items="facet.items" :round-value-fn="roundValueFn" :scale="scale">
+          <template v-slot="tooltipScope">
+            <div v-if="tooltipScope.tooltip.isActive">
+              <b>{{ tooltipScope.tooltip.item.label }}</b> &middot;
+              {{ roundValueFn(tooltipScope.tooltip.item.intersection) }}
+              <div
+                v-html="
+                  $t('tooltip.common.left', {
+                    value: getIntersection(
+                      tooltipScope.tooltip.item.intersection,
+                      tooltipScope.tooltip.item.left
+                    )
+                  })
+                "
+              />
+              <div
+                v-html="
+                  $t('tooltip.common.right', {
+                    value: getIntersection(
+                      tooltipScope.tooltip.item.intersection,
+                      tooltipScope.tooltip.item.right
+                    )
+                  })
+                "
+              />
+            </div>
+          </template>
+        </diverging-bars-chart>
 
-      <b-button
-        v-if="getNumberOfAvailableBucketsToLoad(facet) > 0"
-        size="sm" variant="outline-secondary" class="mt-2 mr-1"
-        @click="handleLoadMore(facet)">
-        <span>
-          {{ $t('actions.more') }}
-          <span v-html="$tc('numbers.moreOptions', getNumberOfAvailableBucketsToLoad(facet), {
-            n: $n(getNumberOfAvailableBucketsToLoad(facet)),
-          })"/>
-        </span>
-      </b-button>
+        <b-button
+          v-if="getNumberOfAvailableBucketsToLoad(facet) > 0"
+          size="sm"
+          variant="outline-secondary"
+          class="mt-2 mr-1"
+          @click="handleLoadMore(facet)"
+        >
+          <span>
+            {{ $t('actions.more') }}
+            <span
+              v-html="
+                $tc('numbers.moreOptions', getNumberOfAvailableBucketsToLoad(facet), {
+                  n: $n(getNumberOfAvailableBucketsToLoad(facet))
+                })
+              "
+            />
+          </span>
+        </b-button>
 
-      <hr />
-    </div>
+        <hr />
+      </div>
     </div>
   </div>
 </template>
@@ -58,7 +85,7 @@ export default {
     roundValueFn: {
       type: Function,
       default: undefined
-    },
+    }
   },
   methods: {
     /**
@@ -67,7 +94,9 @@ export default {
      * @returns {string}
      */
     getIntersection(intersectionValue, comparableValue) {
-      return `${this.$n(intersectionValue / comparableValue * 100, { notation: 'short' })}%`;
+      const v = (intersectionValue / comparableValue) * 100
+      console.log('VV', v)
+      return `${this.$n(v)}%`
     },
     /**
      * @param {FacetContainer} facet
@@ -102,18 +131,18 @@ export default {
 }
 </i18n>
 <style lang="scss">
-.diverging-bars-chart-panel{
+.diverging-bars-chart-panel {
   width: 100%;
   max-width: 500px;
   margin: 0 auto;
 }
 @media (min-width: 992px) {
-  .diverging-bars-chart-panel{
+  .diverging-bars-chart-panel {
     max-width: 700px;
   }
 }
 @media (min-width: 1200px) {
-  .diverging-bars-chart-panel{
+  .diverging-bars-chart-panel {
     max-width: 960px;
   }
 }
