f9c1314e74ae7ea7186ed0422d551ff28227b5e2
Daniele Guido <gui.daniele@gmail.com>
2025-01-11 15:45:39 +0100

    add more info when a requested has been accepted or rejected

--- src/components/UserChangePlanRequestLabel.vue
+++ src/components/UserChangePlanRequestLabel.vue
@@ -1,10 +1,11 @@
 <template>
   <span
     v-html="
-      $t('label', {
+      $t(`label_${item.status}`, {
         planLabel,
-        status: $t(`status_${item.status}`),
-        date: $d(dateCreatedAsDate, 'short')
+        status: item.status,
+        dateCreated: $d(dateCreatedAsDate, 'short'),
+        dateLastModified: $d(dateLastModifiedAsDate, 'short')
       })
     "
   ></span>
@@ -19,15 +20,15 @@ const props = defineProps<{
   plansLabels: Record<string, string>
 }>()
 const dateCreatedAsDate = new Date(props.item.dateCreated)
+const dateLastModifiedAsDate = new Date(props.item.dateLastModified)
 const planLabel = ref(props.plansLabels[props.item.plan.name])
 </script>
 <i18n lang="json">
 {
   "en": {
-    "label": "You requested to change plan to <b>{planLabel}</b> on <span class='date'>{date}</span>. Your request is <b>{status}</b>.",
-    "status_pending": "pending",
-    "status_approved": "approved",
-    "status_rejected": "rejected"
+    "label_pending": "You requested to change plan to <b>{planLabel}</b> on <span class='date'>{dateCreated}</span>. Your request is <b>pending</b> approval.",
+    "label_approved": "Your request for the <b>{planLabel}</b> has been accepted on <span class='date'>{dateLastModified}</span>.",
+    "label_rejected": "We are sorry, but the request made on <span class='date'>{dateCreated}</span> has been rejected on <span class='date'>{dateLastModified}</span>. Please contact us if you think there has been a mistake."
   }
 }
 </i18n>
