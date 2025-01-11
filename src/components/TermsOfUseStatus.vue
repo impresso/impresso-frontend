8fdfa9f3ff86d706323f06477e876a0e5c41d817
Daniele Guido <gui.daniele@gmail.com>
2025-01-11 01:25:57 +0100

    Update TermsOfUseStatus.vue

--- src/components/TermsOfUseStatus.vue
+++ src/components/TermsOfUseStatus.vue
@@ -1,31 +1,33 @@
 <template>
-  <div v-if="acceptTermsDate">
+  <div v-if="acceptTermsDate && isAuthenticated">
     You have accepted the
-    <LinkToModal v-if="withCallToAction">terms of use</LinkToModal> terms of use:
+    <LinkToModal :view="ViewTermsOfUse" v-if="withCallToAction">terms of use</LinkToModal> terms of
+    use:
     <b>{{ $d(new Date(acceptTermsDate), 'precise') }}</b>
   </div>
-  <div v-else-if="withCallToAction">
-    You have not accepted the new terms of use yet. Please read and accept the
-    <LinkToModal>terms of use</LinkToModal>.
+  <div v-else-if="withCallToAction && !isAuthenticated">
+    <LinkToModal class="text-decoration-underline" :view="ViewTermsOfUse">Terms of use</LinkToModal>
+  </div>
+  <div v-else-if="withCallToAction && isAuthenticated">
+    You have not accepted the <LinkToModal :view="ViewTermsOfUse">Terms of Use</LinkToModal>.
   </div>
   <div v-else>
-    You have not accepted the new terms of use yet. Please read the entire terms of use document
+    You have not accepted the terms of use yet. Please read the entire terms of use document
     carefully and accept it at the bottom to continue.
   </div>
 </template>
 
 <script setup lang="ts">
 import LinkToModal from '@/components/LinkToModal.vue'
+import { ViewTermsOfUse } from '@/stores/views'
 import { useUserStore } from '@/stores/user'
-import { ref } from 'vue'
+import { computed } from 'vue'
 
 defineProps<{
   withCallToAction?: boolean
 }>()
 
-const acceptTermsDate = ref(useUserStore().acceptTermsDate)
-// watch changes on pinia store
-useUserStore().$subscribe((mutation, state) => {
-  acceptTermsDate.value = state.acceptTermsDate
-})
+const userStore = useUserStore()
+const acceptTermsDate = computed(() => userStore.acceptTermsDate)
+const isAuthenticated = computed(() => !!userStore.userData)
 </script>
