df830ebc2aada5d3042ded655c687b73485e628f
Daniele Guido <gui.daniele@gmail.com>
2025-01-17 19:06:22 +0100

    Create InfoModal.vue

---
+++ src/components/InfoModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <Modal
+    :show="isVisible"
+    :title="title"
+    modalClasses="InfoModal"
+    :dialogClass="props.dialogClass"
+    @close="dismiss"
+  >
+    <h1>{{ title }}</h1>
+    <slot></slot>
+    <template v-slot:modal-footer>
+      <button type="button" class="btn btn-sm btn-outline-secondary" @click="dismiss">close</button>
+    </template>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from './base/Modal.vue'
+
+const props = defineProps<{
+  isVisible: boolean
+  title: string
+  dialogClass?: string
+}>()
+
+const emit = defineEmits(['close'])
+
+function dismiss() {
+  emit('close')
+}
+</script>
