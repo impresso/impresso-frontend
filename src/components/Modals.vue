a63172cec0179410d6f188920d98636c39148345
Daniele Guido <gui.daniele@gmail.com>
2025-01-17 19:06:12 +0100

    get fresh state on app mounted

--- src/components/Modals.vue
+++ src/components/Modals.vue
@@ -1,21 +1,58 @@
 <template>
   <div class="Modals position-fixed top-0 end-0" style="z-index: var(--z-index-modals)">
+    <!--  -->
     <TermsOfUseModal :isVisible="view === ViewTermsOfUse" @dismiss="() => resetView()">
-      <template v-slot:accept-terms-of-use>
-        <AcceptTermsOfUse v-if="isAuthenticated" />
-      </template>
-      <template v-slot:terms-of-use-status>
+      <template v-slot:terms-of-use-status v-if="termsOfUseResponse.status === 'success'">
         <Alert
-          type="warning"
-          v-if="isAuthenticated"
+          :type="acceptTermsDate || acceptTermsDateOnLocalStorage ? 'info' : 'warning'"
           class="bg-info mb-3"
           style="position: sticky; top: 0"
         >
           <TermsOfUseStatus />
         </Alert>
       </template>
+      <template v-slot:accept-terms-of-use>
+        <AcceptTermsOfUse
+          :is-loading="
+            termsOfUseResponse.status === 'idle' || termsOfUseResponse.status === 'loading'
+          "
+          :checked="!!acceptTermsDate"
+          :disabled="!!acceptTermsDate"
+          @change="
+            (event: Event) => {
+              const isChecked = (event.target as HTMLInputElement).checked
+              console.debug('[Models] AcceptTermsOfUse@onChange', isChecked)
+              if (isChecked) {
+                patchAcceptTermsDate()
+              }
+            }
+          "
+        />
+      </template>
     </TermsOfUseModal>
-
+    <InfoModal
+      :isVisible="view === ViewInfoModal"
+      :title="$t('Verbose Info')"
+      @dismiss="() => resetView()"
+    >
+      <div class="row">
+        <div class="col-6">
+          <h3>{{ $t('User Plan') }}</h3>
+          <pre>{{ userPlan }}</pre>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h2>{{ $t('User Accept Terms Date Local') }}</h2>
+          <pre>{{ acceptTermsDateOnLocalStorage }}</pre>
+        </div>
+        <div class="col">
+          <h2>{{ $t('User Accept Terms Date') }}</h2>
+          <pre>{{ acceptTermsDate }}</pre>
+        </div>
+      </div>
+    </InfoModal>
+    <!--  -->
     <ChangePlanModal
       :available-plans="AvailablePlans"
       :available-plans-labels="PlanLabels"
@@ -34,31 +71,51 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import TermsOfUseModal from './TermsOfUseModal.vue'
 import ChangePlanModal from './ChangePlanModal.vue'
-import type { UserChangePlanRequest } from '@/services/types.ts'
+import type { TermsOfUse, UserChangePlanRequest } from '@/services/types.ts'
 import {
   Views,
   useViewsStore,
   ViewTermsOfUse,
   ViewChangePlanRequest,
-  ViewConfirmChangePlanRequest
+  ViewConfirmChangePlanRequest,
+  ViewInfoModal
 } from '@/stores/views'
-import { userChangePlanRequest as userChangePlanRequestService } from '@/services'
+import {
+  userChangePlanRequest as userChangePlanRequestService,
+  termsOfUse as termsOfUseService
+} from '@/services'
 import type { FeathersError } from '@feathersjs/errors'
 import { useUserStore } from '@/stores/user'
 import { AvailablePlans, PlanLabels } from '@/constants'
 import TermsOfUseStatus from './TermsOfUseStatus.vue'
 import AcceptTermsOfUse from './AcceptTermsOfUse.vue'
 import Alert from './Alert.vue'
+import InfoModal from './InfoModal.vue'
+
 const store = useViewsStore()
 const userStore = useUserStore()
 const userPlan = computed(() => userStore.userPlan)
 const view = ref<(typeof Views)[number] | null>(store.view)
+const isLoading = ref(false)
 const isAuthenticated = computed(() => !!userStore.userData)
+// date of accepting the ToU on localStorage
+const acceptTermsDateOnLocalStorage = computed(() => userStore.acceptTermsDateOnLocalStorage)
+// date of accepting the ToU on current store (sort of cached value)
+const acceptTermsDate = computed(() => userStore.acceptTermsDate)
+
 const userChangePlanRequestResponse = ref<{
   data: UserChangePlanRequest | null
+  status: 'idle' | 'loading' | 'success' | 'error'
+}>({
+  status: 'idle',
+  data: null
+})
+
+const termsOfUseResponse = ref<{
+  data: TermsOfUse
   status: 'idle' | 'loading' | 'success' | 'error'
 }>({
   status: 'idle',
@@ -79,6 +136,54 @@ watch(
     }
   }
 )
+
+const fetchAcceptTermsDate = async () => {
+  /**
+   * Fetches the date when the user accepted the terms of use.
+   *
+   * This method updates the `acceptTermsDateResponse` ref with the status of the request.
+   * It sets the status to 'loading' before making the request, and updates it to 'success' or 'error' based on the outcome.
+   *
+   * @returns {Promise<void>}
+   */
+
+  return termsOfUseService
+    .find()
+    .then((data: TermsOfUse) => {
+      console.debug('[Modals] fetchAcceptTermsDate call termsOfUseService.find() success:', data)
+      termsOfUseResponse.value = { data, status: 'success' }
+      userStore.setAcceptTermsDate(
+        data.dateAcceptedTerms ? new Date(data.dateAcceptedTerms).toISOString() : null
+      )
+    })
+    .catch((err: FeathersError) => {
+      console.error(
+        '[Modals] fetchAcceptTermsDate call termsOfUseService.find() error:',
+        err.message,
+        err.data,
+        err.code
+      )
+      termsOfUseResponse.value = { data: null, status: 'error' }
+    })
+}
+
+const patchAcceptTermsDate = async () => {
+  termsOfUseService
+    .patch(null, {})
+    .then(data => {
+      console.debug(
+        '[Modals] patchAcceptTermsDate call termsOfUseService.patch() success:',
+        data.dateAcceptedTerms
+      )
+      // update with the latest value
+      userStore.setAcceptTermsDate(
+        data.dateAcceptedTerms ? new Date(data.dateAcceptedTerms).toISOString() : null
+      )
+    })
+    .finally(() => {
+      isLoading.value = false
+    })
+}
 const fetchUserPlanChangeRequest = async () => {
   /**
    * Fetches the current user plan change request.
@@ -98,7 +203,7 @@ const fetchUserPlanChangeRequest = async () => {
     })
     .catch((err: FeathersError) => {
       console.error(
-        '[ChangePlanModal] @useEffect - userChangePlanRequestService',
+        '[Modals] @useEffect - userChangePlanRequestService',
         err.message,
         err.data,
         err.code
@@ -121,4 +226,8 @@ const patchCurrentPlanChangeRequest = async ({ plan }) => {
       console.error('[ChangePlanModal] create', err.message, err.data)
     })
 }
+onMounted(() => {
+  console.debug('[Modals] @mounted')
+  fetchAcceptTermsDate()
+})
 </script>
