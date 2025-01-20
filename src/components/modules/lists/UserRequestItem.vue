f09958444e43ed52304afbefb0192135e0ff83bc
Daniele Guido <gui.daniele@gmail.com>
2025-01-20 16:40:52 +0100

    first version of content items for User REequest and Subscription Datasets

---
+++ src/components/modules/lists/UserRequestItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="UserRequestItem d-flex justify-content-between align-items-center">
+    <div>
+      <b>{{ item.subscription.name }}</b>
+      <div>requested on {{ $d(item.dateCreated, 'short') }}</div>
+    </div>
+    <div class="text-muted">
+      {{ item.status }}
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { UserRequest } from '@/services/types'
+
+defineProps<{ item: UserRequest }>()
+</script>
