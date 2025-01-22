96b5c1e1dcfb92b74a36f336c9992c54040007db
Daniele Guido <gui.daniele@gmail.com>
2025-01-22 17:07:59 +0100

    Create PlansModalFeatureCard.vue

---
+++ src/components/modules/PlansModalFeatureCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    PlansModalFeatureCard
+    {{ feature }}
+  </div>
+</template>
+<script lang="ts" setup>
+type PlanFeature = {
+  title: string
+  status: string
+  iconColor: string
+  ref: string
+}
+
+interface PlansModalFeatureCardProps {
+  feature: PlanFeature
+}
+defineProps<PlansModalFeatureCardProps>()
+</script>
