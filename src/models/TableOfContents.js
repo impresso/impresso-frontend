931b7c6dfc58ce1531e4a59176f05b51438a8e87
Daniele Guido <gui.daniele@gmail.com>
2025-02-03 21:48:26 +0100

    add newspaper to tableOfContent

--- src/models/TableOfContents.js
+++ src/models/TableOfContents.js
@@ -1,12 +1,12 @@
-import ArticleBase from './ArticleBase';
+import ArticleBase from './ArticleBase'
 
 export default class TableOfContents {
-  constructor({
-    articles=[],
-    images=[]
-  }) {
-    this.articles = articles
-      .map(d => new ArticleBase(d))
-    this.images = images;
+  constructor({ articles = [], images = [], newspaper = {} }) {
+    this.articles = articles.map(d => new ArticleBase(d))
+    this.images = images
+    this.newspaper = {
+      id: newspaper.uid,
+      type: 'newspaper'
+    }
   }
 }
