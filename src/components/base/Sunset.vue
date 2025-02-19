8e885776c85b04fc243ab76a05eb5596234cd007
Daniele Guido <gui.daniele@gmail.com>
2025-02-19 09:29:54 +0100

    hotfix: simplify code for account picture

--- src/components/base/Sunset.vue
+++ src/components/base/Sunset.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 export interface SunsetProps {
   radius: number
   colors: string[]
@@ -11,27 +13,34 @@ const props = withDefaults(defineProps<SunsetProps>(), {
   gradientId: 'defaultGradient',
   transitionSize: 15 // Percentage of space used for soft edges
 })
+
+const midpoint = Math.ceil(props.colors.length / 2)
+
+const topColors = computed(() => props.colors.slice(0, midpoint))
+const bottomColors = computed(() => props.colors.slice(midpoint - 1, props.colors.length))
 </script>
 
 <template>
   <svg :width="props.radius * 2" :height="props.radius * 2" viewBox="0 0 100 100">
     <defs>
-      <linearGradient :id="props.gradientId" x1="50%" y1="0%" x2="50%" y2="100%">
-        <template v-for="(color, index) in props.colors" :key="index">
-          <stop :offset="`${(index / props.colors.length) * 100}%`" :stop-color="color" />
-          <stop
-            v-if="index < props.colors.length - 1"
-            :offset="`${((index + 1) / props.colors.length) * 100 - props.transitionSize}%`"
-            :stop-color="color"
-          />
-          <stop
-            v-if="index < props.colors.length - 1"
-            :offset="`${((index + 1) / props.colors.length) * 100}%`"
-            :stop-color="props.colors[index + 1]"
-          />
+      <!-- Top Gradient -->
+      <radialGradient :id="`${props.gradientId}-top`" cx="50%" cy="0%" r="150%">
+        <template v-for="(color, index) in topColors" :key="index">
+          <stop :offset="`${(index / (topColors.length - 1)) * 80}%`" :stop-color="color" />
         </template>
-      </linearGradient>
+      </radialGradient>
+
+      <!-- Bottom Gradient -->
+      <radialGradient :id="`${props.gradientId}-bottom`" cx="50%" cy="100%" r="150%">
+        <template v-for="(color, index) in bottomColors" :key="index">
+          <stop :offset="`${(index / (bottomColors.length - 1)) * 100}%`" :stop-color="color" />
+        </template>
+      </radialGradient>
     </defs>
-    <circle cx="50" cy="50" r="50" :fill="`url(#${props.gradientId})`" />
+    <!-- Top Half -->
+    <path d="M 0,50 A 50,50 0 1,0 100,50 Z" :fill="`url(#${props.gradientId}-bottom)`" />
+
+    <!-- Bottom Half -->
+    <path d="M 100,50 A 50,50 0 1,0 0,50 Z" :fill="`url(#${props.gradientId}-top)`" />
   </svg>
 </template>
