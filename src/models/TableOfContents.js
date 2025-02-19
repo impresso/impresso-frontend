8976d432e779aeccb1c84aaf5fc1b363ec50d9d2
Daniele Guido <gui.daniele@gmail.com>
2025-02-19 09:31:08 +0100

    hotfix: sometimes table of contents articles do not have pages...

--- src/models/TableOfContents.js
+++ src/models/TableOfContents.js
@@ -4,7 +4,13 @@ export default class TableOfContents {
   constructor({ articles = [], images = [], newspaper = {} }) {
     this.articles = articles
       .map(d => new ArticleBase(d))
-      .sort((a, b) => a.pages[0].num - b.pages[0].num)
+      .sort((a, b) => {
+        if (!a.pages[0] || !b.pages[0]) {
+          console.warn('[TableOfContents] a.pages[0] or b.pages is undefined', a, b)
+          return a.uid.localeCompare(b.uid)
+        }
+        a.pages[0].num - b.pages[0].num
+      })
     this.images = images
     this.newspaper = {
       id: newspaper.uid,
