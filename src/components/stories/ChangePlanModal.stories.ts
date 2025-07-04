2f39084deaabc206b83912e64a14295e0d199d53
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2025-07-04 14:56:24 +0200

    Update ChangePlanModal.stories.ts

--- src/components/stories/ChangePlanModal.stories.ts
+++ src/components/stories/ChangePlanModal.stories.ts
@@ -1,9 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import ChangePlanModal from '@/components/ChangePlanModal.vue'
-import { AvailablePlans, PlanLabels } from '@/constants'
+import { AvailablePlans, PlanLabels, PlanNone } from '@/constants'
 import { UserChangePlanRequest } from '@/services/types'
 import type { ChangePlanModalProps } from '@/components/ChangePlanModal.vue'
 import { http, HttpResponse } from 'msw'
+import { useUserStore } from '@/stores/user'
+import { computed, ref } from 'vue'
 
 const meta: Meta<typeof ChangePlanModal> = {
   title: 'Components/ChangePlanModal',
@@ -37,6 +39,94 @@ export const Default: Story = {
     }
   }
 }
+
+export const WithConditionalAutomaticPopup: Story = {
+  args: {
+    title: 'Change your plan',
+    automaticPopup: true,
+    planLabels: PlanLabels,
+    availablePlans: AvailablePlans
+  },
+  render: args => {
+    return {
+      setup() {
+        const userStore = useUserStore()
+        userStore.$reset() // 👈 resets to initial state
+        // userPlan is computed from the user groups as getter in the user store
+        const userPlan = computed(() => userStore.userPlan)
+        // isLoggedIn is true if the userData is not false (sic)
+        const isLoggedIn = computed(() => userStore.userData)
+        const hasPendingChangePlanRequest = computed(() => userStore.hasPendingChangePlanRequest)
+        // view is normally stored in a pinia store, but here we use a local ref for simplicity
+        const view = ref(null)
+        // initSequenceDone is a boolean that indicates if the initial sequence of the notifications store is done,
+        // here we use again a ref for simplicity
+        const isInitSequenceDone = ref(true)
+        const showChangePlanToLegacyUsers = computed(() => {
+          // if the user is logged in and has a plan, show the change plan modal
+          return (
+            view.value === null &&
+            isInitSequenceDone.value &&
+            isLoggedIn.value &&
+            userPlan.value === PlanNone &&
+            !userStore.hasPendingChangePlanRequest
+          )
+        })
+
+        return { args, view, showChangePlanToLegacyUsers, userPlan, hasPendingChangePlanRequest }
+      },
+      components: { ChangePlanModal },
+      template: `<div style="height: 400px; width: 100%">
+      <p v-if="!showChangePlanToLegacyUsers">
+        Values:
+        <br />
+        userPlan: {{ userPlan }}
+        <br />
+        isLoggedIn: {{ isLoggedIn }}
+        <br />
+        hasPendingChangePlanRequest: {{ hasPendingChangePlanRequest }}
+        <br />
+        view: {{ view }}  
+        <br />
+        isInitSequenceDone: {{ isInitSequenceDone }}
+        <br />
+        showChangePlanToLegacyUsers: {{ showChangePlanToLegacyUsers }}
+        
+      </p>
+          <ChangePlanModal
+      :show="showChangePlanToLegacyUsers"
+      :title="$t(userPlan === PlanNone ? 'Select a plan' : 'Change Plan')"
+      @dismiss="resetView"
+      @success="() => changeView(ViewConfirmChangePlanRequest)"
+      :submitLabel="$t('Confirm your plan selection')"
+    >
+      <div v-if="userPlan !== PlanGuest && userPlan !== PlanNone">
+        <p>
+          You can request to change your plan any time if your situation changed. More information
+          about the plans can be found in the
+          <LinkToModal :view="ViewPlans">Plans page</LinkToModal>.
+        </p>
+        <p v-if="userPlan !== PlanNone">
+          Your current plan is <b> {{ PlanLabels[userPlan] }} </b>. <br />
+          Please select the plan you want to change to:
+        </p>
+      </div>
+      <div v-if="userPlan === PlanNone">
+        <h4>Important Update: Please Select Your Plan</h4>
+        <p>
+          We've recently updated our website's architecture and Terms of Use to improve how you
+          access transcripts, facsimiles, and audio files.
+        </p>
+        <p>
+          To ensure continued access to these features, please take a moment to select the plan that
+          best reflects your status (Student or Academic Staff).
+        </p>
+      </div>
+    </ChangePlanModal></div>`
+    }
+  }
+}
+
 export const WithPendingRequest: Story = {
   args: {
     show: true,
