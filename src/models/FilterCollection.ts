ca3310017019d0c30d360ef6593cbabb4c4d414e
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2025-10-07 10:35:18 +0200

    fix warning missing identifier in FilterMonitor component

--- src/models/FilterCollection.ts
+++ src/models/FilterCollection.ts
@@ -4,15 +4,15 @@
  * @param {String} query The search query
  */
 
-import FilterItems from '@/models/FilterItems';
-import Collection from '@/models/Collection';
+import FilterItems from '@/models/FilterItems'
+import Collection from '@/models/Collection'
 
 export default class FilterCollection extends FilterItems {
   setItems(items = []) {
-    this.items = items.map((d) => {
-      const item = new Collection(d);
-      item.checked = true;
-      return item;
-    });
+    this.items = items.map(d => {
+      const item = typeof d === 'object' ? new Collection(d) : new Collection({ uid: String(d) })
+      item.checked = true
+      return item
+    })
   }
 }
