eb4aed37a7e2d44683d84f47d0264f7c1c63632f
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2025-12-02 10:45:52 +0100

    use i18N for messages

--- src/components/specialMembership/SpecialMembershipButton.vue
+++ src/components/specialMembership/SpecialMembershipButton.vue
@@ -1,22 +1,38 @@
 <template>
-  <button
-    :class="`UserSpecialmembershipRequestButton btn btn-sm btn-outline-primary ${className}`"
-    @click="viewStore.openSpecialMembershipModal(item)"
-  >
+  <button class="UserSpecialmembershipRequestButton" @click="onClickHandler">
     <span v-if="item">{{ item.title }}</span>
-    <span v-else>Request Special Membership Access</span>
+    <span v-else>{{ $t('actions.viewSpecialMembership') }}</span>
   </button>
 </template>
-
+<i18n lang="json">
+{
+  "en": {
+    "actions": {
+      "viewSpecialMembership": "View Special Membership"
+    }
+  }
+}
+</i18n>
 <script setup lang="ts">
 import { SpecialMembershipAccess } from '@/services/types'
 import { useViewsStore } from '@/stores/views'
 
 export interface SpecialMembershipButtonProps {
   item?: SpecialMembershipAccess
-  className?: string
 }
 
-defineProps<SpecialMembershipButtonProps>()
+const props = defineProps<SpecialMembershipButtonProps>()
 const viewStore = useViewsStore()
+const emit = defineEmits<{
+  (e: 'click', event: MouseEvent): void
+}>()
+
+const onClickHandler = (e: MouseEvent) => {
+  if (props.item) {
+    viewStore.openSpecialMembershipModal(props.item)
+  } else {
+    viewStore.openSpecialMembershipModal()
+  }
+  emit('click', e)
+}
 </script>
