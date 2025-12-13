7ed8de4edf9a18de220fb4a2507cc8fe76eb1c4d
Daniele Guido <gui.daniele@gmail.com>
2025-12-13 23:18:55 +0100

    Add Storybook stories and mock data for EmbeddingsSearch
    Introduces a mock data file for word embeddings and a Storybook story for the EmbeddingsSearch component. The story uses MSW to mock API responses, enabling isolated UI testing with sample data.

---
+++ .storybook/mockData/embeddings.ts
@@ -0,0 +1,102 @@
+export const MockWordEmbeddings = [
+  {
+    word: 'grogne',
+    languageCode: 'fr'
+  },
+  {
+    word: 'rogne',
+    languageCode: 'fr'
+  },
+  {
+    word: 'bisbille',
+    languageCode: 'fr'
+  },
+  {
+    word: 'grognent',
+    languageCode: 'fr'
+  },
+  {
+    word: 'rebiffent',
+    languageCode: 'fr'
+  },
+  {
+    word: 'fustigent',
+    languageCode: 'fr'
+  },
+  {
+    word: 'sinistrose',
+    languageCode: 'fr'
+  },
+  {
+    word: 'fâchent',
+    languageCode: 'fr'
+  },
+  {
+    word: 'grognes',
+    languageCode: 'fr'
+  },
+  {
+    word: 'râlent',
+    languageCode: 'fr'
+  },
+  {
+    word: 'guéguerre',
+    languageCode: 'fr'
+  },
+  {
+    word: 'frilosité',
+    languageCode: 'fr'
+  },
+  {
+    word: 'pingrerie',
+    languageCode: 'fr'
+  },
+  {
+    word: 'grincer',
+    languageCode: 'fr'
+  },
+  {
+    word: 'tollé',
+    languageCode: 'fr'
+  },
+  {
+    word: 'décolère',
+    languageCode: 'fr'
+  },
+  {
+    word: 'chienlit',
+    languageCode: 'fr'
+  },
+  {
+    word: 'renâcle',
+    languageCode: 'fr'
+  },
+  {
+    word: 'renâclent',
+    languageCode: 'fr'
+  },
+  {
+    word: 'excédés',
+    languageCode: 'fr'
+  },
+  {
+    word: 'fustige',
+    languageCode: 'fr'
+  },
+  {
+    word: 'fustiger',
+    languageCode: 'fr'
+  },
+  {
+    word: 'agace',
+    languageCode: 'fr'
+  },
+  {
+    word: 'lebol',
+    languageCode: 'fr'
+  },
+  {
+    word: 'trinque',
+    languageCode: 'fr'
+  }
+]
