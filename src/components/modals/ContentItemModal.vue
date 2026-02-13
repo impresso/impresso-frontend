56c7afbb4c512bd1a725a39472ee9bcf889d058b
Daniele Guido <gui.daniele@gmail.com>
2026-02-13 17:05:19 +0100

    Update ContentItemModal.vue

--- src/components/modals/ContentItemModal.vue
+++ src/components/modals/ContentItemModal.vue
@@ -3,46 +3,32 @@
     :is-visible="isVisible"
     modalClass="InfoModal ContentItemModal"
     dialog-class="modal-dialog-scrollable modal-xl"
+    :modal-title="$t('contentItemCard')"
     bodyClass="pt-0 px-3"
     @close="emit('dismiss')"
   >
-    <ContentItem :item="item" showMeta class="p-1" />
-    <!-- choice of mode view -->
-    <b-tabs pills class="mb-4 mt-0 FeedbackModal__tabs">
-      <template v-slot:tabs-end>
-        <b-nav-item v-for="availableMode in AvailableModes" :key="availableMode">
-          <button
-            size="sm"
-            class="w-100 btn btn-transparent nav-link"
-            :class="{ active: mode === availableMode }"
-            @click="mode = availableMode"
-          >
-            {{ $t(availableMode) }}
-          </button>
-        </b-nav-item>
-      </template>
-    </b-tabs>
+    <template v-slot:modal-header-extra>
+      <section class="px-2">
+        <ContentItem :item="item" showMeta class="p-1" />
+        <!-- choice of mode view -->
+        <b-tabs pills class="mx-0 px-2 mt-0">
+          <template v-slot:tabs-end>
+            <b-nav-item v-for="availableMode in AvailableModes" :key="availableMode" class="mx-2">
+              <button
+                size="sm"
+                class="btn btn-transparent nav-link very-small-caps"
+                :class="{ active: mode === availableMode }"
+                @click="mode = availableMode"
+              >
+                {{ $t(availableMode) }}
+              </button>
+            </b-nav-item>
+          </template>
+        </b-tabs>
+      </section>
+    </template>
     <section v-if="mode === 'card'">
-      <h3 class="font-weight-bold font-size-inherit mt-3">Cite as</h3>
-      <ContentItemCitation :item="item" class="p-2 bg-light border rounded small shadow-sm" />
-      <div class="mt-4">
-        <h3 class="font-weight-bold font-size-inherit">Full metadata</h3>
-        <div class="container-fluid m-0">
-          <div class="row">
-            <div
-              v-for="(field, index) in AvailableFields"
-              :key="field"
-              class="col-md-12 col-lg-6 col-xl-3"
-            >
-              <div class="py-2 small">
-                <div class="font-weight-bold">{{ field }}</div>
-                <span>{{ fields[index] }}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <pre>{{ JSON.stringify(item, null, 2) }}</pre>
-      </div>
+      <ContentItemCard :item="item" />
     </section>
 
     <section v-else-if="mode === 'transcript'">
@@ -79,6 +65,7 @@ import { computed, ref } from 'vue'
 import ContentItem from '../modules/lists/ContentItem.vue'
 import ContentItemCitation from '../ContentItemCitation.vue'
 import BFormCheckbox from '../legacy/bootstrap/BFormCheckbox.vue'
+import ContentItemCard from '../contentItem/ContentItemCard.vue'
 
 /** Content Item to display metadata for */
 export type ContentItemModalProps = {
@@ -179,14 +166,14 @@ const emit = defineEmits<{
 }>()
 </script>
 
-<style scoped lang="scss">
-.ContentItemModal {
-  pre {
-    max-height: 600px;
-    overflow-y: auto;
-    font-size: 0.85rem;
-    word-wrap: break-word;
-    white-space: pre-wrap;
+<i18n lang="json">
+{
+  "en": {
+    "close": "Close",
+    "contentItemCard": "Content Item Card",
+    "card": "Card view",
+    "transcript": "Transcript view",
+    "transcriptModeParagraphs": "Split transcript into paragraphs"
   }
 }
-</style>
+</i18n>
