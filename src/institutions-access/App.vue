e685fe39df3d831d50badc3804e8167156e2bb70
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2026-02-23 16:45:38 +0100

    add notifications in Insitution access app component

--- src/institutions-access/App.vue
+++ src/institutions-access/App.vue
@@ -4,9 +4,11 @@
     <main class="mx-lg-5 mx-md-2" style="padding-top: 100px">
       <router-view />
     </main>
+    <Toaster />
   </div>
 </template>
 <script setup lang="ts">
+import Toaster from '@/components/base/Toaster.vue'
 import Header from './components/Header.vue'
 </script>
 
