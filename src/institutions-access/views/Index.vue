9c0058ea2e4d4c6ab39e177892f438c368a794df
Daniele Guido <gui.daniele@gmail.com>
2026-02-25 11:35:54 +0100

    Add search, sorting & toggle modal to requests view
    Split the layout into a main list and a sidebar and add search/sort controls and a status-toggle UI to the special membership requests view. Replaced the inline params with a computed userSpecialMembershipRequestsReviewsServiceParams (now includes limit, order_by and q driven by reactive refs), added BSearchInputForm and an order-by dropdown in the header, and wired performSearch to update the query. Each SpecialMembershipRequestReviewItem now exposes an actions slot with a "Toggle Status" button, backed by ToggleSpecialMembershipRequestStatusModal plus show/hide and itemToUpdate state. Also added related imports, i18n keys, and a Reviewer card in the sidebar. Note: default limit changed from 10 to 2 in the new params.

--- src/institutions-access/views/Index.vue
+++ src/institutions-access/views/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-10 offset-1">
+    <div class="col-lg-8 col-xl-7 order-2 order-lg-1 offset-xl-1">
       <Card>
         <template #header>
           <h4 class="p-2 m-0 font-weight-bold">Pending Special Membership requests</h4>
@@ -10,11 +10,8 @@
           :list-is-empty-message="$t('listIsEmpty')"
           :service="userSpecialMembershipRequestsReviewsService"
           :title="$t('listTitle')"
-          :params="{
-            query: {
-              limit: 10
-            }
-          }"
+          items-class=""
+          :params="userSpecialMembershipRequestsReviewsServiceParams"
         >
           <template #header="{ total }">
             <div class="p-2 pb-3 d-flex gap-2 justify-content-between align-items-center">
@@ -22,7 +19,7 @@
               <span v-html="$tc('numbers.itemsGeneric', total, { n: $n(total) })"></span>
             </div>
             <div class="container-fluid">
-              <div class="row pb-2 small align-items-center">
+              <div class="row pb-2 small align-items-center font-weight-bold">
                 <div class="col-3">
                   {{ $t('userSpecialMembershipRequestsRequester') }}
                 </div>
@@ -32,19 +29,41 @@
                 <div class="col-3">
                   {{ $t('specialMembershipAccessTitle') }}
                 </div>
-                <div class="col-1">
-                  {{ $t('userHasAlreadySpecialMembership') }}
-                </div>
-                <div class="col-2">
+
+                <div class="col-3">
                   {{ $t('userSpecialMembershipRequestsStatus') }}
                 </div>
               </div>
             </div>
           </template>
-
+          <template #beforeHeader>
+            <div class="container-fluid">
+              <div class="row pb-2 small align-items-center font-weight-bold">
+                <div class="col-md-4">
+                  <BSearchInputForm @submit="performSearch" />
+                </div>
+                <div class="col-md-4">
+                  <label class="small-caps mr-2">{{ $t('sortBy') }}</label>
+                  <i-dropdown
+                    v-model="orderBy"
+                    :options="
+                      orderByOptions.map((value: string) => ({
+                        value,
+                        text: $t(`orderBy_${value}`)
+                      }))
+                    "
+                    class="mr-auto"
+                    size="sm"
+                    variant="outline-primary"
+                  ></i-dropdown>
+                </div>
+              </div>
+            </div>
+          </template>
           <template #default="{ items }">
-            <div class="container">
-              <div
+            <div class="container-fluid">
+              <SpecialMembershipRequestReviewItem
+                :item="item"
                 class="py-3"
                 v-for="(item, index) in items"
                 :key="item.id"
@@ -52,11 +71,34 @@
                   'border-bottom border-dark ': index < items.length - 1
                 }"
               >
-                <SpecialMembershipRequestReviewItem :item="item" />
-              </div>
+                <template #actions>
+                  <button
+                    class="btn btn-sm btn-outline-secondary mt-2"
+                    @click="showToggleStatusModal(item)"
+                  >
+                    {{ $t('actions.toggleStatus') }}
+                  </button>
+                </template>
+              </SpecialMembershipRequestReviewItem>
             </div>
           </template>
         </ListOfFindResponseItems>
+        <ToggleSpecialMembershipRequestStatusModal
+          :is-visible="isToggleStatusModalVisible"
+          :item="itemToUpdate"
+          @dismiss="hideToggleStatusModal()"
+        ></ToggleSpecialMembershipRequestStatusModal>
+      </Card>
+    </div>
+    <div class="col-lg-4 col-xl-3 order-1 order-lg-2">
+      <Card>
+        <template #header>
+          <h4 class="p-2 m-0 font-weight-bold">Special Membership Access</h4>
+        </template>
+        <div>
+          <p class="m-2">Manage special membership access for institutions.</p>
+          <Reviewer class="mt-4"></Reviewer>
+        </div>
       </Card>
     </div>
   </div>
@@ -67,6 +109,39 @@ import Card from '../components/Card.vue'
 import ListOfFindResponseItems from '@/components/ListOfFindResponseItems.vue'
 import SpecialMembershipRequestReviewItem from '@/components/modules/lists/SpecialMembershipRequestReviewItem.vue'
 import { userSpecialMembershipRequestsReviews as userSpecialMembershipRequestsReviewsService } from '@/services'
+import ToggleSpecialMembershipRequestStatusModal from '../components/reviews/ToggleSpecialMembershipRequestStatusModal.vue'
+import { ref, computed } from 'vue'
+import { ServiceFindParams, UserSpecialMembershipRequestReview } from '@/services/types'
+import BSearchInputForm from '@/components/legacy/bootstrap/BSearchInputForm.vue'
+import Reviewer from '../components/Reviewer.vue'
+
+const itemToUpdate = ref<UserSpecialMembershipRequestReview>(null)
+const isToggleStatusModalVisible = ref(false)
+
+const hideToggleStatusModal = () => {
+  isToggleStatusModalVisible.value = false
+}
+const showToggleStatusModal = (item: UserSpecialMembershipRequestReview) => {
+  itemToUpdate.value = item
+  isToggleStatusModalVisible.value = true
+}
+
+const orderByOptions = ['-dateLastModified', 'dateLastModified']
+
+const orderBy = ref('-dateLastModified')
+const q = ref('')
+const userSpecialMembershipRequestsReviewsServiceParams = computed<ServiceFindParams>(() => ({
+  query: {
+    limit: 2,
+    order_by: orderBy.value,
+    q: q.value
+  }
+}))
+
+const performSearch = (searchTerm: string) => {
+  console.info('performSearch', searchTerm)
+  q.value = searchTerm
+}
 </script>
 
 <i18n lang="json">
@@ -79,7 +154,13 @@ import { userSpecialMembershipRequestsReviews as userSpecialMembershipRequestsRe
     "userSpecialMembershipRequestsDates": "Dates",
     "specialMembershipAccessTitle": "Special Membership Access",
     "userHasAlreadySpecialMembership": "Other special Membership",
-    "userSpecialMembershipRequestsStatus": "Status"
+    "userSpecialMembershipRequestsStatus": "Status",
+    "sortBy": "Sort by",
+    "orderBy_dateLastModified": "Date Modified",
+    "orderBy_-dateLastModified": "Date Modified (Descending)",
+    "actions": {
+      "toggleStatus": "Toggle Status"
+    }
   }
 }
 </i18n>
