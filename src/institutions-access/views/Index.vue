682e87c28c316f7d55d467d4525967a74960f716
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2026-03-24 13:27:24 +0100

    Update Index.vue

--- src/institutions-access/views/Index.vue
+++ src/institutions-access/views/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class="col-lg-8 col-xl-7 order-2 order-lg-1 offset-xl-1">
-      <Card>
+      <Card class="mt-4" r>
         <template #header>
           <h4 class="p-2 m-0 font-weight-bold">Pending Special Membership requests</h4>
         </template>
@@ -16,6 +16,7 @@
           <template #header="{ total }">
             <div class="p-2 pb-3 d-flex gap-2 justify-content-between align-items-center">
               <h5 class="m-0 font-size-inherit" v-html="$t('listTitle')"></h5>
+
               <span v-html="$t('numbers.itemsGeneric', { n: $n(total) }, total)"></span>
             </div>
             <div class="container-fluid">
@@ -36,30 +37,73 @@
               </div>
             </div>
           </template>
-          <template #beforeHeader>
+          <template #beforeHeader> </template>
+          <template #default="{ items }">
+            <div class="container-fluid">
+              <SpecialMembershipRequestReviewItem
+                :item="item"
+                class="py-3"
+                v-for="(item, index) in items"
+                :key="item.id"
+                :class="{
+                  'border-bottom border-dark ': index < items.length - 1
+                }"
+              >
+                <template #actions>
+                  <button
+                    class="btn btn-sm btn-outline-secondary mt-2"
+                    @click="showToggleStatusModal(item)"
+                  >
+                    {{ $t('actions.toggleStatus') }}
+                  </button>
+                </template>
+              </SpecialMembershipRequestReviewItem>
+            </div>
+          </template>
+        </ListOfFindResponseItems>
+        <ToggleSpecialMembershipRequestStatusModal
+          :is-visible="isToggleStatusModalVisible"
+          :item="itemToUpdate"
+          @dismiss="hideToggleStatusModal()"
+        ></ToggleSpecialMembershipRequestStatusModal>
+      </Card>
+      <Card class="mt-4">
+        <template #header>
+          <h4 class="p-2 m-0 font-weight-bold">Approved Special Membership requests</h4>
+        </template>
+        <ListOfFindResponseItems
+          :error-loading-items-message="$t('errorLoadingSpecialMembershipRequests')"
+          :list-is-empty-message="$t('listIsEmpty')"
+          :service="userSpecialMembershipRequestsReviewsService"
+          :title="$t('listTitle')"
+          items-class=""
+          :params="userSpecialMembershipRequestsReviewsAcceptedServiceParams"
+        >
+          <template #header="{ total }">
+            <div class="p-2 pb-3 d-flex gap-2 justify-content-between align-items-center">
+              <h5 class="m-0 font-size-inherit" v-html="$t('listTitle')"></h5>
+
+              <span v-html="$t('numbers.itemsGeneric', { n: $n(total) }, total)"></span>
+            </div>
             <div class="container-fluid">
               <div class="row pb-2 small align-items-center font-weight-bold">
-                <div class="col-md-4">
-                  <BSearchInputForm @submit="performSearch" />
+                <div class="col-3">
+                  {{ $t('userSpecialMembershipRequestsRequester') }}
                 </div>
-                <div class="col-md-4">
-                  <label class="small-caps mr-2">{{ $t('sortBy') }}</label>
-                  <i-dropdown
-                    v-model="orderBy"
-                    :options="
-                      orderByOptions.map((value: string) => ({
-                        value,
-                        text: $t(`orderBy_${value}`)
-                      }))
-                    "
-                    class="mr-auto"
-                    size="sm"
-                    variant="outline-primary"
-                  ></i-dropdown>
+                <div class="col-3">
+                  {{ $t('userSpecialMembershipRequestsDates') }}
+                </div>
+                <div class="col-3">
+                  {{ $t('specialMembershipAccessTitle') }}
+                </div>
+
+                <div class="col-3">
+                  {{ $t('userSpecialMembershipRequestsStatus') }}
                 </div>
               </div>
             </div>
           </template>
+
           <template #default="{ items }">
             <div class="container-fluid">
               <SpecialMembershipRequestReviewItem
@@ -98,6 +142,24 @@
         <div>
           <p class="m-2">Manage special membership access for institutions.</p>
         </div>
+        <div>
+          <BSearchInputForm @submit="performSearch" :placeholder="$t('searchPlaceholder')" />
+        </div>
+        <div class="mt-3 pt-2 border-top">
+          <label class="m-2">{{ $t('sortBy') }}</label>
+          <i-dropdown
+            v-model="orderBy"
+            :options="
+              orderByOptions.map((value: string) => ({
+                value,
+                text: $t(`orderBy_${value}`)
+              }))
+            "
+            class="mr-auto"
+            size="sm"
+            variant="outline-primary"
+          ></i-dropdown>
+        </div>
       </Card>
     </div>
   </div>
@@ -130,11 +192,23 @@ const orderBy = ref('-dateLastModified')
 const q = ref('')
 const userSpecialMembershipRequestsReviewsServiceParams = computed<ServiceFindParams>(() => ({
   query: {
-    limit: 2,
+    limit: 20,
+    status: ['pending'],
     order_by: orderBy.value,
     term: q.value
   }
 }))
+
+const userSpecialMembershipRequestsReviewsAcceptedServiceParams = computed<ServiceFindParams>(
+  () => ({
+    query: {
+      limit: 20,
+      status: ['approved'],
+      order_by: orderBy.value,
+      term: q.value
+    }
+  })
+)
 
 const performSearch = (searchTerm: string) => {
   console.info('performSearch', searchTerm)
