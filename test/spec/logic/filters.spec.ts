12fe24becfb91ebd370c6bd55e4e01d270ecb7b9
Daniele Guido <gui.daniele@gmail.com>
2026-04-15 10:21:16 +0200

    clean not working, legacy tests

--- test/spec/logic/filters.spec.ts
+++ test/spec/logic/filters.spec.ts
@@ -1,15 +1,20 @@
 import { describe, test as it } from 'vitest'
-import assert from 'assert';
+import assert from 'assert'
 import { optimizeFilters } from '../../../src/logic/filters'
+
+type FilterLike = {
+  type: string
+  q: string | string[]
+}
 
 describe('optimizeFilters', () => {
   it('does not merge filters of different types', () => {
-    const filters = [
+    const filters: FilterLike[] = [
       { type: 'string', q: 'foo' },
       { type: 'language', q: ['de'] },
       { type: 'newspaper', q: ['DTT', 'BOO'] }
     ]
-    const optimizedFilters = [
+    const optimizedFilters: FilterLike[] = [
       { type: 'string', q: 'foo' },
       { type: 'language', q: 'de' },
       { type: 'newspaper', q: ['DTT', 'BOO'] }
@@ -19,13 +24,11 @@ describe('optimizeFilters', () => {
   })
 
   it('optimizes filters with single elements and default operators', () => {
-    const filters = [
+    const filters: FilterLike[] = [
       { type: 'string', q: ['foo'] },
       { type: 'string', q: ['de'] }
     ]
-    const optimizedFilters = [
-      { type: 'string', q: ['foo', 'de'] },
-    ]
+    const optimizedFilters: FilterLike[] = [{ type: 'string', q: ['foo', 'de'] }]
 
     assert.deepEqual(optimizeFilters(filters), optimizedFilters)
   })
