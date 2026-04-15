6e18b7736f4cdc834f0f438d4c6275105d1873a8
Daniele Guido <gui.daniele@gmail.com>
2026-04-15 10:20:34 +0200

    update vitest to cover context2D

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
+      setupFiles: ['./test/setup/vitest.setup.ts'],
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
