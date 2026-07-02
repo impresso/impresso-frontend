5504ed37be81df2ef463753414f7719676bfba01
Daniele Guido <gui.daniele@gmail.com>
2026-07-02 08:28:46 +0200

    main layout refactored with responsive auto width

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,51 +1,47 @@
 <template>
   <main :style="computedStyle">
-    <slot></slot>
+    <slot />
   </main>
 </template>
 
-<script>
+<script setup lang="ts">
+import { computed, type CSSProperties } from 'vue'
+
 /*
 <i-layout id="SearchPage" width="800px" />
 <i-layout id="SearchPage" min-width="500px" max-width="800px" />
 */
-export default {
-  props: {
-    width: {
-      default: false, // 50vw, 768px, 80%
-    },
-    minWidth: {
-      default: false,
-    },
-    maxWidth: {
-      default: false,
-    },
-  },
-  computed: {
-    computedStyle() {
-      const style = {}
 
-      if (this.width) {
-        style.width = this.width
-      }
-
-      if (this.minWidth) {
-        style['min-width'] = this.minWidth
-      }
-
-      if (this.maxWidth) {
-        style['max-width'] = this.maxWidth
-      }
-
-      return style
-    },
-  },
+interface Props {
+  width?: string | false // 50vw, 768px, 80%
+  minWidth?: string | false
+  maxWidth?: string | false
 }
+
+const props = withDefaults(defineProps<Props>(), {
+  width: false,
+  minWidth: false,
+  maxWidth: false
+})
+
+const computedStyle = computed<CSSProperties>(() => {
+  const style: CSSProperties = {
+    width: props.width || '100%'
+  }
+  if (props.minWidth) {
+    style.minWidth = props.minWidth
+  }
+  if (props.maxWidth) {
+    style.maxWidth = props.maxWidth
+  }
+  return style
+})
 </script>
 
-<style scoped lang="less">
+<style scoped lang="scss">
 main {
   height: 100%;
+  width: 100%;
   display: flex;
   margin: 0 auto; // center if width is not 100% of viewport
 }
